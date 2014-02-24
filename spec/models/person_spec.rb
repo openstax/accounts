fafd090767c179016930ffe1854ff098bb453984
karen chan <karen@karen-chan.com>
2014-02-24 14:18:48 +0100

    Remove pending tests

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'deletion' do
+    before :each do
+      @person = FactoryGirl.create :person
+      @user1 = FactoryGirl.create :user, person: @person
+      @user2 = FactoryGirl.create :user, person: @person
+    end
+
+    it 'does not delete the person when deleting a user' do
+      @user1.destroy
+
+      expect(Person.exists? @person.id).to be_true
+      expect(User.exists? @user1.id).to be_false
+      expect(User.exists? @user2.id).to be_true
+    end
+
+    it 'does delete all the users when deleting a person' do
+      @person.destroy
+
+      expect(Person.exists? @person.id).to be_false
+      expect(User.exists? @user1.id).to be_false
+      expect(User.exists? @user2.id).to be_false
+    end
+  end
+
 end
