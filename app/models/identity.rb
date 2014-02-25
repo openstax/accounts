b82b1f2584f1904aef4cea5a91ef71708e50906f
karen chan <karen@karen-chan.com>
2014-02-25 21:53:00 +0100

    Add SSHA1 password digest authentication to identity users

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -5,4 +5,28 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
                        length: {minimum: 8, maximum: 40}
 
   attr_accessible :password_digest, :password, :password_confirmation, :user_id
+
+  def authenticate unencrypted_password
+    if is_password_digest_ssha?
+      authenticate_with_ssha unencrypted_password
+    else
+      super
+    end
+  end
+
+  protected
+  def is_password_digest_ssha?
+    self.password_digest.start_with? '{SSHA}'
+  end
+
+  def authenticate_with_ssha unencrypted_password
+    _, password_digest = self.password_digest.split('}', 2)
+    salt = Base64::decode64(password_digest)[20..-1]
+    sha_digest = Digest::SHA1.digest(unencrypted_password + salt)
+    if Base64::encode64(sha_digest + salt)[0..-2] == password_digest
+      self
+    else
+      false
+    end
+  end
 end
---
+++ spec/models/identity_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Identity do
+  context 'password authentication' do
+    before :each do
+      @bcrypt = FactoryGirl.create :identity, password: 'password'
+      @plone = FactoryGirl.create :identity
+      @plone.update_attribute :password_digest, '{SSHA}RmBlDXdkdJaQkDsr790+eKaY9xHQdPVNwD/B'
+    end
+
+    it 'returns self if bcrypt password digest matches password' do
+      expect(@bcrypt.authenticate 'password').to eq(@bcrypt)
+    end
+
+    it 'returns false if bcrypt password digest does not match password' do
+      expect(@bcrypt.authenticate 'pass').to be_false
+    end
+
+    it 'returns self if ssha password digest matches password' do
+      expect(@plone.authenticate 'password').to eq(@plone)
+    end
+
+    it 'returns false if ssha password digest does not match password' do
+      expect(@plone.authenticate 'pass').to be_false
+    end
+
+  end
+end
