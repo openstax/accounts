cda3fee3c330f73000b6e1fde2edd620657e77b0
JP Slavinsky <jpslav@gmail.com>
2014-02-25 07:59:04 -0800

    changed some uses of 'Services', still need to change app name

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require "sign_in_state"
 require "omniauth/strategies/custom_identity"
 require "api_constraints"
 
-SITE_NAME = "OpenStax Services"
+SITE_NAME = "OpenStax Accounts"
 TEAM_NAME = "OpenStax" # used when talking about our team
 COPYRIGHT_HOLDER = "Rice University"
 
