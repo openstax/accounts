f84fc4eaff30dc39a55786403da6612934589371
karen chan <karen@karen-chan.com>
2014-02-26 13:27:15 +0100

    Add comment and error handling for plone password authentication code

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -19,9 +19,23 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
     self.password_digest.start_with? '{SSHA}'
   end
 
-  def authenticate_with_ssha unencrypted_password
+  def authenticate_with_ssha(unencrypted_password)
+    # This code is originally in python, Plone 2.5
+    # from AccessControl.AuthEncoding.SSHADigestScheme.validate
+    #
+    # def validate(self, reference, attempt):
+    #     try:
+    #         ref = a2b_base64(reference)
+    #     except binascii.Error:
+    #         # Not valid base64.
+    #         return 0
+    #     salt = ref[20:]
+    #     compare = b2a_base64(sha.new(attempt + salt).digest() + salt)[:-1]
+    #     return (compare == reference)
+    #
     _, password_digest = self.password_digest.split('}', 2)
     salt = Base64::decode64(password_digest)[20..-1]
+    return false if salt.nil?
     sha_digest = Digest::SHA1.digest(unencrypted_password + salt)
     if Base64::encode64(sha_digest + salt)[0..-2] == password_digest
       self
