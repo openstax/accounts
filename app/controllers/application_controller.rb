6eda6c5dac93e6468b688ce0c8d4769efbdc8b49
JP Slavinsky <jpslav@gmail.com>
2014-03-27 18:06:38 -0700

    added explicit rescue_from_exception to application controller, already have in api controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,46 @@ class ApplicationController < ActionController::Base
 
   layout 'application_body_only'
 
+  rescue_from Exception, :with => :rescue_from_exception
+
 protected
 
   # These methods are defined in the base class via the 02... initializer
   helper_method :current_user, :current_user=,
                 :signed_in?, :sign_in, :sign_out!
+
+  def rescue_from_exception(exception)
+    # See https://github.com/rack/rack/blob/master/lib/rack/utils.rb#L453 for error names/symbols
+    error = :internal_server_error
+    notify = true
+
+    case exception
+    when SecurityTransgression
+      error = :forbidden
+      notify = false
+    when ActiveRecord::RecordNotFound, 
+         ActionController::RoutingError,
+         ActionController::UnknownController,
+         AbstractController::ActionNotFound
+      error = :not_found
+      notify = false
+    end
+
+    if notify
+      ExceptionNotifier.notify_exception(
+        exception,
+        env: request.env,
+        data: { message: "An exception occurred" }
+      )
+
+      Rails.logger.error("An exception occurred: #{exception.message}\n\n#{exception.backtrace.join("\n")}")
+    end
+
+    if Accounts::Application.config.consider_all_requests_local
+      raise exception
+    else
+      head error
+    end
+  end
 
 end
