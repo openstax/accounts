667d81908765f845eac5a6ac5977fdf909e6999b
Dante Soares <dms3@rice.edu>
2014-04-10 18:42:19 -0500

    Changed the ApplicationUser create API slightly; Added non-null constraints to DB

--- app/access_policies/application_user_access_policy.rb
+++ app/access_policies/application_user_access_policy.rb
@@ -2,13 +2,16 @@ class ApplicationUserAccessPolicy
   # Contains all the rules for which requestors can do what with which ApplicationUser objects.
 
   def self.action_allowed?(action, requestor, application_user)
+    # Human users take precedence in AccessPolicy,
+    # so the create check has to be done elsewhere
+    # Currently relying on the ApplicationUser's validations
     if requestor.is_human?
       return requestor.is_administrator? ||
         (requestor == application_user.user &&
-          [:read, :update, :destroy].include?(action))
+        [:read, :create, :update, :destroy].include?(action))
     else
       return (requestor == application_user.application &&
-        [:read, :create, :update, :destroy].include?(action))
+        [:read, :update, :destroy].include?(action))
     end
   end
 
