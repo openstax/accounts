a4743ad7c765008a80ca77f3c336976eaed642f3
Dante Soares <dms3@rice.edu>
2014-05-23 19:00:26 -0500

    Fixed access policy for messages

--- config/initializers/access_policies.rb
+++ config/initializers/access_policies.rb
@@ -1,3 +1,4 @@
 OSU::AccessPolicy.register(User, UserAccessPolicy)
 OSU::AccessPolicy.register(ContactInfo, ContactInfoAccessPolicy)
 OSU::AccessPolicy.register(ApplicationUser, ApplicationUserAccessPolicy)
+OSU::AccessPolicy.register(Message, MessageAccessPolicy)
