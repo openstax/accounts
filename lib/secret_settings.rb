3d0544ec8e2e7bb81fd86ca4028e9133f9e3c764
karen chan <karen@karen-chan.com>
2014-06-03 19:16:32 +0200

    Load config/secret_settings.yml in lib/secret_settings.rb

--- lib/secret_settings.rb
+++ lib/secret_settings.rb
@@ -1,2 +1,2 @@
 # Load the secret settings file
-SECRET_SETTINGS = OpenStax::Utilities::Settings.load_settings(__FILE__, '..', 'secret_settings.yml')
+SECRET_SETTINGS = OpenStax::Utilities::Settings.load_settings(__FILE__, '../config', 'secret_settings.yml')
