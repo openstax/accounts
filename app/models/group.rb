b1a8e7cb72b92d88aa58eae34d39ba072733cecc
Dante Soares <dms3@rice.edu>
2014-07-14 17:35:24 -0500

    Removed old group code with access levels

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  belongs_to :supergroup, inverse_of: :groups
+  belongs_to :owner, polymorphic: true
 
   has_many :group_users, dependent: :destroy, inverse_of: :group
   has_many :users, through: :group_users
@@ -8,19 +8,14 @@ class Group < ActiveRecord::Base
                                 as: :owner,
                                 dependent: :destroy
 
-  attr_accessible :name
-
-  validates :name, uniqueness: true, allow_nil: true
-  validates :group_users, presence: true
-
-  after_create :maintenance
+  validates_uniqueness_of :name, scope: [:owner_id, :owner_type]
 
   def group_user_for(user)
     group_users.where(:user_id => user.try(:id)).first
   end
   
-  def add_user(user, access_level = GroupUser::MEMBER)
-    gu = GroupUser.new(access_level: access_level)
+  def add_user(user)
+    gu = GroupUser.new
     gu.group = self
     gu.user = user
     gu.save if persisted?
@@ -32,30 +27,7 @@ class Group < ActiveRecord::Base
   end
   alias_method :has_user?, :has_member?
 
-  def has_manager?(user)
-    gu = group_user_for(user)
-    !gu.nil? && gu.is_manager?
-  end
-
   def has_owner?(user)
-    gu = group_user_for(user)
-    !gu.nil? && gu.is_owner?
-  end
-
-  def maintenance
-    remaining_users = group_users(true)
-
-    # Destroy group if everyone left
-    return destroy if remaining_users.empty?
-
-    remaining_managers = remaining_users.select{|gu| gu.access_level >= GroupUser::MANAGER}
-    remaining_owners = remaining_users.select{|gu| gu.access_level >= GroupUser::OWNER}
-
-    # Promote some user to owner if all owners left
-    if remaining_managers.empty?
-      remaining_users.first.update_attribute(:access_level, GroupUser::OWNER)
-    elsif remaining_owners.empty?
-      remaining_managers.first.update_attribute(:access_level, GroupUser::OWNER)
-    end
+    # TODO
   end
 end
--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,38 +1,9 @@
 class GroupUser < ActiveRecord::Base
   #sortable :user_id
 
-  # Access Levels
-  # Higher levels include all the levels below
-  # So an owner is always a manager, etc
-  OWNER = 300
-  MANAGER = 200
-  MEMBER = 100
-
   belongs_to :group, inverse_of: :group_users
   belongs_to :user, inverse_of: :group_users
 
-  attr_accessible :access_level
-
-  after_update :group_maintenance
-  after_destroy :group_maintenance
-
-  validates :user, :group, presence: true
-  validates_uniqueness_of :user_id, scope: :group_id, if: :group_id
-
-  scope :owners, lambda {where{access_level.gteq OWNER}}
-  scope :managers, lambda {where{access_level.gteq MANAGER}}
-
-  def is_owner?
-    access_level >= OWNER
-  end
-
-  def is_manager?
-    access_level >= MANAGER
-  end
-
-  protected
-
-  def group_maintenance
-    group.maintenance
-  end
+  validates_presence_of :user, :group
+  validates_uniqueness_of :user_id, scope: :group_id
 end
