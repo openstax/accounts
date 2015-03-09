bcd13208ee4c1a2fc3ac073b85eceba624bf5925
Nathan Stitt <nathan@stitt.org>
2015-03-09 15:58:03 -0500

    Default state to "activated"

--- lib/import_users.rb
+++ lib/import_users.rb
@@ -59,7 +59,7 @@ class ImportUsers
     person = Person.create!
     @user = User.new
     @user.username = username
-    @user.state = 'active'
+    @user.state = 'activated'
     @user.title = title
     @user.first_name = first_name
     @user.last_name = last_name
