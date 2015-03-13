a7d82da7c799ec38c733429d8e51f92730f95183
karen chan <karen@karen-chan.com>
2015-03-13 23:37:32 +0100

    Add error pages for 403, 404 and 500
    circumvented Capybara's apparent inability to deal with routes that
    don't exist when testing 404 error pages

---
+++ spec/features/error_pages_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature 'Show error pages', js: true do
+  scenario 'for internal server error 500' do
+    with_error_pages do
+      visit '/admin/raise_not_yet_implemented'
+      expect(page.status_code).to eq(500)
+      expect(page).to have_content('500 Internal Server Error')
+    end
+  end
+
+  scenario 'for not found 404' do
+    with_error_pages do
+      visit '/admin/raise_routing_error'
+      expect(page.status_code).to eq(404)
+      expect(page).to have_content('404 Not Found')
+    end
+  end
+
+  scenario 'for forbidden 403' do
+    with_error_pages do
+      visit '/admin/raise_security_transgression'
+      expect(page.status_code).to eq(403)
+      expect(page).to have_content('403 Forbidden')
+    end
+  end
+end
