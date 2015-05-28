2b04ed4188e04dc0ffde43ae4af4cf1ba86a371c
karen chan <karen@karen-chan.com>
2015-05-28 20:25:02 +0200

    Add accounts:accept_all_terms rake task
    Accept all terms and licenses for all users

---
+++ spec/lib/accept_all_terms_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'accept_all_terms'
+
+describe AcceptAllTerms do
+  let!(:users) { (1..15).collect { |i| FactoryGirl.create(:user) } }
+
+  it 'accept all fine print contracts for users' do
+    expect {
+      AcceptAllTerms.new.run
+    }.to change(FinePrint::Signature, :count).by(User.count * FinePrint::Contract.count)
+  end
+end
