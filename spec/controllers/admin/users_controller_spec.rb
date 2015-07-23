c657ffaad64eda3e8fc78b817b41162a76fb4fc2
karen chan <karen@karen-chan.com>
2015-07-23 19:21:54 +0200

    Add update password tests for admin users controller

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Admin::UsersController do
   let!(:user) { FactoryGirl.create :user }
+  let!(:identity) { FactoryGirl.create :identity, user: user }
 
   describe 'PUT #update' do
     it 'updates a user' do
@@ -11,7 +12,9 @@ RSpec.describe Admin::UsersController do
                      last_name: 'Kristensen',
                      full_name: 'Malik A. Kristensen',
                      email_address: 'malik@example.org',
-                     is_administrator: '1'
+                     is_administrator: '1',
+                     password: 'si4eeSai',
+                     password_confirmation: 'si4eeSai'
                    }
       user.reload
       expect(user.first_name).to eq 'Malik'
@@ -19,6 +22,7 @@ RSpec.describe Admin::UsersController do
       expect(user.full_name).to eq 'Malik A. Kristensen'
       expect(user.email_addresses.first.value).to eq 'malik@example.org'
       expect(user.is_administrator).to be true
+      expect(user.identity.authenticate('si4eeSai')).to eq user.identity
     end
   end
 end
