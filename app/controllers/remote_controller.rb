0df42f043c1035be366f1dba7aebfe5720442546
Nathan Stitt <nathan@stitt.org>
2015-10-14 15:40:25 -0500

    Lock down access to validated origin domains

--- app/controllers/remote_controller.rb
+++ app/controllers/remote_controller.rb
@@ -4,6 +4,8 @@ class RemoteController < ApplicationController
   skip_before_filter :registration,       only: [:iframe, :v1]
   skip_before_filter :expired_password,   only: [:iframe, :v1]
 
+  before_filter :validate_iframe_parent, :only=>:iframe
+
   layout false
 
   def iframe
@@ -19,5 +21,17 @@ class RemoteController < ApplicationController
       format.js
     end
   end
+
+
+  private
+
+  def validate_iframe_parent
+    @iframe_parent = params[:parent]
+    unless SECRET_SETTINGS[:valid_iframe_origins].any?{|origin| @iframe_parent =~ /^#{origin}/ }
+      raise SecurityError.new("#{@iframe_parent} is not allowed to iframe content")
+    end
+  end
+
+
 
 end
