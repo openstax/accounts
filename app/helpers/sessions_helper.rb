1065b013a3d5d6ecfbc5f0a90b769aa29aaf52a9
Krzysztof Mędrzycki <aiwenari@gmail.com>
2015-12-06 12:19:24 +0100

    Internationalised views/sessions/ask_new_or_returning.html.erb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,27 +1,23 @@
 module SessionsHelper
 
-  def authentications_list(authentications)
+  def authentications_list(authentications, scope)
     providers = authentications.collect{|auth| auth.provider}
-    provider_list(providers, 'and')
+    provider_list(providers, :all, scope)
   end
 
-  def remaining_authentications_list(authentications)
+  def remaining_authentications_list(authentications, scope)
     providers = authentications.collect{|auth| auth.provider}
-    provider_list(%w(facebook twitter google identity) - providers, 'or')
+    provider_list(%w(facebook twitter google identity) - providers, :any, scope)
   end
 
-  def provider_list(providers, conjunction)
+  def provider_list(providers, kind, scope)
     list = []
 
-    list.push('Facebook')          if providers.include?('facebook')
-    list.push('Twitter')           if providers.include?('twitter')
-    list.push('Google')            if providers.include?('google_oauth2')
-    list.push('a simple password') if providers.include?('identity')
-    
-    if list.size >= 2
-      list[list.size-1] = "#{conjunction} " + list[list.size-1]
-    end
-    
-    list.size >= 3 ? list.join(', ') : list.join(' ')
+    list.push(:facebook)          if providers.include?('facebook')
+    list.push(:twitter)           if providers.include?('twitter')
+    list.push(:google)            if providers.include?('google_oauth2')
+    list.push(:simple_password)   if providers.include?('identity')
+
+    return I18n.enumerate kind, list, scope: scope
   end
 end
