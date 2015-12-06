4faafa2e2270225a114bbb7b4284c4ed55f607a7
Krzysztof Mędrzycki <aiwenari@gmail.com>
2015-12-06 16:10:28 +0100

    Internationalised handlers/identities_forgot_password.rb

--- app/handlers/identities_forgot_password.rb
+++ app/handlers/identities_forgot_password.rb
@@ -20,16 +20,16 @@ class IdentitiesForgotPassword
     user = User.find_by_username(username_or_email) ||
            ContactInfo.find_by_value(username_or_email).try(:user)
     if user.nil?
-      fatal_error(code: 'Username not found',
+      fatal_error(code: (I18n.t :"handlers.identities_forgot_password.username_not_found"),
                   offending_inputs: [:username])
     end
     if user.identity.nil?
-      fatal_error(code: 'Unable to reset password for this user',
+      fatal_error(code: (I18n.t :"handlers.identities_forgot_password.unable_to_reset_password"),
                   offending_inputs: [:identity])
     end
     email_addresses = user.contact_infos.email_addresses.verified
     if email_addresses.empty?
-      fatal_error(code: 'No verified email addresses found for this user',
+      fatal_error(code: (I18n.t :"handlers.identities_forgot_password.no_verified_emails"),
                   offending_inputs: [:email_address])
     end
     code = run(GeneratePasswordResetCode, user.identity).outputs[:code]
