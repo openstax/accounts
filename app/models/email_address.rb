94ac3f3a9123a5285a1e5080cc53003cfeb8605f
karen chan <karen@karen-chan.com>
2016-01-30 22:33:39 +0000

    Disallow spaces in email addresses
    To fix this error occured in identities#forgot_password:
    
    ```
    data: {:error_id=>"074289",
       :class=>"AWS::SES::ResponseError",
       :message=>
        "InvalidParameterValue - Local address contains control or whitespace",
       :first_line_of_backtrace=>
        "/home/ostaccounts/src/accounts-a4650b5/vendor/bundle/ruby/1.9.1/gems/aws-ses-0.6.0/lib/aws/ses/base.rb:175:in `request'",
       :cause=>nil,
       :dns_name=>"unknown",
       :extras=>{}}
    ```

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,3 +1,5 @@
 class EmailAddress < ContactInfo
-  validates :value, format: { with: /\A[^@]+@[^@.]+\.[^@]+\z/ }
+  validates :value, format: {
+    with: /\A[^@ ]+@[^@. ]+\.[^@ ]+\z/,
+    message: "\"%{value}\" is not a valid email address" }
 end
