95527279ec79f32beff33d5c38375ae620926586
karen chan <karen@karen-chan.com>
2016-02-09 15:36:17 +0000

    Quote full name in "to" header in emails
    When user has a full name that includes certain characters like commas,
    we need to quote it.
    
    Fix `AWS::SES Response Error: InvalidParameterValue`

--- spec/mailers/reset_password_mailer_spec.rb
+++ spec/mailers/reset_password_mailer_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe ResetPasswordMailer do
   describe 'reset_password' do
     before :each do
-      @user = FactoryGirl.create :user, username: 'user1'
+      @user = FactoryGirl.create :user, username: 'user1', full_name: 'John Doe, Jr.'
       @email = FactoryGirl.create :email_address, user: @user
       @mail = ResetPasswordMailer.reset_password @email, '1234'
     end
 
     it 'renders the headers' do
       expect(@mail.subject).to eq('[OpenStax] Reset your password')
-      expect(@mail.to).to eq([@email.value])
+      expect(@mail.header['to'].to_s).to eq("\"John Doe, Jr.\" <#{@email.value}>")
       expect(@mail.from).to eq(['noreply@openstax.org'])
     end
 
