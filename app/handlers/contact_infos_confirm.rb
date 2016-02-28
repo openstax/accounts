4fedd27217eb91c33ade171a857a0c70fff8a7ca
Krzysztof Mędrzycki <aiwenari@gmail.com>
2016-02-28 15:09:35 +0100

    Internationalised handlers/contact_infos_confirm.rb

--- app/handlers/contact_infos_confirm.rb
+++ app/handlers/contact_infos_confirm.rb
@@ -13,10 +13,10 @@ class ContactInfosConfirm
 
   def handle
     fatal_error(code: :no_contact_info_for_code,
-                message: 'Unable to verify email address') if params[:code].nil?
+                message: (I18n.t :"handlers.contact_infos_confirm.unable_to_verify_address")) if params[:code].nil?
     contact_info = ContactInfo.where(confirmation_code: params[:code]).first
     fatal_error(code: :no_contact_info_for_code,
-                message: 'Unable to verify email address') if contact_info.nil?
+                message: (I18n.t :"handlers.contact_infos_confirm.unable_to_verify_address")) if contact_info.nil?
     outputs[:contact_info] = contact_info
     run(MergeUnclaimedUsers, contact_info)
     run(MarkContactInfoVerified, contact_info)
