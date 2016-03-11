b24e45cfc2fdf2aa8f5e1263f9de9bff0a9e8bf8
JP Slavinsky <jpslav@gmail.com>
2016-03-11 13:26:46 -0800

    Step 1 of converting from jquery-ui to bootstrap and cleaning unused stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,14 +8,14 @@ module ApplicationHelper
     end
 
     @handler_result.errors.any? ?
-      js_refresh_alerts(options) : 
+      js_refresh_alerts(options) :
       js_refresh_alerts(options) + capture(&block).html_safe
   end
 
   def js_refresh_alerts(options={})
     options[:alerts_html_id] ||= 'local-alerts'
     options[:alerts_partial] ||= 'shared/local_alerts'
-    options[:trigger] ||= 'alerts-updated'    
+    options[:trigger] ||= 'alerts-updated'
 
     "$('##{options[:alerts_html_id]}').html('#{ j(render options[:alerts_partial]) }').trigger('#{options[:trigger]}');".html_safe
   end
@@ -28,54 +28,45 @@ module ApplicationHelper
     attention_tag(messages, :notice)
   end
 
-  def attention_tag(messages, type, classes='')
+  def attention_tag(messages, type)
     return if messages.blank? || messages.empty?
-    messages = Array.new(messages).flatten
-    
-    div_class = type == :alert ? "ui-state-error" : "ui-state-highlight"
-    icon_class = type == :alert ? "ui-icon-alert" : "ui-icon-info"
-    
-    content_tag :div, :class => "ui-widget #{classes}" do
-      content_tag :div, :class => "#{div_class} ui-corner-all", 
-                        :style => "margin: 10px 0px -5px; padding: 0 .7em;" do
-        content_tag :p do
-          content_tag(:span, "", :class => "ui-icon #{icon_class}",
-                             :style => "float:left; margin-right: .3em;") +
-          (type == :alert ? content_tag(:strong, "Alert: ") : "") +
+    messages = [messages].flatten
 
-          (messages.size == 1 ? 
-           messages.first : 
-           ("<ul>"+messages.collect{|a| "<li>"+a+"</li>"}.join("")+"</ul>").html_safe)
-        end
-      end
+    div_class = type == :alert ? "alert-danger" : "alert-info"
+
+    content_tag :div, class: div_class, role: "alert" do
+      (type == :alert ? content_tag(:strong, "Alert: ") : "") +
+      (messages.size == 1 ?
+       messages.first :
+       ("<ul>"+messages.collect{|a| "<li>"+a+"</li>"}.join("")+"</ul>").html_safe)
     end
   end
 
   def page_heading(heading_text, options={})
     options[:take_out_site_name] = true if options[:take_out_site_name].nil?
     options[:sub_heading_text] ||= ""
-    options[:title_text] ||= heading_text + (options[:sub_heading_text].blank? ? 
-                                             "" : 
+    options[:title_text] ||= heading_text + (options[:sub_heading_text].blank? ?
+                                             "" :
                                              " [#{options[:sub_heading_text]}]")
-    
+
     @page_title = options[:title_text]
     @page_title.sub!(SITE_NAME,"").strip! if @page_title.include?(SITE_NAME) && options[:take_out_site_name]
-    
+
     return if heading_text.blank?
-    
+
     content_for :page_heading do
-      render(:partial => 'shared/page_heading', 
-             :locals => {:heading_text => heading_text, 
+      render(:partial => 'shared/page_heading',
+             :locals => {:heading_text => heading_text,
                          :sub_heading_text => options[:sub_heading_text]})
     end
-    
+
   end
 
   def standard_field(options={})
 
-    raise IllegalArgument, "Must specify a :type" if !options[:type] 
-    raise IllegalArgument, "Must specify a :form" if !options[:form] 
-    raise IllegalArgument, "Must specify a :name" if !options[:name] 
+    raise IllegalArgument, "Must specify a :type" if !options[:type]
+    raise IllegalArgument, "Must specify a :form" if !options[:form]
+    raise IllegalArgument, "Must specify a :name" if !options[:name]
 
     options[:options] ||= {}
 
@@ -88,7 +79,7 @@ module ApplicationHelper
                 end
       end
       output << content_tag(:div, class: 'form-input') do
-        case options[:type] 
+        case options[:type]
         when :text_field
           options[:form].text_field(options[:name], options[:options])
         when :password_field
