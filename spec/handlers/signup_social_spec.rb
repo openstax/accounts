a0630f5d5bdbe9583afb4e57df203f4245549b9f
JP Slavinsky <jpslav@gmail.com>
2016-03-16 21:02:05 -0700

    Split SignupProcess into SignupSocial and ...Password

---
+++ spec/handlers/signup_social_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+# TODO add specs for missing, but required, params
+# TODO add in UsersRegister specs -- oops maybe are none?  then add specs to test rest of SignupProcess
+
+describe SignupSocial do
+
+  xit 'implement specs' do
+  end
+
+end
