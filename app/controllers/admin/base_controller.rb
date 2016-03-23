810976dd25e4d2f9764154e5d01dcd969f0c0fb7
JP Slavinsky <jpslav@gmail.com>
2016-03-22 18:25:46 -0700

    more moving away from regist* and misc cleanup

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,24 +1,24 @@
-# Copyright 2011-2012 Rice University. Licensed under the Affero General Public 
+# Copyright 2011-2012 Rice University. Licensed under the Affero General Public
 # License version 3 or later.  See the COPYRIGHT file for details.
 
 module Admin
   class BaseController < ApplicationController
-    
+
     include FakeExceptionHelper
 
     if Rails.env.production?
       before_filter :authenticate_admin!
     else
       skip_before_filter :authenticate_user!
-      skip_before_filter :require_registration!
+      skip_before_filter :finish_sign_up
 
       fine_print_skip :general_terms_of_use, :privacy_policy
     end
-    
+
     def cron
       Ost::Cron::execute_cron_jobs
       flash[:notice] = "Ran cron tasks"
-      redirect_to admin_path  
+      redirect_to admin_path
     end
 
     def raise_exception
