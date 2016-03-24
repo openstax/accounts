c24bc4d81cb3a4c48c6f9c9fe83fb0a23fb84d50
JP Slavinsky <jpslav@gmail.com>
2016-03-23 22:39:43 -0700

    Mostly deleting unused files and content, a few small meaningful changes

--- spec/mailers/confirmation_mailer_spec.rb
+++ spec/mailers/confirmation_mailer_spec.rb
@@ -5,21 +5,6 @@ describe ConfirmationMailer, type: :mailer do
   let(:email) { FactoryGirl.create :email_address, value: 'to@example.org',
                                    user_id: user.id, confirmation_code: '1234' }
 
-  describe "reminder" do
-    let(:mail) { ConfirmationMailer.reminder email }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("[OpenStax] Reminder: please verify this email address")
-      expect(mail.header['to'].to_s).to eq('"John Doe Jr." <to@example.org>')
-      expect(mail.from).to eq(["noreply@openstax.org"])
-    end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to include("Hi #{user.casual_name}")
-      expect(mail.body.encoded).to include('http://nohost/confirm?code=1234')
-    end
-  end
-
   describe "instructions" do
     let(:mail) { ConfirmationMailer.instructions email }
 
