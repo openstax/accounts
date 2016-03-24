a17cf0f2727089128a1764d81b87f3985ad32c70
JP Slavinsky <jpslav@gmail.com>
2016-03-23 18:19:26 -0700

    do not delete last auth, check that auths not duplicatable

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -5,4 +5,14 @@ class Authentication < ActiveRecord::Base
 
   validates :provider, presence: true
   validates :uid, presence: true, uniqueness: { scope: :provider }
+
+  before_destroy :check_not_last
+
+  protected
+
+  def check_not_last
+    errors.add(:base, "Cannot delete a user's last authentication") \
+      if user.authentications.size == 1
+    errors.none?
+  end
 end
