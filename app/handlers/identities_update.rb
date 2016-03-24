f270f99b0c82b24443b47cb3db960745f64d5afe
Nathan Stitt <nathan@stitt.org>
2016-03-24 16:27:28 -0500

    Use id of identity for uid of authentication

--- app/handlers/identities_update.rb
+++ app/handlers/identities_update.rb
@@ -22,13 +22,13 @@ class IdentitiesUpdate
     identity_attributes = identity_params.as_hash(:password,
                                                   :password_confirmation)
 
-    # This may be better moved elsewhere, but we'll do so here since it doesn't make
-    # sense to set the password of a identity that won't work without an authentication
+    @identity.update_attributes(identity_attributes)
+
+    # If the user does not have an authentication for an identity then we create once
     unless caller.authentications.where(provider: 'identity').any?
-      caller.authentications.create!(provider: 'identity', uid: caller.id)
+      caller.authentications.create!(provider: 'identity', uid: @identity.id.to_s)
     end
 
-    @identity.update_attributes(identity_attributes)
     outputs[:identity] = @identity
     transfer_errors_from(@identity, {scope: :identity})
   end
