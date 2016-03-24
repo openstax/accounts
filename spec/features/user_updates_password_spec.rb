4c36bfc69192cfd62e81f26750b03f6bf1053f1b
karen chan <karen@karen-chan.com>
2016-03-24 13:32:32 +0000

    Re-enable user updates password feature specs

--- spec/features/user_updates_password_spec.rb
+++ spec/features/user_updates_password_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-xfeature 'User updates password' do
+feature 'User updates password', js: true do
   before(:each) do
     create_user('user')
     visit '/signin'
@@ -10,70 +10,62 @@ xfeature 'User updates password' do
 
   context 'without local password' do
     before(:each) do
-      User.find_by_username('user').identity.destroy
+      user = User.find_by_username('user')
+      FactoryGirl.create :authentication, user: user, provider: 'facebook'
+      user.authentications.where(provider: 'identity').destroy_all
+      user.identity.destroy
     end
 
     scenario 'password form is invisible', js: true do
       visit '/profile'
-      expect(page).to have_content('First Name')
-      expect(page).not_to have_content('Change Your Password')
+      expect(page).to have_content('How you sign in')
+      expect(page).to have_css('.facebook')
+      expect(page).to_not have_css('.identity')
     end
   end
 
   context 'with local password' do
     before(:each) do
       visit '/profile'
-      click_link 'Change Your Password'
-      expect(page).to have_content('Current Password')
-      expect(page).to have_content('Change Your Password')
+      within(:css, '.identity') {
+        find('.glyphicon-pencil').click
+      }
     end
 
     scenario 'success', js: true do
-      fill_in 'Current Password', with: 'password'
-      fill_in 'New Password', with: 'new_password'
-      fill_in 'New Password Confirmation', with: 'new_password'
-      click_button 'Change Password'
+      within(:css, '.identity') {
+        fill_in 'password', with: 'new_password'
+        fill_in 'password_confirmation', with: 'new_password'
+        find('.glyphicon-ok').click
+      }
       expect(page).to have_content('Password changed')
     end
 
-    scenario 'with current password empty or incorrect', js: true do
-      fill_in 'Current Password', with: ''
-      fill_in 'New Password', with: 'new_password'
-      fill_in 'New Password Confirmation', with: 'new_password'
-      click_button 'Change Password'
-      expect(page).to have_content('The password provided did not match our records')
+    scenario 'with password confirmation empty or incorrect' do
+      within(:css, '.identity') {
+        fill_in 'password', with: 'new_password'
+        fill_in 'password_confirmation', with: ''
+        find('.glyphicon-ok').click
+      }
+      expect(page).to have_content("doesn't match confirmation and can't be blank")
       expect(page).not_to have_content('Password changed')
 
-      fill_in 'Current Password', with: 'apswords'
-      fill_in 'New Password', with: 'new_password'
-      fill_in 'New Password Confirmation', with: 'new_password'
-      click_button 'Change Password'
-      expect(page).to have_content('The password provided did not match our records')
-      expect(page).not_to have_content('Password changed')
-    end
-
-    scenario 'with password confirmation empty or incorrect', js: true do
-      fill_in 'Current Password', with: 'password'
-      fill_in 'New Password', with: 'new_password'
-      fill_in 'New Password Confirmation', with: ''
-      click_button 'Change Password'
-      expect(page).to have_content("Password confirmation can't be blank")
-      expect(page).not_to have_content('Password changed')
-
-      fill_in 'Current Password', with: 'password'
-      fill_in 'New Password', with: 'new_password'
-      fill_in 'New Password Confirmation', with: 'new_apswords'
-      click_button 'Change Password'
-      expect(page).to have_content("Password doesn't match confirmation")
+      within(:css, '.identity') {
+        fill_in 'password', with: 'new_password'
+        fill_in 'password_confirmation', with: 'new_apswords'
+        find('.glyphicon-ok').click
+      }
+      expect(page).to have_content("doesn't match confirmation")
       expect(page).not_to have_content('Password changed')
     end
 
-    scenario 'with new password too short', js: true do
-      fill_in 'Current Password', with: 'password'
-      fill_in 'New Password', with: 'pass'
-      fill_in 'New Password Confirmation', with: 'pass'
-      click_button 'Change Password'
-      expect(page).to have_content('Password is too short')
+    scenario 'with new password too short' do
+      within(:css, '.identity') {
+        fill_in 'password', with: 'pass'
+        fill_in 'password_confirmation', with: 'pass'
+        find('.glyphicon-ok').click
+      }
+      expect(page).to have_content('is too short (minimum is 8 characters)')
       expect(page).not_to have_content('Password changed')
     end
   end
