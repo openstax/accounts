8701ccdd5a7975ef17bc7ee78de814b56c8b5b17
Krzysztof Mędrzycki <aiwenari@gmail.com>
2016-07-12 11:16:34 +0200

    Gracefully handle requests without Accept-Language header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,14 +17,20 @@ class ApplicationController < ActionController::Base
   # Sets locale based on information provided by the browser, defaulting to
   # I18n.default_locale.
   def set_locale
-    accepts = request.env['HTTP_ACCEPT_LANGUAGE'].split(',').map do |e|
-      v = e.split ';'
-      lang = v[0].split('-')[0]
-      q = 1 == v.size ? 1 : v[1].to_f
-      [lang.to_sym, q]
-    end.keep_if do |x|
-      I18n.available_locales.include? x[0]
-    end.sort {|a, b| b[1] <=> a[1] }.map {|x| x[0] }
-    I18n.locale = accepts.first || I18n.default_locale
+    accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
+    I18n.locale = unless accept_language.nil?
+        accept_language.split(',').map do |e|
+          v = e.split ';'
+          lang = v[0].split('-')[0]
+          q = 1 == v.size ? 1 : v[1].to_f
+          [lang.to_sym, q]
+        end.keep_if do |x|
+          I18n.available_locales.include? x[0]
+        end.sort {|a, b| b[1] <=> a[1] }
+           .map {|x| x[0] }
+           .first || I18n.default_locale
+      else
+       I18n.default_locale
+      end
   end
 end
