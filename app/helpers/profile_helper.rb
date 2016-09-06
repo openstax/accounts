318b4fef1f9b0ed76748ef2f8a451d92b16d3e1f
bryanelidimas <bryanelidimas@gmail.com>
2016-09-06 10:51:57 -0500

    Disable Click to verify button on click

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -35,7 +35,7 @@ module ProfileHelper
   end
 
   def email_entry(value:, id:, is_verified:, is_searchable:)
-    verify_link = is_verified ? '' : "<span class='verify'>(#{link_to('Click to verify', resend_confirmation_contact_info_path(id))})</span>"
+    verify_link = is_verified ? '' : "<span class='verify'>(#{button_to('Click to verify', resend_confirmation_contact_info_path(id), method: :put ) })</span>"
     (
       <<-SNIPPET
         <div class="email-entry controls-hidden" data-id="#{id}">
