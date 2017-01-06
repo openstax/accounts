06ffa6caed22121429e04b9f92b49367c0f4cae1
JP Slavinsky <jpslav@gmail.com>
2017-01-06 08:24:10 -0800

    Allow add social where collides w current user email

--- spec/features/add_social_auth_spec.rb
+++ spec/features/add_social_auth_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'Add social auth', js: true do
 
-  scenario "adding social and email collides with existing user's verified email" do
+  scenario "adding social and email collides with a different existing user's verified email" do
     create_email_address_for(create_user('other_user'), 'user@example.com')
 
     user = create_user 'user'
@@ -20,6 +20,27 @@ feature 'Add social auth', js: true do
       screenshot!
       expect_profile_page
       expect(page).to have_content("already in use")
+    end
+  end
+
+  scenario "adding social and email collides with the current user's verified email" do
+    user = create_user 'user'
+    create_email_address_for(user, 'user@example.com')
+
+    log_in('user', 'password')
+
+    expect_profile_page
+
+    click_link (t :"users.edit.enable_other_sign_in_options")
+    wait_for_animations
+    expect(page).to have_content('Facebook')
+
+    with_omniauth_test_mode(email: 'user@example.com') do
+      find('.authentication[data-provider="facebook"] .add').click
+      wait_for_ajax
+      expect_profile_page
+      expect(page).not_to have_content("already in use")
+      expect(page).to have_content('Facebook')
     end
   end
 
