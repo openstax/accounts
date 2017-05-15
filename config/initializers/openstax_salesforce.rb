49167fc526b2a7050b0b9f00e04c329af102a66a
JP Slavinsky <jpslav@gmail.com>
2017-05-15 15:25:41 -0700

    update openstax_salesforce to 0.12.0

--- config/initializers/openstax_salesforce.rb
+++ config/initializers/openstax_salesforce.rb
@@ -5,17 +5,13 @@ OpenStax::Salesforce.configure do |config|
   # Default: 'application'
   config.layout = 'admin'
 
-  # Proc called with a controller as self. Returns the current user.
-  # Default: lambda { current_user }
-  config.current_user_proc = -> { current_user }
-
-  # Proc called with a user as argument and a controller as self.
+  # Proc called with an argument of the controller where this is called.
   # This proc is called when a user tries to access the engine's controllers.
   # Should raise an exception, render or redirect unless the user is a manager
   # or admin. The default renders 403 Forbidden for all users.
-  # Note: Proc must account for nil users, if current_user_proc returns nil.
-  # Default: lambda { |user| head(:forbidden) }
-  config.authenticate_admin_proc = ->(user) { head(:forbidden) }
+  config.authenticate_admin_proc = ->(controller) {
+    controller.authenticate_admin!
+  }
 
   secrets = Rails.application.secrets[:salesforce]
 
@@ -34,5 +30,3 @@ OpenStax::Salesforce.configure do |config|
 
   config.page_heading_proc = ->(view, text) { view.content_for(:page_header, text) }
 end
-
-
