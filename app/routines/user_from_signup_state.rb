60c2bae4c18e07e1fadc37384e4b87334e7bf28f
Nathan Stitt <nathan@stitt.org>
2017-08-28 13:05:19 -0500

    save external uuid when building from trusted state

--- app/routines/user_from_signup_state.rb
+++ app/routines/user_from_signup_state.rb
@@ -1,6 +1,6 @@
 class UserFromSignupState
 
-  lev_routine
+  lev_routine express_output: :user
 
   protected
 
@@ -9,6 +9,9 @@ class UserFromSignupState
     if signup_state.trusted?
       user.role = signup_state.role
       user.full_name = signup_state.trusted_data['name']
+      if signup_state.trusted_data['uuid']
+        user.external_uuids.build(uuid: signup_state.trusted_data['uuid'])
+      end
     end
     user.save
     outputs.user = user
---
+++ spec/routines/user_from_signup_state_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe UserFromSignupState do
+
+  context 'an untrusted state' do
+    let(:signup_state) { FactoryGirl.create :signup_state }
+
+    it 'builds a user account' do
+      user = UserFromSignupState[signup_state]
+      expect(user).to be_valid
+      expect(user.external_uuids).to be_empty
+    end
+  end
+
+  context 'a trusted state' do
+    let(:signup_state) { FactoryGirl.create :signup_state, :trusted, role: 'instructor' }
+
+    it 'builds a user account with attributes set from tusted' do
+      user = UserFromSignupState[signup_state]
+      expect(user).to be_valid
+      expect(user.role).to eq('instructor')
+      expect(user.external_uuids.first.uuid).to eq(signup_state.trusted_data['uuid'])
+    end
+
+  end
+
+end
