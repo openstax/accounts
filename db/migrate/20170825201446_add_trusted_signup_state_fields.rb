a89da0156a2466148584dee8f5e46d688cde56a9
Nathan Stitt <nathan@stitt.org>
2017-08-31 16:05:34 -0500

    Allow trusted_data to be null
    we're already testing using `.presence?`

--- db/migrate/20170825201446_add_trusted_signup_state_fields.rb
+++ db/migrate/20170825201446_add_trusted_signup_state_fields.rb
@@ -1,6 +1,6 @@
 class AddTrustedSignupStateFields < ActiveRecord::Migration
   def change
-    add_column :signup_states, :trusted_data, :jsonb
-    add_column :users, :trusted_signup_data, :jsonb
+    add_column :signup_states, :trusted_data, :json
+    add_column :users, :trusted_signup_data, :json
   end
 end
