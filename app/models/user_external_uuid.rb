da2f18deabd8cb9097f80ba97e116071a1375b62
Nathan Stitt <nathan@stitt.org>
2017-09-13 11:14:30 -0500

    add uniqueness check on UUID

--- app/models/user_external_uuid.rb
+++ app/models/user_external_uuid.rb
@@ -2,5 +2,5 @@ class UserExternalUuid < ActiveRecord::Base
 
   belongs_to :user, inverse_of: :external_uuids
 
-  validates :uuid, :user, presence: true
+  validates :uuid, :user, presence: true, uniqueness: { scope: [:user_id, :uuid] }
 end
