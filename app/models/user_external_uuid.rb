3a31bef6c4543c9e2bafa5ec509d41038747e6a2
Nathan Stitt <nathan@stitt.org>
2017-09-13 11:36:22 -0500

    uuid should be globally unique

--- app/models/user_external_uuid.rb
+++ app/models/user_external_uuid.rb
@@ -2,5 +2,5 @@ class UserExternalUuid < ActiveRecord::Base
 
   belongs_to :user, inverse_of: :external_uuids
 
-  validates :uuid, :user, presence: true, uniqueness: { scope: [:user_id, :uuid] }
+  validates :uuid, :user, presence: true, uniqueness: true
 end
