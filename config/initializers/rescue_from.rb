a4efaaeb4d5a068cb60c0eb6c19978919410751c
Dante Soares <dante.m.soares@rice.edu>
2017-10-06 13:32:55 -0500

    Clarify variable names in rescue_from initializer

--- config/initializers/rescue_from.rb
+++ config/initializers/rescue_from.rb
@@ -1,15 +1,14 @@
 require 'openstax_rescue_from'
 
-secrets = Rails.application.secrets[:exception]
-
+exception_secrets = Rails.application.secrets.exception
 OpenStax::RescueFrom.configure do |config|
   # Show the default Rails exception debugging page on dev
   config.raise_exceptions = EnvUtilities.load_boolean(name: 'RAISE',
                                                       default: Rails.env.development?)
 
   config.app_name = 'Accounts'
-  config.app_env = secrets['environment_name']
-  config.contact_name = secrets['contact_name'].html_safe
+  config.app_env = exception_secrets['environment_name']
+  config.contact_name = exception_secrets['contact_name'].html_safe
 
   # config.notifier = ExceptionNotifier
 
@@ -17,8 +16,8 @@ OpenStax::RescueFrom.configure do |config|
   config.html_error_template_layout_name = 'error'
 
   # config.email_prefix = "[#{config.app_name}] (#{config.app_env}) "
-  config.sender_address = secrets['sender']
-  config.exception_recipients = secrets['recipients']
+  config.sender_address = exception_secrets['sender']
+  config.exception_recipients = exception_secrets['recipients']
 end
 
 OpenStax::RescueFrom.register_exception(
