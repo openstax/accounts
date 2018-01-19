356d9fee06959740e6258814129ba966c9590bfa
Nathan Stitt <nathan@stitt.org>
2018-01-19 12:36:08 -0600

    always show an error when email is in use

--- app/handlers/signup_start.rb
+++ app/handlers/signup_start.rb
@@ -19,18 +19,18 @@ class SignupStart
     end
     outputs.role = signup_params.role
 
+    # If email in use, want users to login with that email, not create another account
+    fatal_error(code: :email_in_use, offending_inputs: [:signup, :email]) if email_in_use?
+
     # is there a signup_state and it's email is unchanged
     if existing_signup_state.try(:contact_info_value) == email
       existing_signup_state.update_attributes(role: signup_params.role)
       outputs.signup_state = existing_signup_state
-      # signup_state may have beenn created in session start
+      # signup_state may have been created in session start
       # and the the confirmation email will not yet have been sent
       deliver_validation_email if existing_signup_state.confirmation_sent_at.nil?
       return
     end
-
-    # If email in use, want users to login with that email, not create another account
-    fatal_error(code: :email_in_use, offending_inputs: [:signup, :email]) if email_in_use?
 
     # Create a new one
     new_signup_state = SignupState.email_address.create(
