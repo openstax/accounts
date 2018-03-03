920acd681cdc08813813786ac3902015918620d0
Dante Soares <dante.m.soares@rice.edu>
2018-03-02 20:01:39 -0600

    Fix spec that wasn't working right

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -32,13 +32,28 @@ RSpec.describe SessionsController, type: :controller do
     context 'anything else' do
       it 'raises IllegalState' do
         expect(SessionsCreate).to receive(:handle).and_return(
-          Hashie::Mash.new(outputs: { status: :something_else }, errors: ['Some error'])
+          Hashie::Mash.new(
+            outputs: { status: :something_else },
+            errors: [code: :some_error, message: 'Some error']
+          )
         )
         expect{ post :create, provider: 'identity' }.to(
           change{ ActionMailer::Base.deliveries.count }.by(1)
         )
         expect(response).to have_http_status(:internal_server_error)
       end
+    end
+  end
+
+  context '#destroy' do
+    it 'redirects to caller-specified URL if in whitelist' do
+      delete :destroy, r: "https://something.openstax.org/howdy?blah=true"
+      expect(response).to redirect_to("https://something.openstax.org/howdy?blah=true")
+    end
+
+    it 'does not redirect to a caller-specified URL if not in whitelist' do
+      delete :destroy, r: "http://www.google.com"
+      expect(response).to redirect_to("/")
     end
   end
 
