55184858b6b10c0a0a5cb0ba6f196e8e6464522f
Michael Harrison <mwharrison@rice.edu>
2018-03-12 14:27:07 -0500

    Add spec for reports

---
+++ spec/controllers/admin/reports_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Admin::ReportsController, type: :controller do
+  let(:admin) { FactoryGirl.create :user, :admin, :terms_agreed }
+
+  before(:each) do
+    controller.sign_in! admin
+  end
+
+  it 'gets number of users' do
+    expect(User.count).to (eq 1)
+  end
+
+end
