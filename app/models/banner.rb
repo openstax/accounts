c11fac34e6b27d9765292405d2ca1a23927dae67
BryanEliDimas <bryanelidimas@gmail.com>
2018-07-31 11:43:33 -0500

    Timezone related fixes

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,10 +1,13 @@
 class Banner < ActiveRecord::Base
+  TIME_ZONE = 'Central Time (US & Canada)'
+
   scope :active, -> { where('expires_at > ?', DateTime.now) }
 
   validates :message, presence: true
   validates :expires_at, presence: true
 
   def expires_at=(expires_at)
-    super(expires_at).try(:in_time_zone, 'Central Time (US & Canada)')
+    with_time_zone = ActiveSupport::TimeZone[TIME_ZONE].parse(expires_at.to_s)
+    super(with_time_zone)
   end
 end
