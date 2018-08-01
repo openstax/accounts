bccf9ae6ccef026ac5d81a0360287b58ae965382
BryanEliDimas <bryanelidimas@gmail.com>
2018-08-01 17:20:41 -0500

    Test Banner model saves time in UTC

--- spec/models/banner_spec.rb
+++ spec/models/banner_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Banner, type: :model do
       expect(subject.active_until).to eq(expected)
     end
   end
+
+  describe "timezone" do
+    it "saves UTC in the database" do
+      central_time = 'Wed, 01 Aug 2018 17:11:52 -0500'
+      utc_time = 'Wed, 01 Aug 2018 22:11:52 UTC +00:00'
+      banner = Banner.create(message: 'wtvr', expires_at: central_time)
+      expect(Banner.pluck(:expires_at)).to eq([central_time])
+    end
+  end
 end
