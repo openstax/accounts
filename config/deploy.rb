52df82730d2163245bc1c5b98deed02141ea7462
Nathan Stitt <nathan@stitt.org>
2018-08-02 12:51:18 -0500

    update server address and role

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+set :passenger_restart_with_touch, true
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
