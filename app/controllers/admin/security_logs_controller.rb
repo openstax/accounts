c6967f1218773dec7f3c23c75e8d3502663e08ba
Dante Soares <dante.m.soares@rice.edu>
2018-08-16 15:50:49 -0500

    Fix a redirect loop in authenticate_admin! when already logged in

--- app/controllers/admin/security_logs_controller.rb
+++ app/controllers/admin/security_logs_controller.rb
@@ -6,6 +6,5 @@ module Admin
       items = SearchSecurityLog.call(params[:search] || {}).outputs.items || SecurityLog.none
       @security_log = items.paginate(page: params[:page], per_page: params[:per_page] || 20)
     end
-
   end
 end
