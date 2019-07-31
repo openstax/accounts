4080b727fc6a5f53e29a1c88ea071d20789cba56
BryanEliDimas <bryanelidimas@gmail.com>
2019-07-31 17:34:10 -0500

    WIP

--- config/initializers/sso_cookies.rb
+++ config/initializers/sso_cookies.rb
@@ -1,27 +1,60 @@
+# Creates a separate cookie jar on top of Rails' internal ones
+# ... magically because Rails doesn't really provide a way to do so.
 # https://github.com/rails/rails/blob/master/actionpack/lib/action_dispatch/middleware/cookies.rb
 
-Rails.application.configure do
-  config.action_dispatch.use_authenticated_cookie_encryption = false
-  config.action_dispatch.use_authenticated_message_encryption = false
-  config.active_support.use_authenticated_message_encryption = false
+# Rails.application.configure do
+#   config.action_dispatch.use_authenticated_cookie_encryption = false
+#   config.action_dispatch.use_authenticated_message_encryption = false
+#   config.active_support.use_authenticated_message_encryption = false
 
-  salt = Rails.application.secrets.sso[:shared_secret_salt] || 'cookie'
-  config.action_dispatch.encrypted_cookie_salt = salt
-  config.action_dispatch.encrypted_signed_cookie_salt = "signed encrypted #{salt}"
+#   salt = Rails.application.secrets.sso[:shared_secret_salt] || 'cookie'
+#   config.action_dispatch.encrypted_cookie_salt = salt
+#   config.action_dispatch.encrypted_signed_cookie_salt = "signed encrypted #{salt}"
 
-  config.action_dispatch.cookies_serializer = ActionDispatch::Cookies::JsonSerializer
-end
+#   config.action_dispatch.cookies_serializer = ActionDispatch::Cookies::JsonSerializer
+# end
+
+# Rails.application.config.action_dispatch.use_authenticated_cookie_encryption = false
+
+# Rails.application.config.active_support.use_authenticated_message_encryption = false
+
+Rails.application.config.action_dispatch.cookies_serializer = ActionDispatch::Cookies::JsonSerializer
+# Rails.application.config.action_dispatch.cookies_serializer = ActiveSupport::MessageEncryptor::NullSerializer
+Rails.application.config.action_dispatch.encrypted_cookie_cipher = 'aes-256-cbc'
+
+
+
+Rails.application.config.action_dispatch.use_authenticated_cookie_encryption = false
 
 class SsoEncryptedCookieJar < ActionDispatch::Cookies::EncryptedKeyRotatingCookieJar
   def initialize(parent_jar)
+    @parent_jar = parent_jar
+
     super
+
+    sso_shared_secret = Rails.application.secrets.sso[:shared_secret]
+    sso_shared_salt = Rails.application.secrets.sso[:shared_secret_salt]
+    sso_signed_salt = "signed encrypted #{sso_shared_salt}"
+
+    key_length = OpenSSL::Cipher.new('aes-256-cbc').key_len
+
+    sso_keygen = ActiveSupport::CachingKeyGenerator.new(
+      ActiveSupport::KeyGenerator.new(sso_shared_secret, iterations: 1000)
+    )
+    secret = sso_keygen.generate_key(sso_shared_salt)[0, key_length]
+    sign_secret = sso_keygen.generate_key(sso_signed_salt)
+
+    json_serializer = ActionDispatch::Cookies::JsonSerializer
+    sso_encryptor = ActiveSupport::MessageEncryptor.new(secret, sign_secret, serializer: json_serializer)
+
+    self.instance_variable_set(:@encryptor, sso_encryptor)
   end
 end
 
 # Provides a separate CookieJar for the SSO cookie, with a different shared_secret
 class SsoCookieJar < ActionDispatch::Cookies::CookieJar
   def encrypted
-    @encrypted ||= SsoEncryptedCookieJar.new(self)
+    @encrypted = SsoEncryptedCookieJar.new(self)
   end
 end
 
@@ -39,6 +72,11 @@ ActionDispatch::Request.class_exec do
      self.sso_cookie_jar = SsoCookieJar.build(self, cookies)
    end
   end
+
+  # def encrypted_cookie_cipher
+  # should probably set the Rails config instead
+  #   'aes-256-cbc'
+  # end
 end
 
 ActionDispatch::Cookies.class_exec do
--- spec/lib/sso_cookie_jar_spec.rb
+++ spec/lib/sso_cookie_jar_spec.rb
@@ -2,19 +2,34 @@ require 'rails_helper'
 
 RSpec.describe SsoCookieJar do
 
+  let(:json_serializer) {
+    ActionDispatch::Cookies::JsonSerializer
+  }
+
+  let(:sso_shared_secret) {
+    Rails.application.secrets.sso[:shared_secret]
+  }
+
+  let(:sso_shared_salt) {
+    Rails.application.secrets.sso[:shared_secret_salt] # || 'cookie'
+  }
+
   let(:key_generator) {
     ActiveSupport::CachingKeyGenerator.new(
-      ActiveSupport::KeyGenerator.new(Rails.application.secret_key_base, iterations: 1000)
+      ActiveSupport::KeyGenerator.new(sso_shared_secret, iterations: 1000)
     )
   }
 
   let(:request) {
     OpenStruct.new(
       key_generator: key_generator,
-      encrypted_cookie_salt: Rails.application.config.action_dispatch.encrypted_cookie_salt,
-      encrypted_signed_cookie_salt: Rails.application.config.action_dispatch.encrypted_signed_cookie_salt,
+      encrypted_cookie_salt: sso_shared_salt,
+      encrypted_signed_cookie_salt: "signed encrypted #{sso_shared_salt}",
       cookies_rotations: Rails.application.config.action_dispatch.cookies_rotations,
-      cookies_serializer: Rails.application.config.action_dispatch.cookies_serializer
+      cookies_serializer: json_serializer,
+      # use_authenticated_cookie_encryption: Rails.application.config.action_dispatch.use_authenticated_cookie_encryption,
+      use_authenticated_cookie_encryption: false,
+      secret_key_base: sso_shared_secret,
     )
   }
 
@@ -31,28 +46,48 @@ RSpec.describe SsoCookieJar do
   end
 
   it 'cookie jar can be decoded using key' do
+    # request['cookie_jar'] = sso_cookie_jar
     sso_cookie_jar = SsoCookieJar.build(request, cookies)
+
     sso_cookie_jar.encrypted['ox'] = {
       value: { 'test-answer' => 4242 }
     }
 
-    salt = Rails.application.secrets.sso[:shared_secret_salt]
-    # same as:
     # salt = request.encrypted_cookie_salt
+    # secret = request.key_generator.generate_key(salt)[0, OpenSSL::Cipher.new('aes-256-cbc').key_len]
 
-    signed_salt = request.encrypted_signed_cookie_salt
-    # same as:
-    # signed_salt = "signed encrypted #{salt}"
+    # signed_salt = request.encrypted_signed_cookie_salt
+    # sign_secret = request.key_generator.generate_key(signed_salt)
 
-    secret = request.key_generator.generate_key(salt)[0, OpenSSL::Cipher.new('aes-256-cbc').key_len]
-    sign_secret = request.key_generator.generate_key(signed_salt)
+    # encryptor = ActiveSupport::MessageEncryptor.new(
+    #   secret,
+    #   sign_secret,
+    #   cipher: 'aes-256-cbc',
+    #   serializer: ActiveSupport::MessageEncryptor::NullSerializer,
+    #   )
 
-    encryptor = ActiveSupport::MessageEncryptor.new(secret, sign_secret, serializer: request.cookies_serializer)
+
+    sso_shared_secret = Rails.application.secrets.sso[:shared_secret]
+    sso_shared_salt = Rails.application.secrets.sso[:shared_secret_salt]
+    sso_signed_salt = "signed encrypted #{sso_shared_salt}"
+
+    key_length = OpenSSL::Cipher.new('aes-256-cbc').key_len
+
+    sso_keygen = ActiveSupport::CachingKeyGenerator.new(
+      ActiveSupport::KeyGenerator.new(sso_shared_secret, iterations: 1000)
+    )
+    secret = sso_keygen.generate_key(sso_shared_salt)[0, key_length]
+    sign_secret = sso_keygen.generate_key(sso_signed_salt)
+
+    json_serializer = ActionDispatch::Cookies::JsonSerializer
+    sso_encryptor = ActiveSupport::MessageEncryptor.new(secret, sign_secret, serializer: json_serializer)
 
     cookie = sso_cookie_jar['ox']
 
     expect(
-      encryptor.decrypt_and_verify(cookie)
-    ).to eq({ 'test-answer' => 4242 })
+      sso_encryptor.decrypt_and_verify(cookie)
+    ).to eq(
+      { 'test-answer' => 4242 }.to_json
+    )
   end
 end
