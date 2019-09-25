8526e86f29a2d80e87c370cefedc6b7130643a0e
Ross Reedstrom <reedstrm@gmail.com>
2019-09-25 10:49:46 -0500

    fix the spec too

--- spec/config/initializers/session_store_cookie_name_spec.rb
+++ spec/config/initializers/session_store_cookie_name_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe SessionStoreCookieName do
   it 'works for production' do
     allow(Rails.application.secrets).to receive(:environment_name) { "prodtutor" }
-    expect(described_class.to_s).to eq "_accounts_session"
+    expect(described_class.to_s).to eq "_accounts_session_prodtutor"
   end
 
   it 'works for non production' do
