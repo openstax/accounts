c9f854462b818047229bbd5511397dd22c52fbf2
BryanEliDimas <bryanelidimas@gmail.com>
2019-10-25 16:11:36 -0500

    Add more error states to the login form

--- app/handlers/authenticate_user.rb
+++ app/handlers/authenticate_user.rb
@@ -2,10 +2,10 @@ class AuthenticateUser
     lev_handler
 
     paramify :login do
-        attribute :username_or_email, type: String
+        attribute :email, type: String
         attribute :password, type: String
 
-        validates :username_or_email, presence: { message: "Email #{I18n.t(:'login_signup_form.cant_be_blank')}" }
+        validates :email, presence: true
         validates :password, presence: true
     end
 
@@ -16,25 +16,41 @@ protected
     end
 
     def handle
-        user = LookupUsers.by_email_or_username(login_params.username_or_email).first
+        outputs.email = login_params.email
 
+        # We should be searching by email,
+        #   but we'd like to continue to support users who only have a username.
+        users = LookupUsers.by_email_or_username(login_params.email)
+
+        if users.size == 0
+            fail_with_log!('cannot_find_user', :email)
+        elsif users.size > 1
+            fail_with_log!('multiple_users', :email) # should user really be nil? why not the email value?
+        end
+
+        user = users.first
         identity = Identity.authenticate({ user_id: user&.id }, login_params.password)
-        unless identity
-            fatal_error(
-                code: :invalid_auth_credentials,
-                offending_inputs: [:username_or_email, :password],
-                message: I18n.t(:"login_signup_form.invalid_login_credentials")
-            )
+        unless identity.present?
+            fail_with_log!('incorrect_password', :password, user)
+            # TODO: MAYBE add the error state (like in custom_identity):
+            # :bad_reauthenticate_password
         end
 
         outputs.user = user
+    end
 
-=begin
-TODO: add the following error states, like in `custom_identity`:
-    :cannot_find_user
-    :multiple_users
-    :bad_authenticate_password
-    :bad_reauthenticate_password
-=end
+    def fail_with_log!(reason, input_field, user = nil)
+        SecurityLog.create!(
+            event_type: :sign_in_failed,
+            event_data: { reason: reason },
+            user: user,
+            remote_ip: request.ip,
+        )
+
+        fatal_error(
+            code: reason.to_sym,
+            offending_inputs: input_field,
+            message: I18n.t("login_signup_form.#{reason}".to_sym)
+        )
     end
 end
