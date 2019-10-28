a20760d391dfd1ccf15b029209364ede54184a79
BryanEliDimas <bryanelidimas@gmail.com>
2019-10-28 15:52:40 -0500

    Add the ability to sign up using facebook

--- app/handlers/newflow_student_signup.rb
+++ app/handlers/newflow_student_signup.rb
@@ -26,32 +26,37 @@ class NewflowStudentSignup
   end
 
   def handle
-    outputs.pre_auth_state = create_pre_auth_state
-
-    user = User.create(
-      first_name: signup_params.first_name.camelize,
-      last_name: signup_params.last_name.camelize,
-      state: 'unclaimed',
-      role: 'student'
-    )
-    transfer_errors_from(user, { type: :verbatim }, fail_if_errors=true)
-
-    # Agree to terms
-    if options[:contracts_required] && signup_params.terms_accepted
-      run(AgreeToTerms, profile_params.contract_1_id, user, no_error_if_already_signed: true)
-      run(AgreeToTerms, profile_params.contract_2_id, user, no_error_if_already_signed: true)
-    end
-
-    email = EmailAddress.new(
-      value: signup_params.email, user_id: user.id,
-      confirmation_pin: outputs.pre_auth_state.confirmation_pin
-    )
-    email.save!
+    create_pre_auth_state
+    create_user
+    create_authentication
+    create_identity
+    agree_to_terms
+    create_email_address
     send_confirmation_email
   end
 
+  #####################################################################
+
+  def create_authentication
+    Authentication.create(
+      provider: 'identity',
+      # because of the way that user signup used to work in the old flow,
+      # `user_id` and `uid` are both required and the same.
+       user_id: outputs.user.id, uid: outputs.user.id
+    )
+    # TODO: catch errorr states like - if auth already exists for this user
+  end
+
+  def create_identity
+    Identity.create(
+      password: signup_params.password,
+      password_confirmation: signup_params.password,
+      user: outputs.user
+    )
+  end
+
   def create_pre_auth_state
-    PreAuthState.email_address.create(
+    outputs.pre_auth_state = PreAuthState.email_address.create(
       is_partial_info_allowed: true,
       contact_info_value: signup_params.email,
       first_name: signup_params.first_name.camelize,
@@ -59,6 +64,33 @@ class NewflowStudentSignup
       # signed_data: existing_pre_auth_state.try!(:signed_data),
       # return_to: options[:return_to]
     )
+  end
+
+  def create_user
+    outputs.user = User.create(
+      first_name: signup_params.first_name.camelize,
+      last_name: signup_params.last_name.camelize,
+      state: 'unclaimed',
+      role: 'student'
+    )
+    transfer_errors_from(outputs.user, { type: :verbatim }, fail_if_errors=true)
+  end
+
+  def agree_to_terms
+    if options[:contracts_required] && signup_params.terms_accepted
+      run(AgreeToTerms, signup_params.contract_1_id, outputs.user, no_error_if_already_signed: true)
+      run(AgreeToTerms, signup_params.contract_2_id, outputs.user, no_error_if_already_signed: true)
+    end
+    # fatal_error(code: :TODO, message)
+  end
+
+  def create_email_address
+    email = EmailAddress.new(
+      value: signup_params.email, user_id: outputs.user.id,
+      confirmation_pin: outputs.pre_auth_state.confirmation_pin
+    )
+    email.save
+    transfer_errors_from(email, { type: :verbatim }, fail_if_errors=true)
   end
 
   def send_confirmation_email
