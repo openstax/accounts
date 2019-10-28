7271687a8ac44c7f8d324c3c6620e3f9b858e944
BryanEliDimas <bryanelidimas@gmail.com>
2019-10-28 13:22:48 -0500

    Add the ability to show/hide password

--- app/assets/javascripts/newflow.js
+++ app/assets/javascripts/newflow.js
@@ -1,5 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
-//= require ./application/
-//= require ./signup/
+//= require ./application
+//= require ./signup
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+
+$(document).ready(function () {
+    // Toggle show/hide password field
+    $("#show-hide-button").click(function (e) {
+        $(".toggle-show-hide").toggle();
+
+        var password_field = "input#login_form_password";
+        if ($(password_field).attr("type") == "password") {
+            $(password_field).attr("type", "text");
+        } else {
+            $(password_field).attr("type", "password");
+        }
+    });
+});
