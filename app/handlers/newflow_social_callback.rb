5b77987cae3b9cda1f1e8eb3c4924d45f426f327
BryanEliDimas <bryanelidimas@gmail.com>
2019-10-28 15:20:43 -0500

    Existing users can log in with facebook

--- app/handlers/newflow_social_callback.rb
+++ app/handlers/newflow_social_callback.rb
@@ -2,24 +2,40 @@
 # Handles the omniauth callback.
 #
 class NewflowSocialCallback
+  include Rails.application.routes.url_helpers
+
   lev_handler
 
-  protected
+  protected #######################
 
   def authorized?
     true
   end
 
   def setup
+    @came_from = URI.parse(request.env['omniauth.origin']).path
     @data = OmniauthData.new(request.env['omniauth.auth']) \
                         rescue fatal_error(code: :invalid_omniauth_data)
   end
 
   def handle
+    # TODO: might want to undo the following line when we deploy to production
+    social_provider = @data.provider == 'facebooknewflow' ? 'facebook' : @data.provider
+
+    if logging_in?
+      authentication = Authentication.find_by(
+        provider: social_provider, uid: @data.uid.to_s
+      )
+      outputs[:user] = authentication.user
     outputs[:user] = authentication.user
+    end
   end
 
-#####################
+  private #######################
+
+  def logging_in?
+    @came_from == newflow_login_path
+  end
 
   def authentication
     # We don't use fatal_errors in this handler (we could, but we don't), so just build
