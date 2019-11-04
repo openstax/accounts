0fdebb3d408ab4dca5b392f24c2b9b3342d71321
BryanEliDimas <bryanelidimas@gmail.com>
2019-11-04 10:50:13 -0600

    Lint login flow rspec file

--- spec/support/newflow_feature_helpers.rb
+++ spec/support/newflow_feature_helpers.rb
@@ -1,14 +1,17 @@
-def create_newflow_user(username, password='password', terms_agreed=nil, provider='identity')
+def create_newflow_user(email, password='password', terms_agreed=nil, confirmation_code=nil)
   terms_agreed_option = (terms_agreed.nil? || terms_agreed) ?
                           :terms_agreed :
                           :terms_not_agreed
 
-  return if User.find_by_username(username).present?
+  # return if User.find_by_username(username).present?
 
-  user = FactoryBot.create :user, terms_agreed_option, username: username
+  user = FactoryBot.create :user, terms_agreed_option
+  FactoryBot.create(:email_address, user: user, value: email,
+                     confirmation_code: confirmation_code,
+                     verified: confirmation_code.nil?)
   identity = FactoryBot.create :identity, user: user, password: password
   authentication = FactoryBot.create :authentication, user: user,
-                                                       provider: provider,
+                                                       provider: 'identity',
                                                        uid: identity.uid
   return user
 end
