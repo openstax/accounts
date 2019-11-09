1a0b3a8ae5ff7142f26802b9d7f71785806c56b7
BryanEliDimas <bryanelidimas@gmail.com>
2019-11-08 18:08:24 -0600

    Lint app/controllers/login_signup_controller.rb

--- app/controllers/login_signup_controller.rb
+++ app/controllers/login_signup_controller.rb
@@ -3,7 +3,8 @@ class LoginSignupController < ApplicationController
   layout 'newflow_layout'
   skip_before_action :authenticate_user!, except: [:profile_newflow]
   skip_before_action :check_if_password_expired
-  fine_print_skip :general_terms_of_use, :privacy_policy
+  fine_print_skip :general_terms_of_use, :privacy_policy,
+                  except: [:profile_newflow, :verify_pin, :signup_done]
 
   def login_form
     # Send to profile upon login unless in the middle of authorizing an oauth app
@@ -13,40 +14,38 @@ class LoginSignupController < ApplicationController
 
   def login
     handle_with(AuthenticateUser,
-      success: -> {
-        sign_in!(@handler_result.outputs.user)
-        redirect_back(fallback_location: profile_newflow_url)
-      },
-      failure: -> {
-        security_log :login_not_found, tried: @handler_result.outputs.email
-        render :login_form
-      }
-    )
+                success: lambda {
+                  sign_in!(@handler_result.outputs.user)
+                  redirect_back(fallback_location: profile_newflow_url)
+                },
+                failure: lambda {
+                  security_log :login_not_found, tried: @handler_result.outputs.email
+                  render :login_form
+                })
   end
 
   def signup
     handle_with(StudentSignup,
-      contracts_required: !contracts_not_required,
-      success: -> {
-        # clear_login_state
-        save_pre_auth_state(@handler_result.outputs.pre_auth_state)
-        redirect_to confirmation_form_path
-      }, failure: -> {
-        render :signup_form
-      }
-    )
+                contracts_required: !contracts_not_required,
+                success: lambda {
+                  # clear_login_state
+                  save_pre_auth_state(@handler_result.outputs.pre_auth_state)
+                  redirect_to confirmation_form_path
+                }, failure: lambda {
+                  render :signup_form
+                })
   end
 
   # Log in (or sign up and then log in) a user using a social (OAuth) provider
   def oauth_callback
     handle_with(
       OauthCallback,
-      success: -> {
+      success: lambda {
         sign_in!(@handler_result.outputs.user)
         # redirect_to profile_newflow_path
         redirect_back(fallback_location: profile_newflow_path)
       },
-      failure: -> {
+      failure: lambda {
         redirect_to newflow_social_login_failed_path
       }
     )
@@ -54,13 +53,12 @@ class LoginSignupController < ApplicationController
 
   def verify_pin
     handle_with(NewflowVerifyEmail,
-      success: -> {
-        sign_in!(@handler_result.outputs.user)
-        redirect_to signup_done_path
-      }, failure: -> {
-        render :confirmation_form
-      }
-    )
+                success: lambda {
+                  sign_in!(@handler_result.outputs.user)
+                  redirect_to signup_done_path
+                }, failure: lambda {
+                  render :confirmation_form
+                })
   end
 
   # TODO: require a PreAuthState present in the session OR  a logged in user.
