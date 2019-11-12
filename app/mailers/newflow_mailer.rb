edddb3db74ac4c4c6e6a6d29de6212d02ab32860
BryanEliDimas <bryanelidimas@gmail.com>
2019-11-12 17:21:02 -0600

    Don't use PreAuthState in the newflow anymore

--- app/mailers/newflow_mailer.rb
+++ app/mailers/newflow_mailer.rb
@@ -1,8 +1,25 @@
 class NewflowMailer < ApplicationMailer
-
   def newflow_setup_password(user:, email:)
     # @show_pin = ConfirmByPin.sequential_failure_for().attempts_remaining?
     @user = user
-    mail to: email, subject: "Set up a password for your OpenStax account"
+    mail to: email, subject: 'Set up a password for your OpenStax account'
+  end
+
+  def signup_email_confirmation(email_address:)
+    @should_show_pin = ConfirmByPin.sequential_failure_for(email_address).attempts_remaining?
+    @email_value = email_address.value
+    @confirmation_pin = email_address.confirmation_pin
+    @confirmation_code = email_address.confirmation_code
+    # TODO: create my own newflow url
+    @confirmation_url = signup_verify_by_token_url(code: @confirmation_code)
+
+    mail to: @email_value,
+         subject: if @should_show_pin
+                    "Use PIN #{@confirmation_pin} to confirm your email address"
+                  else
+                    'Confirm your email address'
+                  end
+
+    email_address.update_column(:confirmation_sent_at, Time.now)
   end
 end
