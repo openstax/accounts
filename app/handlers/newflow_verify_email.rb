edc870a59d0457c62680dbcf89777e9685c2840a
BryanEliDimas <bryanelidimas@gmail.com>
2019-11-13 22:28:10 -0600

    Use ConfirmByPin in NewflowVerifyEmail

--- app/handlers/newflow_verify_email.rb
+++ app/handlers/newflow_verify_email.rb
@@ -1,5 +1,6 @@
 class NewflowVerifyEmail
   lev_handler
+  uses_routine ConfirmByPin
 
   paramify :confirm do
     attribute :pin, type: String
@@ -13,19 +14,18 @@ class NewflowVerifyEmail
 
   # TODO: rate-limit this action here (or in the controller?)
   def handle
-    email = EmailAddress.where(confirmation_pin: confirm_params.pin, verified: false).first
-    # OR get the email from the user stored in the session[:unverified_user]
-    # OR check both and make sure that the emails are the same? probably not necessary.
-    if email
-      email.update_attributes(verified: true)
-      email.user.update_attributes(state: 'activated')
-      outputs.user = email.user
-    else
+    result = ConfirmByPin.call(contact_info: options[:email_address], pin: confirm_params.pin)
+    if result.errors.any?
       fatal_error(
         code: :invalid_confirmation_pin,
         offending_inputs: [:pin],
         message: I18n.t(:"login_signup_form.invalid_confirmation_pin")
       )
     end
+
+    claiming_user = options[:email_address].user
+    claiming_user.update(state: 'activated')
+
+    outputs.user = claiming_user
   end
 end
