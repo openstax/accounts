7136cb7ba0a961ac0a4fabe91fedfb6b9c730f8a
BryanEliDimas <bryanelidimas@gmail.com>
2019-11-14 14:40:54 -0600

    Fix tests, remove unreasonable ones

--- app/handlers/change_signup_email.rb
+++ app/handlers/change_signup_email.rb
@@ -22,7 +22,7 @@ class ChangeSignupEmail
 
     @email_address = EmailAddress.where(user_id: options[:user].id).first
     @email_address.value = email_param
-    @email_address.reset_pin_code
+    @email_address.set_confirmation_pin_code
     @email_address.save
     transfer_errors_from(@email_address, { scope: :email }, :fail_if_errors)
 
