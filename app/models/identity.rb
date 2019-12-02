29a1dc2c6bc950b4102ceb9df69407a1bd97c8d4
BryanEliDimas <bryanelidimas@gmail.com>
2019-12-02 15:34:19 -0600

    Add Identity validation MIN_PASSWORD_LENGTH

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -10,7 +10,7 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
   # We need these validations because
   # omniauth-identity does not provide them by default
   # These attributes are (obviously) not saved in the database
-  validates :password, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: MIN_PASSWORD_LENGTH, maximum: 50 }
   validates :user, presence: true
   validates :user_id, uniqueness: true
 
