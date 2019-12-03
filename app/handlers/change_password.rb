c881338505036dc249ecb2a9ea4f86918c8a2f39
BryanEliDimas <bryanelidimas@gmail.com>
2019-12-03 14:03:54 -0600

    Refactor into new student flow into newflow module

--- app/handlers/change_password.rb
+++ app/handlers/change_password.rb
@@ -1,27 +1,29 @@
-class ChangePassword
-  lev_handler
+module Newflow
+  class ChangePassword
+    lev_handler
 
-  LOGIN_TOKEN_EXPIRES_AFTER = 2.days
+    LOGIN_TOKEN_EXPIRES_AFTER = 2.days
 
-  paramify :change_password_form do
-    attribute :password, type: String
-    validates :password, presence: true
-  end
+    paramify :change_password_form do
+      attribute :password, type: String
+      validates :password, presence: true
+    end
 
-  protected #################
+    protected #################
 
-  def authorized?
-    true
-  end
+    def authorized?
+      true
+    end
 
-  def handle
-    user = options[:user]
-    identity = user.identity || user.build_identity
-    identity.password = change_password_form_params.password
-    identity.password_confirmation = change_password_form_params.password
+    def handle
+      user = options[:user]
+      identity = user.identity || user.build_identity
+      identity.password = change_password_form_params.password
+      identity.password_confirmation = change_password_form_params.password
 
-    identity.save
+      identity.save
 
-    transfer_errors_from(identity, { scope: :password }, :fail_if_errors)
+      transfer_errors_from(identity, { scope: :password }, :fail_if_errors)
+    end
   end
 end
---
+++ app/handlers/newflow/reset_password_form.rb
@@ -0,0 +1,38 @@
+module Newflow
+  # If a user with the given email address is found, we send (to each of their verified
+  # email addresses) an email to reset their password.
+  # Otherwise, for security reasons, it returns early with no errors.
+  class ResetPasswordForm
+    lev_handler
+
+    LOGIN_TOKEN_EXPIRATION = 2.days
+
+    paramify :reset_password_form do
+      attribute :email
+      validates :email, presence: true
+    end
+
+    protected #################
+
+    def authorized?
+      true
+    end
+
+    def handle
+      outputs.email = reset_password_form_params.email
+      user = LookupUsers.by_verified_email(outputs.email).first
+      outputs.user = user
+
+      return unless user.present?
+
+      user.refresh_login_token(expiration_period: LOGIN_TOKEN_EXPIRATION)
+      user.save!
+
+      email_addresses = user.email_addresses.verified.map(&:value)
+
+      email_addresses.each do |email_address|
+        NewflowMailer.reset_password_email(user: user, email_address: email_address).deliver_later
+      end
+    end
+  end
+end
