f2b3cb7e236e7e62796a2a68d44007a5b929dbeb
BryanEliDimas <bryanelidimas@gmail.com>
2019-12-06 10:17:17 -0600

    Test the login flow

--- spec/features/newflow/student_login_flow_spec.rb
+++ spec/features/newflow/student_login_flow_spec.rb
@@ -57,18 +57,17 @@ feature 'student login flow', js: true do
         expect(find('#login_form_password')['type']).to eq('password')
       end
     end
-
   end
 
   context 'no user found with such email' do
     it 'adds a message to the email input field' do
       with_forgery_protection do
-          visit newflow_login_path
-          newflow_log_in_user('NOone@openstax.org', 'password')
-          expect(page.current_url).to match(newflow_login_path)
-          field_text = find('#login_form_email + .errors .invalid-message').text
-          expect(field_text).to  eq(I18n.t(:"login_signup_form.cannot_find_user"))
-        end
+        visit newflow_login_path
+        newflow_log_in_user('NOone@openstax.org', 'password')
+        expect(page.current_url).to match(newflow_login_path)
+        field_text = find('#login_form_email + .errors .invalid-message').text
+        expect(field_text).to  eq(I18n.t(:"login_signup_form.cannot_find_user"))
+      end
     end
   end
 
@@ -81,6 +80,46 @@ feature 'student login flow', js: true do
           field_text = find('#login_form_password + .errors .invalid-message').text
           expect(field_text).to  eq(I18n.t(:"login_signup_form.incorrect_password"))
         end
+    end
+  end
+
+  context 'forgot password' do
+    it 'enables the user to reset their password' do
+      with_forgery_protection do
+        visit newflow_login_path
+        screenshot!
+        newflow_log_in_user('user@openstax.org', 'WRONGpassword')
+        screenshot!
+
+        click_on(I18n.t(:"login_signup_form.forgot_password"))
+        # pre-populates the email for them since they already typed it in the login form
+        expect(find('#reset_password_form_email')['value']).to  eq('user@openstax.org')
+        screenshot!
+        click_on(I18n.t(:"login_signup_form.reset_my_password_button"))
+        screenshot!
+
+        expect(page).to have_content(I18n.t(:"login_signup_form.password_reset_email_sent"))
+        screenshot!
+
+        open_email('user@openstax.org')
+        capture_email!
+        change_password_link = get_path_from_absolute_link(current_email, 'a')
+        expect(change_password_link).to include(change_password_form_path)
+
+        # set the new password
+        visit change_password_link
+        expect(page).to have_content(I18n.t(:"login_signup_form.enter_new_password_description"))
+        fill_in('change_password_form_password', with: 'NEWpassword')
+        screenshot!
+        click_button('Log in')
+        screenshot!
+
+        # user is subsequently able to log in with the new password
+        click_on('Log out')
+        screenshot!
+        newflow_log_in_user('user@openstax.org', 'NEWpassword')
+        expect(page).to  have_content('My Account')
+      end
     end
   end
 
