9fd795832b3e0a075a2c5162bc456add718787a2
BryanEliDimas <bryanelidimas@gmail.com>
2019-12-16 11:26:49 -0600

    Update schema version in schema.rb after rebase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_194244) do
+ActiveRecord::Schema.define(version: 2019_12_04_193412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
