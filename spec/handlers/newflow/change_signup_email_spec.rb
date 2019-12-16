971efca90b9169f50952b99d352d6637b7939545
BryanEliDimas <bryanelidimas@gmail.com>
2019-12-16 16:21:54 -0600

    Update ChangeSignupEmail spec now that we dont use preauthstate

--- spec/handlers/newflow/change_signup_email_spec.rb
+++ spec/handlers/newflow/change_signup_email_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 module Newflow
   RSpec.describe ChangeSignupEmail, type: :handler do
+    before do
+      FactoryBot.create :email_address, value: before_change_email, verified: false, user: user
+    end
+
     let(:user) { FactoryBot.create :user }
+
     let(:before_change_email) {  'original@example.com' }
 
     let(:params) {
@@ -13,42 +18,25 @@ module Newflow
       }
     }
 
-    before do
-      FactoryBot.create :email_address, value: before_change_email, verified: false, user: user
-    end
-
     context 'when success' do
       it 'changes the user\'s only email address' do
         new_email = params.dig(:change_signup_email, :email)
+
+        expect(user.email_addresses.first.value).not_to eq new_email
 
         described_class.call(params: params, user: user)
         expect(user.email_addresses.first.value).to eq new_email
       end
 
-      it 'changes the PreAuthState\'s email address' do
-        new_email = params.dig(:change_signup_email, :email)
-
-        described_class.call(params: params, user: user)
-        expect(new_email).not_to eq before_change_email
-      end
-
-      it 'deletes the previous email address, creates a new one' do
+      it 'does not create another email address record' do
         expect {
           described_class.call(params: params, user: user)
         }.not_to change(EmailAddress, :count)
       end
 
-      it 'updates the PreAuthState\'s contact_info_value' do
-        new_email = params.dig(:change_signup_email, :email)
-
-        described_class.call(params: params, user: user)
-        user.reload
-        expect(user.email_addresses.first.value).to eq new_email
-      end
-
       it 'sends an email to the new email address' do
         expect_any_instance_of(NewflowMailer).to(
-          receive(:signup_email_confirmation).and_call_original
+          receive(:signup_email_confirmation).once.and_call_original
         )
         described_class.call(params: params, user: user)
       end
