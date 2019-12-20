f8198121985ca46196a3b2f50dc7af9d277851a4
BryanEliDimas <bryanelidimas@gmail.com>
2019-12-20 10:35:37 -0600

    create profile screen feature spec file

---
+++ spec/features/newflow/profile_screen_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature 'profile screen', js: true do
+ describe 'resetting your password' do
+
+ end
+
+ describe 'adding a password (when you only have a social login ATM' do
+
+ end
+
+ describe 'adding an email address' do
+
+ end
+
+
+  describe 'adding a social login' do
+
+  end
+
+  describe 'deleting your password' do
+
+  end
+
+  describe 'editing your email' do
+
+  end
+
+  describe 'editing your name' do
+
+  end
+
+  describe 'log out' do
+
+  end
+
+end
