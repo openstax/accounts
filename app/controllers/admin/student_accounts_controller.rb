c1599c73d0ee264900620ee25eb42f9d35eccc7f
Ed Woodward <ecw1@rice.edu>
2020-01-06 09:58:37 -0600

    Adding report on number of student accounts since July

---
+++ app/controllers/admin/student_accounts_controller.rb
@@ -0,0 +1,38 @@
+require 'date'
+
+module Admin
+    class StudentAccountsController < BaseController
+      layout 'admin'
+  
+      def show
+      end
+  
+      def get_number_of_accounts
+        #UpdateUserSalesforceInfo.call(allow_error_email: true)
+        days_num = get_number_of_days
+        accounts_num = query(days_num)
+        flash[:notice] = "The number of student acccounts created since July 1 is " + accounts_num.to_s
+        #redirect_to actions_admin_salesforce_path
+      end
+
+      def get_number_of_days
+        # number of days since July 1
+        today = Date.new
+        year = today.year
+        month = today.month
+        if month < 6
+            year = year - 1
+        end
+        #July is a keyword, so using month7
+        month7 = Date.parse year.inspect + "-07-01"
+        return (month7...today).count
+      end
+
+      def query(days_num)
+        role_type = :student
+        user_ids = User.where(role: role_type)
+        return SecurityLog.where(event_type: :sign_up_successful, user_id: user_ids).where('created_at > ?', (days_num.inspect).days.ago).count
+      end
+  
+    end
+  end
