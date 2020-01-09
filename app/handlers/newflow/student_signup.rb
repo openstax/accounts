7c52fc5ddf6ecc7984093c734a991a38e3ef39f0
BryanEliDimas <bryanelidimas@gmail.com>
2020-01-09 12:45:23 -0600

    Translate invalid_email_provider msg

--- app/handlers/newflow/student_signup.rb
+++ app/handlers/newflow/student_signup.rb
@@ -87,7 +87,7 @@ module Newflow
       # Customize the error message about having an invalid email domain
       if @email.errors && @email.errors.types.fetch(:value, {}).include?(:missing_mx_records)
         domain = @email.send(:domain)
-        @email.errors.messages[:value][0] = "\"#{domain}\" is not a valid email provider"
+        @email.errors.messages[:value][0] = I18n.t(:"login_signup_form.invalid_email_provider", domain: domain)
         transfer_errors_from(@email, { scope: :email }, :fail_if_errors)
       end
     end
