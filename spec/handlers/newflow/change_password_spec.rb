4ac9dbda98f94d9aa0b40ebdaa637e1c983181bd
BryanEliDimas <bryanelidimas@gmail.com>
2020-01-17 12:37:54 -0600

    Test ChangePassword handler

--- spec/handlers/newflow/change_password_spec.rb
+++ spec/handlers/newflow/change_password_spec.rb
@@ -2,6 +2,50 @@ require 'rails_helper'
 
 module Newflow
   describe ChangePassword, type: :handler do
-    # TODO
+    let(:user) do
+      FactoryBot.create(:user)
+    end
+
+    context 'success' do
+      before do
+        FactoryBot.create(:identity, user: user, password: 'password')
+      end
+
+      let(:params) do
+        {
+          change_password_form: {
+            password: 'newpassword',
+          }
+        }
+      end
+
+      it "changes the user's identity's password" do
+        described_class.call(user: user, params: params)
+        expect(user.identity.password).to eq('newpassword')
+      end
+    end
+
+    context 'failure' do
+    let(:params) do
+      {
+        change_password_form: {
+          password: 'pwd',
+        }
+      }
+    end
+
+      it 'fails if user does not have a password to begin with' do
+        expect {
+          described_class.call(user: user, params: params).errors
+        }.to  raise_error(Lev::SecurityTransgression)
+      end
+
+      it 'fails if password is too short' do
+        # create the password to begin with
+        FactoryBot.create(:identity, user: user, password: 'password')
+
+        expect(described_class.call(user: user, params: params).errors.any?).to be(true)
+      end
+    end
   end
 end
