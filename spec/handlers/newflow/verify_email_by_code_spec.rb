f728638717bbe03efe436c306d6d657b54d36a0a
BryanEliDimas <bryanelidimas@gmail.com>
2020-01-21 15:50:47 -0600

    Test VerifyEmailByCode, update accordingly

--- spec/handlers/newflow/verify_email_by_code_spec.rb
+++ spec/handlers/newflow/verify_email_by_code_spec.rb
@@ -2,6 +2,41 @@ require 'rails_helper'
 
 module Newflow
   describe VerifyEmailByCode, type: :handler do
+    context 'when success' do
+      let(:params) do
+        { code: email.confirmation_code }
+      end
 
+      let(:email) do
+        FactoryBot.create(:email_address, user: user)
+      end
+
+      let(:user) do
+        FactoryBot.create(:user, state: 'unverified')
+      end
+
+      it 'verifies the email address found by the given code' do
+        expect(email.verified).to be(false)
+        described_class.call(params: params)
+        email.reload
+        expect(email.verified).to be(true)
+      end
+
+      it 'activates the user' do
+        expect_any_instance_of(ActivateUser).to receive(:call).and_call_original
+        described_class.call(params: params)
+        user.reload
+        expect(user.state).to eq('activated')
+      end
+
+      it 'outputs the user' do
+        result = described_class.call(params: params)
+        expect(result.outputs.user).to eq(user)
+      end
+    end
+
+    context 'when failure' do
+      # TODO
+    end
   end
 end
