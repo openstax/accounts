16ff23cc4879fb9c54a3f4049c4f7980dbb6d508
Ed Woodward <ecw1@rice.edu>
2020-01-29 11:21:12 -0600

    Made requested changes

--- app/controllers/admin/student_accounts_controller.rb
+++ app/controllers/admin/student_accounts_controller.rb
@@ -8,29 +8,27 @@ module Admin
       end
   
       def get_number_of_accounts
-        days_num = get_number_of_days
-        accounts_num = query(days_num)
-        flash[:notice] = "The number of student acccounts created since July 1 is " + accounts_num.to_s
+        year = get_year
+        num_of_accounts = query(year)
+        flash[:notice] = "The number of student acccounts created since July 1 is #{num_of_accounts}"
         redirect_to actions_admin_student_accounts_path
       end
 
-      def get_number_of_days
-        # number of days since July 1
-        today = Date.new
+      def get_year
+        # correct year for July 1
+        today = DateTime.now
         year = today.year
         month = today.month
-        if month < 6
+        if month < 7
             year = year - 1
         end
-        #July is a keyword, so using month7
-        month7 = Date.parse year.inspect + "-07-01"
-        return (month7...today).count
+        return year
       end
 
-      def query(days_num)
+      def query(year)
         role_type = :student
         user_ids = User.where(role: role_type)
-        return SecurityLog.where(event_type: :sign_up_successful, user_id: user_ids).where('created_at > ?', days_num.days.ago).count
+        return SecurityLog.where(event_type: :sign_up_successful, user_id: user_ids).where('created_at >= ?', Date.parse(year.inspect + '-07-01')).count
       end
   
     end
