bc2e3f34b84df502cf74b7d62b6b031b52983d94
BryanEliDimas <bryanelidimas@gmail.com>
2020-01-29 13:31:14 -0600

    Create entry_params_spec.rb

---
+++ spec/features/newflow/entry_params_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+feature "Params given on entry", js: true do
+
+  context "go=signup" do
+    scenario "arriving from app" do
+      arrive_from_app(params: {go: "signup"}, do_expect: false)
+      expect_sign_up_page
+    end
+
+    scenario "straight to login" do
+      visit 'i/login?go=signup'
+      expect_sign_up_page
+    end
+  end
+
+  context "signup_at=something" do
+    let(:alt_signup_url)     do
+      server = Capybara.current_session.server
+      "http://#{server.host}:#{server.port}/copyright"
+    end
+    let(:alt_signup_content) { t :"static_pages.copyright.page_heading" }
+
+    before(:each) do
+      @app = create_default_application
+      @app.update_attribute :redirect_uri, "#{@app.redirect_uri}\n#{alt_signup_url}"
+    end
+
+    scenario "arriving from app" do
+      arrive_from_app(params: {signup_at: alt_signup_url}, do_expect: false)
+      click_link(t :"login_signup_form.sign_up")
+      expect(page).to have_content(alt_signup_content)
+    end
+
+    scenario "straight to login" do
+      visit "i/login?signup_at=#{alt_signup_url}&client_id=#{@app.uid}"
+      click_link(t :"login_signup_form.sign_up")
+      expect(page).to have_content(alt_signup_content)
+    end
+  end
+end
