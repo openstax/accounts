e2e334169a61a63b276afaf4ce0097590048d9bd
Ed Woodward <ecw1@rice.edu>
2020-01-30 17:18:18 -0600

    Fixed indent and reworked test

--- spec/lib/date_time_spec.rb
+++ spec/lib/date_time_spec.rb
@@ -2,14 +2,12 @@ require 'date'
 require 'date_time'
 
 describe DateTime do
-    it 'get correct beginning of rice fiscal year' do
-        allow(DateTime).to receive(:now).and_return(Date.parse("2019-09-28"))
-        year1 = DateTime.beginning_of_rice_fiscal_year
-        allow(DateTime).to receive(:now).and_return(Date.parse("2020-01-01"))
-        year2 = DateTime.beginning_of_rice_fiscal_year
-        allow(DateTime).to receive(:now).and_return(Date.parse("2019-03-14"))
-        year3 = DateTime.beginning_of_rice_fiscal_year
-        expect(year1 == year2).to eq true 
-        expect(year1 == year3).to eq false
-    end
+  it 'get correct beginning of rice fiscal year' do
+    allow(DateTime).to receive(:now).and_return(Date.parse("2019-09-28"))
+    expect(DateTime.beginning_of_rice_fiscal_year).to eq DateTime.parse("2019-07-01")
+    allow(DateTime).to receive(:now).and_return(Date.parse("2020-01-01"))
+    expect(DateTime.beginning_of_rice_fiscal_year).to eq DateTime.parse("2019-07-01")
+    allow(DateTime).to receive(:now).and_return(Date.parse("2019-03-14"))
+    expect(DateTime.beginning_of_rice_fiscal_year).to eq DateTime.parse("2018-07-01")
+  end
 end
