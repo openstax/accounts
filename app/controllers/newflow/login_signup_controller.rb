81a76d00407eae1a742d27fbaa84e285f8d650d6
BryanEliDimas <bryanelidimas@gmail.com>
2020-02-12 15:29:02 -0600

    Clean up the before_action-s in LoginSignupController

--- app/controllers/newflow/login_signup_controller.rb
+++ app/controllers/newflow/login_signup_controller.rb
@@ -2,7 +2,12 @@ module Newflow
   # Contains every action for login and signup
   class LoginSignupController < ApplicationController
     layout 'newflow_layout'
+
     skip_before_action :authenticate_user!
+    skip_before_action :check_if_password_expired
+    fine_print_skip :general_terms_of_use, :privacy_policy,
+      except: [:profile_newflow, :verify_pin, :signup_done]
+
     before_action :newflow_authenticate_user!, only: [:profile_newflow]
     before_action :save_new_params_in_session, only: [:login_form, :welcome]
     before_action :store_authorization_url_as_fallback, only: [:login_form, :login, :student_signup_form, :student_signup]
@@ -11,14 +16,9 @@ module Newflow
     before_action :restart_if_missing_unverified_user,
       only: [:verify_email, :verify_pin, :change_your_email, :confirmation_form, :confirmation_form_updated_email]
     before_action :exit_newflow_signup_if_logged_in, only: [:student_signup_form, :welcome]
-    before_action :exit_newflow_signup_if_logged_in, only: [:student_signup_form, :welcome]
     before_action :set_active_banners
     before_action :cache_client_app, only: [:login, :welcome]
-    skip_before_action :check_if_password_expired
-    fine_print_skip :general_terms_of_use, :privacy_policy,
-                    except: [:profile_newflow, :verify_pin, :signup_done]
-
-  before_action :redirect_back, if: -> { signed_in? }, only: :login_form
+    before_action :redirect_back, if: -> { signed_in? }, only: :login_form
 
     def login
       handle_with(
