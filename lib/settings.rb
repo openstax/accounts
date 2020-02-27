62505c43325c4e789029fc2cbcb8c6b2843ee0a3
scb6 <scb6@rice.edu>
2020-02-27 06:50:14 -0600

    remove util method for now

--- lib/settings.rb
+++ lib/settings.rb
@@ -34,7 +34,7 @@ module Settings
 
   module Redis
     mattr_accessor :store
-    if ! is_assets_precompile?
+    if ARGV[0] != "assets:precompile"
       redis_secrets = Rails.application.secrets[:redis]
       self.store = ::Redis::Store.new(
         url: redis_secrets[:url],
