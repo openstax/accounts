7cc260eaf8b5fe02b33b203c21eeb323b84ad863
BryanEliDimas <bryanelidimas@gmail.com>
2020-03-06 09:05:32 -0600

    Create admin report for: How many people start the account creation process versus how many finish it?

--- spec/features/admin/reports_spec.rb
+++ spec/features/admin/reports_spec.rb
@@ -6,6 +6,7 @@ feature 'User reports page', js: true do
     complete_login_username_or_email_screen('admin')
     complete_login_password_screen('password')
   end
+
   it 'counts student users since the start of the fiscal year' do
     # Set up data with a student and instructor in two separate fiscal years
     Timecop.freeze(DateTime.parse("2019-06-30"))
@@ -16,11 +17,11 @@ feature 'User reports page', js: true do
     User.instructor.create
     # Go to the second of those fiscal years
     Timecop.freeze(DateTime.parse("2020-03-01"))
-    visit '/admin/reports'
+    visit admin_reports_path
     expect(page).to have_content("Student accounts created since July 1: 1")
     # Go to the next fiscal year
     Timecop.freeze(DateTime.parse("2020-07-02"))
-    visit '/admin/reports'
+    visit admin_reports_path
     expect(page).to have_content("Student accounts created since July 1: 0")
   end
 
@@ -33,7 +34,22 @@ feature 'User reports page', js: true do
     User.student.create
     User.instructor.create
     # run report
-    visit '/admin/reports'
+    visit admin_reports_path
     expect(page).to have_content("Student accounts created in the past week: 1")
+  end
+
+  example 'How many people start the account creation process versus how many finish it?' do
+    years = (2016..2020).to_a
+    years.each_with_index do |year, index|
+        Timecop.freeze(Time.local(year) + 1.day) do
+          (index * 1).times { FactoryBot.create(:pre_auth_state) }
+          (index * 2).times { User.create(state: User::ACTIVATED) }
+        end
+    end
+
+    visit admin_reports_path
+    expect(page).to have_content('2018: 2 started / 4 finished = 50.0%')
+    expect(page).to have_content('2019: 3 started / 6 finished = 50.0%')
+    expect(page).to have_content('2020: 4 started / 9 finished = 44.44%') # 8 plus the admin
   end
 end
