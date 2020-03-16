ffaf4ad18892a08f44f67e7d1c1d85288780af57
Steve Burkett <steve@burkettnet.com>
2020-03-16 10:23:00 -0700

    step 1 educator flow UI

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     get 'i/signup', action: :welcome, as: :newflow_signup
     get 'i/signup/student', action: :student_signup_form, as: :newflow_signup_student
     post 'i/signup/student', action: :student_signup, as: :newflow_signup_post
+    get 'i/signup/educator', action: :educator_signup_form, as: :educator_signup
     post 'i/signup/educator', action: :educator_signup, as: :educator_signup_post
     get 'i/confirmation_form', action: :confirmation_form, as: :confirmation_form
     post 'i/verify_email_by_pin', action: :verify_email_by_pin, as: :newflow_verify_pin
