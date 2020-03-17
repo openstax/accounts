3e16cc23144a1ab1b94bdf3184ab4b9a65ddba7a
Steve Burkett <steve@burkettnet.com>
2020-03-17 10:01:16 -0700

    WIP - get intlTelInput to work

--- app/assets/javascripts/signup/index.js
+++ app/assets/javascripts/signup/index.js
@@ -10,4 +10,11 @@ $(document).ready(function(){
   $('form:first *:input[type!=hidden]:first').focus();
   OX.Signup.TypeSelector.initialize();
   $('[data-toggle="tooltip"]').tooltip()
+
+  var input = document.querySelector(".int-country-code");
+  window.intlTelInput(input, {
+    formatOnInit: true,
+    separateDialCode: true,
+    preferredCountries: ['us', 'pl']
+  });
 });
