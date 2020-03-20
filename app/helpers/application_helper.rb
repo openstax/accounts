4fd3613e3ae4fca29fc369a14fe7aa6816f3b977
BryanEliDimas <bryanelidimas@gmail.com>
2020-03-20 11:42:23 -0500

    Fix logo URL

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,8 @@ module ApplicationHelper
     'https://openstax.secure.force.com/help/articles/FAQ/Can-t-log-in-to-your-OpenStax-account'
   end
 
-  def openstax_url
-    'https://openstax.org'
+  def logo_url
+    Rails.application.secrets.openstax_url
   end
 
   def unless_errors(options={}, &block)
