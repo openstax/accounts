c2c472a73d1844f636f92def1f0844781b1fb43b
scb6 <scb6@rice.edu>
2020-03-24 12:39:55 -0500

    adding concourse to accounts repo

---
+++ concourse/create-release-candidate-environment/create-release-candidate-environment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+AMI=`cat -v accounts-ami/ami | sed 's/\^\[\[0m//g'`
+cat -v accounts-ami/ami
+echo "AMI: $AMI"
+
+ENV_NAME=p-`cat accounts-git/.git/short_ref`
+echo "ENV_NAME: $ENV_NAME"
+
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET
+export AWS_ACCESS_KEY_ID=$AWS_KEY
+
+set -xe 
+
+cd /accounts-deployment/scripts
+./create_env --region us-east-2 --env_type prod_lite --do_it --env_name "$ENV_NAME" --image_id "$AMI"
