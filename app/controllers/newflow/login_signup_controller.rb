9e8d93b0f3bc34e8f94fdaecb13a12f321a2d623
BryanEliDimas <bryanelidimas@gmail.com>
2020-04-03 11:28:28 -0500

    Improve

--- app/controllers/newflow/login_signup_controller.rb
+++ app/controllers/newflow/login_signup_controller.rb
@@ -176,7 +176,13 @@ module Newflow
 
             if !user.activated?
               # not activated means signup
-              # NOTE: not necessarily "unverified" user, just not activated 😣. # TODO: refactor
+              # Users could have started signing up in the old flow in which case their state is 'needs_profile'
+              # ... in the new flow, that translates to 'unverified'. So update the user state:
+              if user.state == 'needs_profile'
+                user.update_attributes(state: 'unverified', is_newflow: true)
+                security_log(:user_updated, user: user, state_was: 'needs_profile', state_changed_to: 'unverified')
+              end
+
               save_unverified_user(user)
               @first_name = user.first_name
               @last_name = user.last_name
@@ -369,7 +375,7 @@ module Newflow
       id = session[:unverified_user_id]&.to_i
       return unless id.present?
 
-      @unverified_user ||= User.find_by(id: id)
+      @unverified_user ||= User.find_by(id: id, state: 'unverified')
     end
 
     def exit_newflow_signup_if_logged_in
