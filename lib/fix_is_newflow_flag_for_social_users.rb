e04d4e8ad10e6194052fabc58a1f0d04e934953b
BryanEliDimas <bryanelidimas@gmail.com>
2020-05-11 11:51:26 -0500

    WIP: fix is_newflow flag

--- lib/fix_is_newflow_flag_for_social_users.rb
+++ lib/fix_is_newflow_flag_for_social_users.rb
@@ -5,7 +5,10 @@ class FixIsNewflowFlagForSocialUsers
   def self.call
     ActiveRecord::Base.transaction do
       begin
-        users_missed.update_all(is_newflow: true)
+        # users_missed.update_all(is_newflow: true)
+        users_missed.each do |u|
+          u.update!(is_newflow: true)
+        end
       rescue Exception => e
         puts e
         Raven.capture_exception(e)
@@ -22,15 +25,50 @@ class FixIsNewflowFlagForSocialUsers
     }.select(:user_id)
 
     oldflow_social_auth_owners = Authentication.where.has { |auth|
-      (auth.provider == 'facebook') | (auth.provider == 'google') | (auth.provider == 'identity')
+      (auth.provider == 'facebook') | (auth.provider == 'google')
     }.select(:user_id)
 
+    identity_owners = Authentication.where(provider:  'identity').select(:user_id);
+
+    # Note: that identity owners got their `is_newflow` flag set correctly (unless
+    # they were newflow users who added a password after signing up with social :/
+    # but this is likely a handful of users so not a very big deal at this point in time)
     users_with_only_newflow_social_auths = User.where(
-      id: newflow_social_auth_owners
+      id: newflow_social_auth_owners,
+      is_newflow: false
     ).where.not(
       id: oldflow_social_auth_owners
+    ).where.not(
+      id: identity_owners
     )
 
+    users_who_added_a_pwd_after_social_signup = User.find_by_sql(
+      <<~SQL
+      SELECT *
+      FROM users
+      WHERE users.id IN (
+        SELECT auths.user_id
+        FROM authentications auths
+        WHERE (
+          SELECT authentications.*
+          FROM authentications
+          INNER JOIN authentications authentications_with_same_owner
+          ON authentications.user_id = authentications_with_same_owner.user_id
+          WHERE authentications.provider = 'identity' OR authentications.provider = 'facebooknewflow' OR authentications.provider = 'googlenewflow'
+        )
+      )
+      SQL
+    )
+
+    # ORDER BY authentications_with_same_owner.created_at DESC
+    # GROUP BY authentications_with_same_owner.user_id
+    # HAVING (authentications_with_same_owner.provider = 'identity')
+
     users_with_only_newflow_social_auths
+    # users_who_added_a_pwd_after_social_signup
   end
 end
+
+# All users who signed up in the new flow can only be in one of two states, 'unverified' or 'activated'
+# ... and the users
+# User.where.has{ |u| (u.is_newflow == false) & ((u.state == 'unverified') | (u.activated_at != nil)) }
---
+++ spec/lib/fix_is_newflow_flag_for_social_users_spec.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+require 'accept_all_terms'
+
+describe FixIsNewflowFlagForSocialUsers do
+  subject { described_class.call }
+
+  context 'users who signed up with a new flow social provider' do
+    let!(:users) do
+      ['facebooknewflow', 'googlenewflow'].map do |provider|
+        user = FactoryBot.create(:user)
+        FactoryBot.create(:authentication, user: user, provider: provider)
+        user
+      end
+    end
+
+    context 'who have not created a password' do
+      it 'switches `is_newflow` from false to true' do
+        expect(users.pluck(:is_newflow)).to match([false, false])
+        subject
+        expect(users.pluck(:is_newflow)).to match([true, true])
+      end
+    end
+
+    context 'who have created a password' do
+      before do
+        users.each_with_index do |user, index|
+          FactoryBot.create(:authentication, user: user, provider: 'identity')
+          expect(users[index].authentications.count).to eq(2)
+        end
+      end
+
+      it 'switches `is_newflow` from false to true' do
+        expect(users.pluck(:is_newflow)).to match([false, false])
+        subject
+        expect(users.pluck(:is_newflow)).to match([true, true])
+      end
+    end
+  end
+
+  context 'users who signed up with an old flow social provider' do
+    let!(:oldflow_social_auth_owners) do
+      ['facebook', 'google'].map do |provider|
+        user = FactoryBot.create(:user)
+        FactoryBot.create(:authentication, user: user, provider: provider)
+        user
+      end
+    end
+
+    context 'who have not created a password' do
+      it 'does not alter them' do
+        expect { subject }.not_to change { User.pluck(:is_newflow) }
+      end
+    end
+
+    context 'who have created a password' do
+      let(:users) { oldflow_social_auth_owners }
+
+      before do
+        users.each_with_index do |user, index|
+          FactoryBot.create(:authentication, user: user, provider: 'identity')
+          expect(users[index].authentications.count).to eq(2)
+        end
+      end
+
+      it 'does not alter them' do
+        expect { subject }.not_to change { User.pluck(:is_newflow) }
+      end
+    end
+  end
+
+  context 'users who signed up with a password' do
+    let(:users) do
+      (1..2).map do
+        user = FactoryBot.create(:user)
+        FactoryBot.create(:authentication, user: user, provider: 'identity')
+        user
+      end
+    end
+
+    context 'and then added a new flow social provider' do
+      before do
+        ['facebooknewflow', 'googlenewflow'].each_with_index do |provider, index|
+          FactoryBot.create(:authentication, user: users[index], provider: provider)
+          expect(users[index].authentications.count).to eq(2)
+          expect(users[index].authentications.pluck(:provider)).to match(['identity', provider])
+        end
+      end
+
+      it 'does not alter them' do
+        expect { subject }.not_to change { User.pluck(:is_newflow) }
+      end
+    end
+  end
+end
