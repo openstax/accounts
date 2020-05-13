ce5b74d020a5a3c7eaf3fb42d625b4dcdf8f5a79
BryanEliDimas <bryanelidimas@gmail.com>
2020-05-13 15:12:43 -0500

    Improvements for mobile phones and rails logs

--- app/assets/javascripts/multiselect/multiselect.js
+++ app/assets/javascripts/multiselect/multiselect.js
@@ -11,4 +11,3 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
   },{"./ms-filtered-results":"f2rN","./ms-selections":"JZil","./ms-toggle-button":"R1Ae","./os-multi-select.scss":"MUnp"}],"BERD":[function(require,module,exports) {
     "use strict";var e=t(require("./os-multi-select"));function t(e){return e&&e.__esModule?e:{default:e}}window.osMultiSelect=e.default;
   },{"./os-multi-select":"O63z"}]},{},["BERD"], null)
-//# sourceMappingURL=/os-multi-select-global.js.map
