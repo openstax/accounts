edc482a54d56ef3cfa1083871f10cc1147286510
scb6 <scb6@rice.edu>
2020-09-22 07:41:31 -0500

    updating test secrets to work with QA snapshot

--- concourse/test-release-candidate-environment/test-release-candidate-environment.py
+++ concourse/test-release-candidate-environment/test-release-candidate-environment.py
@@ -6,14 +6,14 @@ import sys
 import requests
 
 CIRCLE_API_TOKEN = os.environ["CIRCLE_API_TOKEN"]
-CIRCLE_BRANCH = os.environ.get("CIRCLE_BRANCH", "master")
+CIRCLE_BRANCH = os.environ.get("CIRCLE_BRANCH", "main")
 CIRCLE_JOB = "test-accounts"
 CIRCLE_API_URL = f"https://circleci.com/api/v1.1/project/github/" \
                    f"openstax/os-automation/tree/{CIRCLE_BRANCH}?circle-token={CIRCLE_API_TOKEN}"
 INSTANCE = "unique"
-ADMIN_PASSWORD_UNIQUE = os.environ["TEST_PASSWORD"]
-TEACHER_PASSWORD_UNIQUE = os.environ["TEST_PASSWORD"]
-STUDENT_PASSWORD_UNIQUE = os.environ["TEST_PASSWORD"]
+ADMIN_PASSWORD_UNIQUE = os.environ["ADMIN_PASSWORD"]
+TEACHER_PASSWORD_UNIQUE = os.environ["TEACHER_PASSWORD"]
+STUDENT_PASSWORD_UNIQUE = os.environ["STUDENT_PASSWORD"]
 
 def log(message, file=sys.stderr):
     print(message, file=file)
@@ -32,11 +32,11 @@ data = {"build_parameters":
     {
         "CIRCLE_JOB": CIRCLE_JOB,
         "INSTANCE": INSTANCE,
-        "ADMIN_USER": "sandbox",
+        "ADMIN_USER": "testadmin",
         "ADMIN_PASSWORD_UNIQUE": ADMIN_PASSWORD_UNIQUE,
-        "TEACHER_USER": "teacher@sandbox.openstax.org",
+        "TEACHER_USER": "teacher+01@sandbox.openstax.org",
         "TEACHER_PASSWORD_UNIQUE": TEACHER_PASSWORD_UNIQUE,
-        "STUDENT_USER": "student@sandbox.openstax.org",
+        "STUDENT_USER": "student+01@sandbox.openstax.org",
         "STUDENT_PASSWORD_UNIQUE": STUDENT_PASSWORD_UNIQUE,
         "ACCOUNTS_BASE_URL": "https://accounts-" + env + ".sandbox.openstax.org"
     }
