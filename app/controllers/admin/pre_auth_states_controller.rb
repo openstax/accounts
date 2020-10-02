70e4f957df26db5b5a568cac5f327ba22e413de8
BryanEliDimas <bryanelidimas@gmail.com>
2020-10-02 12:17:33 -0500

    Narrow list to only unverified Contacts in Signup States view

--- app/controllers/admin/pre_auth_states_controller.rb
+++ app/controllers/admin/pre_auth_states_controller.rb
@@ -10,9 +10,10 @@ module Admin
         @unverified_contacts = ContactInfo.where(verified: 'false')
       else
         since = (params[:since] || 1).to_i
-        @pre_auth_states = PreAuthState.where.has{ |t| t.created_at > since.days.ago}
-        @unverified_contacts = ContactInfo.where.has{ |t| t.created_at > since.days.ago}
+        @pre_auth_states = PreAuthState.where.has { |t| t.created_at > since.days.ago }
+        @unverified_contacts = ContactInfo.where.has { |t| (t.verified == false) & (t.created_at > since.days.ago) }
       end
+
       @pre_auth_states = @pre_auth_states.order(created_at: :desc)
       @unverified_contacts = @unverified_contacts.order(created_at: :desc)
     end
