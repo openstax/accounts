b5715972e382f35a302d1f3d1202d0c4c6bb1f48
Michael Harrison <mwharrison@rice.edu>
2021-04-29 15:51:51 -0500

    Revert "tweak aws config"
    This reverts commit adf9fb2e5048eb5a3d7a698d2e174479652ed25b.

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,14 +1,11 @@
-require 'aws-sdk-core'
-
 if Rails.env.production?
   secrets = Rails.application.secrets[:aws][:ses]
 
-  Aws.config.update(
-    region: 'us-west-2',
-    credentials: Aws::Credentials.new(secrets[:access_key_id], secrets[:secret_access_key])
-  )
+  creds = Aws::Credentials.new(secrets[:access_key_id], secrets[:secret_access_key])
 
   Aws::Rails.add_action_mailer_delivery_method(
-    :ses
+    :ses,
+    credentials: creds,
+    region: 'us-west-2'
   )
 end
