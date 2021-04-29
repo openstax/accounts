adf9fb2e5048eb5a3d7a698d2e174479652ed25b
Michael Harrison <mwharrison@rice.edu>
2021-04-29 15:32:54 -0500

    tweak aws config

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,11 +1,14 @@
+require 'aws-sdk-core'
+
 if Rails.env.production?
   secrets = Rails.application.secrets[:aws][:ses]
 
-  creds = Aws::Credentials.new(secrets[:access_key_id], secrets[:secret_access_key])
+  Aws.config.update(
+    region: 'us-west-2',
+    credentials: Aws::Credentials.new(secrets[:access_key_id], secrets[:secret_access_key])
+  )
 
   Aws::Rails.add_action_mailer_delivery_method(
-    :ses,
-    credentials: creds,
-    region: 'us-west-2'
+    :ses
   )
 end
