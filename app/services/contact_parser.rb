21455611ab289d828882c50921e29b0a457f668e
Michael Harrison <mwharrison@rice.edu>
2021-08-03 14:54:22 -0500

    tabs to spaces for JP ;P

--- app/services/contact_parser.rb
+++ app/services/contact_parser.rb
@@ -1,52 +1,52 @@
 class ContactParser
-	LOG_PATH = "#{Rails.root}/log/#{Rails.env}_salesforce.log"
+  LOG_PATH = "#{Rails.root}/log/#{Rails.env}_salesforce.log"
 
-	def initialize(event)
-		@event = event
-	end
+  def initialize(event)
+    @event = event
+  end
 
-	def save_contact
-		contact_params = sanitize_contact
-		ci_table = ContactInfo.arel_table
+  def save_contact
+    contact_params = sanitize_contact
+    ci_table = ContactInfo.arel_table
 
-		contact = User.joins(:contact_infos).eager_load(:contact_infos).where(ci_table[:value].lower.eq(contact_params[:email])).first
+    contact = User.joins(:contact_infos).eager_load(:contact_infos).where(ci_table[:value].lower.eq(contact_params[:email])).first
 
-		if contact.present?
-			# update user salesforce id
-			contact.salesforce_contact_id = contact_params[:sf_id]
-			# update user faculty status
-			contact.faculty_status = case contact_params[:faculty_verified]
-			                      when "Confirmed"
-				                      :confirmed_faculty
-			                      when "Pending"
-				                      :pending_faculty
-			                      when /Rejected/
-				                      :rejected_faculty
-			                      when NilClass
-				                      :no_faculty_info
-			                      else
-				                      raise "Unknown faculty_verified field: '#{contact.faculty_verified}'' on contact #{contact.id}"
-			                      end
+    if contact.present?
+      # update user salesforce id
+      contact.salesforce_contact_id = contact_params[:sf_id]
+      # update user faculty status
+      contact.faculty_status = case contact_params[:faculty_verified]
+                            when "Confirmed"
+                              :confirmed_faculty
+                            when "Pending"
+                              :pending_faculty
+                            when /Rejected/
+                              :rejected_faculty
+                            when NilClass
+                              :no_faculty_info
+                            else
+                              raise "Unknown faculty_verified field: '#{contact.faculty_verified}'' on contact #{contact.id}"
+                            end
 
 
-			contact.save
-			Logger.new(LOG_PATH).debug('Contact saved ID: ' + contact.salesforce_contact_id)
-		else
-			Logger.new(LOG_PATH).debug("No contact found for email #{contact_params[:email]}")
-			# this should not be happening for people we don't have emails for - let's log to sentry so we can investigate
-			Sentry.capture_message("[SF streaming] No contact found for email #{contact_params[:email]}")
-		end
+      contact.save
+      Logger.new(LOG_PATH).debug('Contact saved ID: ' + contact.salesforce_contact_id)
+    else
+      Logger.new(LOG_PATH).debug("No contact found for email #{contact_params[:email]}")
+      # this should not be happening for people we don't have emails for - let's log to sentry so we can investigate
+      Sentry.capture_message("[SF streaming] No contact found for email #{contact_params[:email]}")
+    end
 
-	end
+  end
 
-	private
+  private
 
-	def sanitize_contact
-		sobject = @event['sobject']
-		{
-			email: sobject['Email'],
-			faculty_verified: sobject['Faculty_Verified__c'],
-			sf_id: sobject['Id'],
-		}
-	end
+  def sanitize_contact
+    sobject = @event['sobject']
+    {
+      email: sobject['Email'],
+      faculty_verified: sobject['Faculty_Verified__c'],
+      sf_id: sobject['Id'],
+    }
+  end
 end
--- app/services/salesforce_subscriber.rb
+++ app/services/salesforce_subscriber.rb
@@ -4,56 +4,56 @@ require 'restforce'
 require 'faye'
 
 class SalesforceSubscriber
-	attr_reader :client
-	LOG_PATH = "#{Rails.root}/log/#{Rails.env}_salesforce.log"
-	CONTACT_PUSH_TOPIC_NAME = 'ContactChange'
-	# Initialize a client with your username/password/oauth token/etc.
-	def initialize
-		@client = Restforce.new(username: ENV['SALESFORCE_USERNAME'],
-		                        password: ENV['SALESFORCE_PASSWORD'],
-		                        security_token: ENV['SALESFORCE_SECURITY_TOKEN'],
-		                        client_id: ENV['SALESFORCE_CONSUMER_KEY'],
-		                        client_secret: ENV['SALESFORCE_CONSUMER_SECRET'],
-		                        host: ENV['SALESFORCE_LOGIN_DOMAIN'])
-	end
+  attr_reader :client
+  LOG_PATH = "#{Rails.root}/log/#{Rails.env}_salesforce.log"
+  CONTACT_PUSH_TOPIC_NAME = 'ContactChange'
+  # Initialize a client with your username/password/oauth token/etc.
+  def initialize
+    @client = Restforce.new(username: ENV['SALESFORCE_USERNAME'],
+                            password: ENV['SALESFORCE_PASSWORD'],
+                            security_token: ENV['SALESFORCE_SECURITY_TOKEN'],
+                            client_id: ENV['SALESFORCE_CONSUMER_KEY'],
+                            client_secret: ENV['SALESFORCE_CONSUMER_SECRET'],
+                            host: ENV['SALESFORCE_LOGIN_DOMAIN'])
+  end
 
-	def create_contact_push_topic
-		# Create a PushTopic for subscribing to Contact changes.
-		delete_contact_topics
-		contact_topic = @client.create('PushTopic',
-		                               ApiVersion: '48.0',
-		                               Name: CONTACT_PUSH_TOPIC_NAME,
-		                               Description: 'all contact records',
-		                               NotifyForOperations: 'All',
-		                               NotifyForFields: 'All',
-		                               Query: 'select Id, Email, Faculty_Verified__c from Contact')
+  def create_contact_push_topic
+    # Create a PushTopic for subscribing to Contact changes.
+    delete_contact_topics
+    contact_topic = @client.create('PushTopic',
+                                   ApiVersion: '48.0',
+                                   Name: CONTACT_PUSH_TOPIC_NAME,
+                                   Description: 'all contact records',
+                                   NotifyForOperations: 'All',
+                                   NotifyForFields: 'All',
+                                   Query: 'select Id, Email, Faculty_Verified__c from Contact')
 
-		PushTopic.create(topic_salesforce_id: contact_topic, topic_name: CONTACT_PUSH_TOPIC_NAME) if contact_topic.present? && contact_topic.is_a?(String)
-		Logger.new(LOG_PATH).debug('Contact Push Topic Id: ' + contact_topic)
-	rescue Restforce::ErrorCode::DuplicateValue
-		delete_contact_topics
-		create_contact_push_topic
-	end
+    PushTopic.create(topic_salesforce_id: contact_topic, topic_name: CONTACT_PUSH_TOPIC_NAME) if contact_topic.present? && contact_topic.is_a?(String)
+    Logger.new(LOG_PATH).debug('Contact Push Topic Id: ' + contact_topic)
+  rescue Restforce::ErrorCode::DuplicateValue
+    delete_contact_topics
+    create_contact_push_topic
+  end
 
-	def subscribe
-		authorization_hash = @client.authenticate!
-		@client.faye.set_header 'Authorization', "OAuth #{authorization_hash.access_token}"
-		EM.run do
-			@client.subscription "/topic/#{CONTACT_PUSH_TOPIC_NAME}", replay: -1 do |message|
-				Logger.new(LOG_PATH).debug('Contact Received')
-				ContactParser.new(message).save_contact
-			end
-		end
-	end
+  def subscribe
+    authorization_hash = @client.authenticate!
+    @client.faye.set_header 'Authorization', "OAuth #{authorization_hash.access_token}"
+    EM.run do
+      @client.subscription "/topic/#{CONTACT_PUSH_TOPIC_NAME}", replay: -1 do |message|
+        Logger.new(LOG_PATH).debug('Contact Received')
+        ContactParser.new(message).save_contact
+      end
+    end
+  end
 
-	def delete_contact_topics
-		topics = PushTopic.where(topic_name: CONTACT_PUSH_TOPIC_NAME)
-		if topics.present?
-			topics.each do |topic|
-				@client.destroy('PushTopic', topic.topic_salesforce_id)
-				Logger.new(LOG_PATH).debug('Contact PushTopic destroyed: ' + topic.topic_salesforce_id)
-				topic.destroy
-			end
-		end
-	end
+  def delete_contact_topics
+    topics = PushTopic.where(topic_name: CONTACT_PUSH_TOPIC_NAME)
+    if topics.present?
+      topics.each do |topic|
+        @client.destroy('PushTopic', topic.topic_salesforce_id)
+        Logger.new(LOG_PATH).debug('Contact PushTopic destroyed: ' + topic.topic_salesforce_id)
+        topic.destroy
+      end
+    end
+  end
 end
