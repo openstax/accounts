03787063a43015212c821e4c68c21cfa28c39b29
Michael Harrison <mwharrison@rice.edu>
2021-09-07 12:05:11 -0500

    add all fields to the contact streaming api from SF

--- app/services/contact_parser.rb
+++ app/services/contact_parser.rb
@@ -8,28 +8,57 @@ class ContactParser
     contact_params = sanitize_contact
     ci_table = ContactInfo.arel_table
 
-    contact = User.joins(:contact_infos).eager_load(:contact_infos).where(ci_table[:value].lower.eq(contact_params[:email])).first
+    user = User.joins(:contact_infos).eager_load(:contact_infos).where(ci_table[:value].lower.eq(contact_params[:email])).first
 
-    if contact.present?
-      # update user salesforce id
-      contact.salesforce_contact_id = contact_params[:sf_id]
-      # update user faculty status
-      contact.faculty_status = case contact_params[:faculty_verified]
-                            when "Confirmed"
-                              :confirmed_faculty
-                            when "Pending"
-                              :pending_faculty
-                            when /Rejected/
-                              :rejected_faculty
-                            when NilClass
-                              :no_faculty_info
-                            else
-                              raise "Unknown faculty_verified field: '#{contact.faculty_verified}'' on contact #{contact.id}"
+    if user.present?
+      school = School.select(:id, :salesforce_id, :location, :type).where(
+        salesforce_id: contact_params[:sf_id]
+      ).index_by(&:salesforce_id)
+
+      user.salesforce_contact_id = contact_params[:sf_id]
+      user.using_openstax = contact_params[:adoption_status]
+
+      user.faculty_status = case contact_params[:faculty_verified]
+                              when "Confirmed"
+                                :confirmed_faculty
+                              when "Pending"
+                                :pending_faculty
+                              when /Rejected/
+                                :rejected_faculty
+                              when NilClass
+                                :no_faculty_info
+                              else
+                                raise "Unknown faculty_verified field: '#{contact.faculty_verified}'' on contact #{contact.id}"
                             end
 
+      user.school_type = case school&.type
+                              when *COLLEGE_TYPES
+                                :college
+                              when *HIGH_SCHOOL_TYPES
+                                :high_school
+                              when *K12_TYPES
+                                :k12_school
+                              when *HOME_SCHOOL_TYPES
+                                :home_school
+                              when NilClass
+                                :unknown_school_type
+                              else
+                                :other_school_type
+                         end
 
-      contact.save
-      Rails.logger.debug('Contact saved ID: ' + contact.salesforce_contact_id)
+      user.school_location = case school&.location
+                               when *DOMESTIC_SCHOOL_LOCATIONS
+                                 :domestic_school
+                               when *FOREIGN_SCHOOL_LOCATIONS
+                                 :foreign_school
+                               else
+                                 :unknown_school_location
+                             end
+
+      user.grant_tutor_access = contact_params[:grant_tutor_access]
+      user.is_kip = school&.is_kip || school&.is_child_of_kip
+      user.save
+      Rails.logger.debug('Contact saved ID: ' + user.salesforce_contact_id)
     else
       Rails.logger.debug("No contact found for email #{contact_params[:email]}")
       # this should not be happening for people we don't have emails for - let's log to sentry so we can investigate
@@ -43,9 +72,12 @@ class ContactParser
   def sanitize_contact
     sobject = @event['sobject']
     {
-      email: sobject['Email'],
-      faculty_verified: sobject['Faculty_Verified__c'],
       sf_id: sobject['Id'],
+      email: sobject['Email'],
+      email_alt: sobject['Email_alt__c'],
+      faculty_verified: sobject['Faculty_Verified__c'],
+      adoption_status: sobject['Adoption_Status__c'],
+      grant_tutor_access: sobject['Grant_Tutor_Access__c']
     }
   end
 end
--- app/services/salesforce_subscriber.rb
+++ app/services/salesforce_subscriber.rb
@@ -13,27 +13,29 @@ class SalesforceSubscriber
   end
 
   def create_contact_push_topic
-    delete_contact_topics
-    contact_topic = @client.create!('PushTopic',
-                                   ApiVersion: '51.0',
-                                   Name: CONTACT_PUSH_TOPIC_NAME,
-                                   Description: 'all contact records',
-                                   NotifyForOperationCreate: 'true',
-                                   NotifyForOperationUpdate: 'true',
-                                   NotifyForFields: 'Referenced',
-                                   Query: 'select Id, Email, Faculty_Verified__c from Contact')
+    topic = PushTopic.where(topic_name: CONTACT_PUSH_TOPIC_NAME).first
 
-    if contact_topic.present? && contact_topic.is_a?(String)
-      PushTopic.create(topic_salesforce_id: contact_topic, topic_name: CONTACT_PUSH_TOPIC_NAME)
-      Rails.logger.debug('Contact Push Topic Id: ' + contact_topic)
-    else
-      Rails.logger.error('failed to create push topic: '+ CONTACT_PUSH_TOPIC_NAME)
-      Sentry.capture_message('failed to create push topic: '+ CONTACT_PUSH_TOPIC_NAME)
-      raise
+    unless topic
+      contact_topic = @client.create!('PushTopic',
+                                      ApiVersion: '51.0',
+                                      Name: CONTACT_PUSH_TOPIC_NAME,
+                                      Description: 'all contact records',
+                                      NotifyForOperationCreate: 'true',
+                                      NotifyForOperationUpdate: 'true',
+                                      NotifyForFields: 'Referenced',
+                                      Query: 'select Id, Email, Email_alt__c, Faculty_Verified__c, Adoption_Status__c, Grant_Tutor_Access__c from Contact')
+
+      if contact_topic.present? && contact_topic.is_a?(String)
+        PushTopic.create(topic_salesforce_id: contact_topic, topic_name: CONTACT_PUSH_TOPIC_NAME)
+        warn('Contact Push Topic Id: ' + contact_topic)
+      else
+        Rails.logger.error('failed to create push topic: ' + CONTACT_PUSH_TOPIC_NAME)
+        Sentry.capture_message('failed to create push topic: ' + CONTACT_PUSH_TOPIC_NAME)
+        raise
+      end
     end
   rescue Restforce::ErrorCode::DuplicateValue
-    delete_contact_topics
-    create_contact_push_topic
+    Rails.logger.debug('Push topic duplicate found.')
   end
 
   def subscribe
@@ -42,17 +44,6 @@ class SalesforceSubscriber
       @client.subscription "/topic/#{CONTACT_PUSH_TOPIC_NAME}", replay: -1 do |message|
         Rails.logger.debug('Contact Received')
         ContactParser.new(message).save_contact
-      end
-    end
-  end
-
-  def delete_contact_topics
-    topics = PushTopic.where(topic_name: CONTACT_PUSH_TOPIC_NAME)
-    if topics.present?
-      topics.each do |topic|
-        @client.destroy('PushTopic', topic.topic_salesforce_id)
-        Rails.logger.debug('Contact PushTopic destroyed: ' + topic.topic_salesforce_id)
-        topic.destroy
       end
     end
   end
