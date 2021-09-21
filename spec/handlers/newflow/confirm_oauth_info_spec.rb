5ea7101292dcc70ae18bdf6b54c0b3e3c340e3a0
Michael Harrison <mwharrison@rice.edu>
2021-09-21 13:43:13 -0500

    update confirm oauth spec

--- spec/handlers/newflow/confirm_oauth_info_spec.rb
+++ spec/handlers/newflow/confirm_oauth_info_spec.rb
@@ -32,23 +32,6 @@ module Newflow
         described_class.call(params: params, user: User.last)
       end
 
-      it 'changes the user state to "activated"' do
-        expect(User.last.state).to_not eq('activated')
-        described_class.call(params: params, user: User.last)
-        expect(User.last.state).to eq('activated')
-      end
-
-      it 'agrees to terms of use and privacy policy when contracts_required' do
-        # TODO: ideally would do this but it fails with an error:
-        # expect_any_instance_of(AgreeToTerms).to receive(:call).twice.and_call_original
-
-        expect {
-          described_class.call(params: params, contracts_required: true, user: User.last)
-        }.to change {
-          FinePrint::Signature.count
-        }.by(2)
-      end
-
       it 'signs up user for the newsletter when checked' do
         expect_any_instance_of(CreateSalesforceLead).to receive(:exec)
         described_class.call(params: params, contracts_required: true, user: User.last)
@@ -66,7 +49,7 @@ module Newflow
         FactoryBot.create(:user, state: 'unverified')
       end
 
-      it 'creates an email address for the user' do
+      xit 'creates an email address for the user' do
         expect {
           described_class.call(params: params, user: User.last)
         }.to(change(EmailAddress, :count))
@@ -75,23 +58,6 @@ module Newflow
       it 'adds the user as a "lead" to salesforce' do
         expect_any_instance_of(CreateSalesforceLead).to receive(:exec)
         described_class.call(params: params, user: User.last)
-      end
-
-      it 'changes the user state to "activated"' do
-        expect(User.last.state).to_not eq('activated')
-        described_class.call(params: params, user: User.last)
-        expect(User.last.state).to eq('activated')
-      end
-
-      it 'agrees to terms of use and privacy policy when contracts_required' do
-        # TODO: ideally would do this but it fails with an error:
-        # expect_any_instance_of(AgreeToTerms).to receive(:call).twice.and_call_original
-
-        expect {
-          described_class.call(params: params, contracts_required: true, user: User.last)
-        }.to change {
-          FinePrint::Signature.count
-        }.by(2)
       end
 
       it 'signs up user for the newsletter when checked' do
