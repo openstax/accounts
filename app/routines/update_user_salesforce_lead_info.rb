af642409df39e3225bb981aade57197313d3b8e2
Ed Woodward <ecw1@rice.edu>
2021-10-13 10:46:22 -0500

    Updated to use user_helper

--- app/routines/update_user_salesforce_lead_info.rb
+++ app/routines/update_user_salesforce_lead_info.rb
@@ -1,16 +1,4 @@
 class UpdateUserSalesforceLeadInfo
-  COLLEGE_TYPES = [
-    'College/University (4)',
-    'Technical/Community College (2)',
-    'Career School/For-Profit (2)'
-  ]
-  HIGH_SCHOOL_TYPES = [ 'High School' ]
-  K12_TYPES = [ 'K-12 School' ]
-  HOME_SCHOOL_TYPES = [ 'Home School' ]
-
-  DOMESTIC_SCHOOL_LOCATIONS = [ 'Domestic' ]
-  FOREIGN_SCHOOL_LOCATIONS = [ 'Foreign' ]
-
   def self.call
     new.call
   end
@@ -25,29 +13,9 @@ class UpdateUserSalesforceLeadInfo
       user['grant_tutor_access'] = lead['grant_tutor_access']
       lookedup_school = School.where(name: user['self_reported_school']).first if user['self_reported_school'].present?
       location = lookedup_school['location'] if lookedup_school.present?
-      user['school_location'] = case location
-                                when *DOMESTIC_SCHOOL_LOCATIONS
-                                  :domestic_school
-                                when *FOREIGN_SCHOOL_LOCATIONS
-                                  :foreign_school
-                                else
-                                  :unknown_school_location
-                                end
+      user['school_location'] = UserHelper.convert_to_user_location(location)
       type = lookedup_school['type'] if lookedup_school.present?
-      user['school_type'] = case type
-                         when *COLLEGE_TYPES
-                           :college
-                         when *HIGH_SCHOOL_TYPES
-                           :high_school
-                         when *K12_TYPES
-                           :k12_school
-                         when *HOME_SCHOOL_TYPES
-                           :home_school
-                         when NilClass
-                           :unknown_school_type
-                         else
-                           :other_school_type
-                         end
+      user['school_type'] = UserHelper.convert_to_user_type(type)
 
       user.save! if user.changed?
 
