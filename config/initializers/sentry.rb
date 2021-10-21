95b426fa98c4e65573328231a37512f3be3034c6
Michael Harrison <mwharrison@rice.edu>
2021-10-21 16:16:12 -0500

    readd sentry configuration file

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,12 @@
+Sentry.init do |config|
+  config.dsn = Rails.application.secrets.sentry[:dsn]
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # To activate performance monitoring, set one of these options.
+  # We recommend adjusting the value in production:
+  config.traces_sample_rate = 0.5
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
