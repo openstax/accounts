19b03fa3f2fb10e2778a3d87134b26669fb49e64
Michael Harrison <mwharrison@rice.edu>
2021-10-25 17:32:05 -0500

    Remove unused preauth states from admin report

--- app/controllers/admin/pre_auth_states_controller.rb
+++ app/controllers/admin/pre_auth_states_controller.rb
@@ -6,7 +6,6 @@ module Admin
 
     def index
       if params[:since] == "forever"
-        @pre_auth_states = PreAuthState.all
         @unverified_contacts = ContactInfo.where(verified: 'false')
       else
         since = (params[:since] || 1).to_i
@@ -14,11 +13,9 @@ module Admin
         not_verified = ci_table[:verified].eq(false)
         since_days_ago = ci_table[:created_at].gt(since.days.ago)
 
-        @pre_auth_states = PreAuthState.where(PreAuthState.arel_table[:created_at].gt(since.days.ago) )
         @unverified_contacts = ContactInfo.where(not_verified.and(since_days_ago))
       end
 
-      @pre_auth_states = @pre_auth_states.order(created_at: :desc)
       @unverified_contacts = @unverified_contacts.order(created_at: :desc)
     end
 
