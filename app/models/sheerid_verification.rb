ed9c0688a99990aea1e21963411e541c4f7f1fd4
Michael Harrison <mwharrison@rice.edu>
2022-01-27 13:05:04 -0600

    cleanup sheer id verification model

--- app/models/sheerid_verification.rb
+++ app/models/sheerid_verification.rb
@@ -1,25 +1,13 @@
 class SheeridVerification < ApplicationRecord
-
-  VERIFIED = 'success'
-  REJECTED = 'rejected'
-
   validates :verification_id, presence: true
   validates :current_step, presence: true
-
-  def verified?
-    self.current_step == VERIFIED
-  end
-
-  def rejected?
-    self.current_step == REJECTED
-  end
 
   # Translate SheerID nomenclature to `User#faculty_status` nomenclature
   def current_step_to_faculty_status
     case self.current_step
-    when VERIFIED
+    when self.current_step == 'success'
       User.faculty_statuses[:confirmed_faculty]
-    when REJECTED
+    when self.current_step == 'rejected'
       User.faculty_statuses[:rejected_faculty]
     else
       User.faculty_statuses[:pending_faculty]
