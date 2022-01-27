7bd22f7f13e5e7b05558b8b81f118c15de0bfdfa
Michael Harrison <mwharrison@rice.edu>
2022-01-26 19:09:36 -0600

    Fixes for scout (#1025)
    * use golden image ENV_NAME
    
    * fix env name for production in spec
    
    * update to sentry config and assets workflow
    
    * update to codecov

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,6 +5,9 @@ Sentry.init do |config|
   config.environment = secrets.environment_name
   config.release = secrets.release_version
 
+  config.breadcrumbs_logger = %i[sentry_logger active_support_logger http_logger]
+  config.traces_sample_rate = 0.5
+
   # Send POST data and cookies to Sentry
   config.send_default_pii = true
 end if Rails.env.production?
