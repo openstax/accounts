864573b3967b85a3894293273c8998a457e1e1af
Michael Harrison <mwharrison@rice.edu>
2022-03-03 12:52:03 -0600

    update field names for openstax account object to SF

--- app/routines/add_account_to_salesforce.rb
+++ app/routines/add_account_to_salesforce.rb
@@ -17,11 +17,11 @@ class AddAccountToSalesforce
       )
 
       sf_ox_account.account_id = user.id
-      sf_ox_account.role = user.role.to_s,
-      sf_ox_account.contact_id = user&.salesforce_contact_id,
-      sf_ox_account.lead_id = user&.salesforce_lead_id,
+      sf_ox_account.account_role = user.role.to_s,
+      sf_ox_account.salesforce_contact_id = user&.salesforce_contact_id,
+      sf_ox_account.salesforce_lead_id = user&.salesforce_lead_id,
       sf_ox_account.signup_date = user.created_at.strftime("%Y-%m-%dT%H:%M:%S%z")
-      sf_ox_account.environment = Rails.application.secrets.environment_name
+      sf_ox_account.account_environment = Rails.application.secrets.environment_name
 
       if sf_ox_account.save!
         contacts_added_or_updated += 1
