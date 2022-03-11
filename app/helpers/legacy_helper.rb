e6c3dcb13ffe49c11c65115cd8740c364def3d1a
Michael Harrison <mwharrison@rice.edu>
2022-03-11 11:53:25 -0600

    revert changes to legacy helper

--- app/helpers/legacy_helper.rb
+++ app/helpers/legacy_helper.rb
@@ -1,11 +1,13 @@
 module LegacyHelper
 
-  def redirect_to_newflow_if_enabled
-    return unless request.get?
+  return unless request.get?
+  should_redirect_to_newflow = Settings::FeatureFlags.any_newflow_feature_flags? && get_param_to_permit_legacy_flow.blank?
+  if should_redirect_to_newflow
     if request.path == signup_path
       redirect_to newflow_signup_path(request.query_parameters)
-    elsif request.path == login_path
+    else
       redirect_to newflow_login_path(request.query_parameters)
     end
   end
+
 end
