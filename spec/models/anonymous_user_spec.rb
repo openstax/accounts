13baef0ff78ac9c744447aeed168294518bc110b
Michael Harrison <mwharrison@rice.edu>
2022-03-29 10:06:15 -0500

    update anon user spec

--- spec/models/anonymous_user_spec.rb
+++ spec/models/anonymous_user_spec.rb
@@ -4,12 +4,15 @@ describe AnonymousUser do
   let!(:au) { AnonymousUser.instance }
 
   it 'handles faculty status enum methods' do
-    expect(au.no_faculty_info?).to be_truthy
+    expect(au.incomplete_signup?).to be_truthy
+    expect(au.no_faculty_info?).to be_falsy
     expect(au.pending_faculty?).to be_falsy
     expect(au.rejected_faculty?).to be_falsy
+    expect(au.pending_sheerid?).to be_falsy
+    expect(au.rejected_by_sheerid?).to be_falsy
     expect(au.confirmed_faculty?).to be_falsy
 
-    expect(au.faculty_status).to eq "no_faculty_info"
+    expect(au.faculty_status).to eq "incomplete_signup"
     expect{au.faculty_status="blah"}.to raise_error(AnonymousUserIsImmutableError)
     expect{au.no_faculty_info!}.to raise_error(AnonymousUserIsImmutableError)
     expect{au.pending_faculty!}.to raise_error(AnonymousUserIsImmutableError)
