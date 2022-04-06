ad8bd3a3b3ea246b78833a5cb1b839baf65a4e7a
Michael Harrison <mwharrison@rice.edu>
2022-04-06 15:11:43 -0500

    remove unsupported feature flags

--- lib/settings/feature_flags.rb
+++ lib/settings/feature_flags.rb
@@ -2,25 +2,10 @@ module Settings
   module FeatureFlags
     class << self
 
-      def any_newflow_feature_flags?
-        student_feature_flag || educator_feature_flag
-      end
-
-      def student_feature_flag
-        Settings::Db.store.student_feature_flag
-      end
-
-      def student_feature_flag=(bool)
-        Settings::Db.store.student_feature_flag = bool
-      end
-
-      def educator_feature_flag
-        Settings::Db.store.educator_feature_flag
-      end
-
-      def educator_feature_flag=(bool)
-        Settings::Db.store.educator_feature_flag = bool
-      end
+      # Example of a defined feature flag
+      # def student_feature_flag=(bool)
+      #   Settings::Db.store.student_feature_flag = bool
+      # end
 
     end
   end
