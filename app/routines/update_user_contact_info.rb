4e81d0fa17e7c9cbdeb64c9ab5007f0a6749adbf
Michael Harrison <mwharrison@rice.edu>
2022-04-07 11:59:23 -0500

    add exception capture to contact update

--- app/routines/update_user_contact_info.rb
+++ app/routines/update_user_contact_info.rb
@@ -31,38 +31,38 @@ class UpdateUserContactInfo
     log("#{contacts.count} contacts fetched from Salesforce")
     contacts_by_uuid = contacts_by_uuid_hash(contacts)
 
+    begin
+      users ||= User.where(uuid: contacts.map(&:accounts_uuid))
+      schools_by_salesforce_id = School.select(:id, :salesforce_id).where(
+        salesforce_id: contacts_by_uuid.values.compact.map(&:school_id)
+      ).index_by(&:salesforce_id)
 
-    users ||= User.where(uuid: contacts.map(&:accounts_uuid))
-    schools_by_salesforce_id = School.select(:id, :salesforce_id).where(
-      salesforce_id: contacts_by_uuid.values.compact.map(&:school_id)
-    ).index_by(&:salesforce_id)
 
+      # loop through users - we keep some counts for logging out
+      users_updated = 0
+      users_fv_status_changed = 0
+      users_without_cached_school = 0
+      log("Updating #{users.count} users from Salesforce")
 
-    # loop through users - we keep some counts for logging out
-    users_updated = 0
-    users_fv_status_changed = 0
-    users_without_cached_school = 0
-    log("Updating #{users.count} users from Salesforce")
+        users.each do |user|
+          sf_contact = contacts_by_uuid[user.uuid]
+          school = schools_by_salesforce_id[sf_contact.school_id]
 
-    users.each do |user|
-      sf_contact = contacts_by_uuid[user.uuid]
-      school = schools_by_salesforce_id[sf_contact.school_id]
-
-      if user.salesforce_contact_id.blank?
-        user.salesforce_contact_id = sf_contact.id
-        SecurityLog.create!(
-          user:       user,
-          event_type: :user_contact_id_updated_from_salesforce,
-          event_data: { contact_id: sf_contact.id }
-        )
-      elsif user.salesforce_contact_id != sf_contact.id
-        user.salesforce_contact_id = sf_contact.id
-        SecurityLog.create!(
-          user:       user,
-          event_type: :user_contact_id_updated_from_salesforce,
-          event_data: { contact_id: sf_contact.id }
-        )
-      end
+          if user.salesforce_contact_id.blank?
+            user.salesforce_contact_id = sf_contact.id
+            SecurityLog.create!(
+              user:       user,
+              event_type: :user_contact_id_updated_from_salesforce,
+              event_data: { contact_id: sf_contact.id }
+            )
+          elsif user.salesforce_contact_id != sf_contact.id
+            user.salesforce_contact_id = sf_contact.id
+            SecurityLog.create!(
+              user:       user,
+              event_type: :user_contact_id_updated_from_salesforce,
+              event_data: { contact_id: sf_contact.id }
+            )
+          end
 
       old_fv_status = user.faculty_status
       user.faculty_status = case sf_contact.faculty_verified
@@ -83,59 +83,63 @@ class UpdateUserContactInfo
                                 sf_contact.faculty_verified}'' on contact #{sf_contact.id}")
                             end
 
-      user.school_type = case sf_contact.school_type
-                         when *COLLEGE_TYPES
-                           :college
-                         when *HIGH_SCHOOL_TYPES
-                           :high_school
-                         when *K12_TYPES
-                           :k12_school
-                         when *HOME_SCHOOL_TYPES
-                           :home_school
-                         when NilClass
-                           :unknown_school_type
-                         else
-                           :other_school_type
-                         end
 
-      sf_school = sf_contact.school
-      user.school_location = case sf_school&.school_location
-                             when *DOMESTIC_SCHOOL_LOCATIONS
-                               :domestic_school
-                             when *FOREIGN_SCHOOL_LOCATIONS
-                               :foreign_school
+          user.school_type = case sf_contact.school_type
+                             when *COLLEGE_TYPES
+                               :college
+                             when *HIGH_SCHOOL_TYPES
+                               :high_school
+                             when *K12_TYPES
+                               :k12_school
+                             when *HOME_SCHOOL_TYPES
+                               :home_school
+                             when NilClass
+                               :unknown_school_type
                              else
-                               :unknown_school_location
+                               :other_school_type
                              end
 
-      unless sf_contact.adoption_status.blank?
-        user.using_openstax = ADOPTION_STATUSES[sf_contact.adoption_status]
-      end
+          sf_school = sf_contact.school
+          user.school_location = case sf_school&.school_location
+                                 when *DOMESTIC_SCHOOL_LOCATIONS
+                                   :domestic_school
+                                 when *FOREIGN_SCHOOL_LOCATIONS
+                                   :foreign_school
+                                 else
+                                   :unknown_school_location
+                                 end
+          
+          unless sf_contact.adoption_status.blank?
+            user.using_openstax = ADOPTION_STATUSES[sf_contact.adoption_status]
+          end
 
-      user.is_kip = sf_school&.is_kip || sf_school&.is_child_of_kip
-      user.grant_tutor_access = sf_contact.grant_tutor_access
-      user.is_b_r_i_user = sf_contact.b_r_i_marketing
-      user.renewal_eligible = sf_contact.renewal_eligible
+          user.is_kip = sf_school&.is_kip || sf_school&.is_child_of_kip
+          user.grant_tutor_access = sf_contact.grant_tutor_access
+          user.is_b_r_i_user = sf_contact.b_r_i_marketing
+          user.renewal_eligible = sf_contact.renewal_eligible
 
-      if school.nil? && !sf_school.nil?
-        users_without_cached_school += 1
-      else
-        user.school = school
-      end
+          if school.nil? && !sf_school.nil?
+            users_without_cached_school += 1
+          else
+            user.school = school
+          end
 
-      if user.faculty_status_changed?
-        users_fv_status_changed += 1
-        SecurityLog.create!(
-          user: user,
-          event_type: :salesforce_updated_faculty_status,
-          event_data: { user_id: user.id, salesforce_contact_id: sf_contact.id, old_status:old_fv_status, new_status: user.faculty_status }
-        )
-      end
+          if user.faculty_status_changed?
+            users_fv_status_changed += 1
+            SecurityLog.create!(
+              user: user,
+              event_type: :salesforce_updated_faculty_status,
+              event_data: { user_id: user.id, salesforce_contact_id: sf_contact.id, old_status:old_fv_status, new_status: user.faculty_status }
+            )
+          end
 
-      if user.changed?
-        user.save!
-        users_updated += 1
-      end
+          if user.changed?
+            user.save!
+            users_updated += 1
+          end
+        end
+    rescue StandardError => se
+      Sentry.capture_exception se
     end
 
     log("Completed updating #{users_updated} users.")
