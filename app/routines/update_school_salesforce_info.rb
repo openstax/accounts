f525187cd182c539a72dfb3a95ae7a41e1c626ce
Michael Harrison <mwharrison@rice.edu>
2022-04-07 11:58:54 -0500

    removing batching from school - update once per day

--- app/routines/update_school_salesforce_info.rb
+++ app/routines/update_school_salesforce_info.rb
@@ -1,5 +1,4 @@
 class UpdateSchoolSalesforceInfo
-  BATCH_SIZE = 250
 
   SF_TO_DB_CACHE_COLUMNS_MAP = {
     id: :salesforce_id,
@@ -28,7 +27,7 @@ class UpdateSchoolSalesforceInfo
     # Check if any Schools that have 0 users have been deleted from Salesforce and remove them
     School.where(
       'NOT EXISTS (SELECT * FROM "users" WHERE "users"."school_id" = "schools"."id")'
-    ).find_in_batches(batch_size: BATCH_SIZE) do |schools|
+    ) do |schools|
       salesforce_ids = schools.map(&:salesforce_id)
 
       existing_salesforce_ids = OpenStax::Salesforce::Remote::School.select(:id).where(
@@ -41,44 +40,32 @@ class UpdateSchoolSalesforceInfo
     end
 
     # Go through all SF Schools and cache their information, if it changed
-    schools_updated = 0
-    last_id = nil
-    loop do
-      sf_schools = OpenStax::Salesforce::Remote::School.order(:Id).limit(BATCH_SIZE)
+    sf_schools = OpenStax::Salesforce::Remote::School.order(:Id)
+    sf_schools = sf_schools.to_a
 
-      sf_schools = sf_schools.where("Id > '#{last_id}'") unless last_id.nil?
-      sf_schools = sf_schools.to_a
-      last_id = sf_schools.last&.id
+    begin
+      schools_by_sf_id = School.where(
+        salesforce_id: sf_schools.map(&:id)
+      ).index_by(&:salesforce_id)
 
-      begin
-        schools_by_sf_id = School.where(
-          salesforce_id: sf_schools.map(&:id)
-        ).index_by(&:salesforce_id)
+      schools = sf_schools.map do |sf_school|
+        school = schools_by_sf_id[sf_school.id]
+        school = School.new(salesforce_id: sf_school.id) if school.nil?
 
-        schools = sf_schools.map do |sf_school|
-          school = schools_by_sf_id[sf_school.id]
-          schools_updated += 1 if school.nil?
-          school = School.new(salesforce_id: sf_school.id) if school.nil?
+        SF_TO_DB_CACHE_COLUMNS_MAP.each do |sf_column, db_column|
+          school.public_send "#{db_column}=", sf_school.public_send(sf_column)
+        end
 
-          SF_TO_DB_CACHE_COLUMNS_MAP.each do |sf_column, db_column|
-            school.public_send "#{db_column}=", sf_school.public_send(sf_column)
-          end
+        school.changed? ? school : nil
+      end.compact
 
-          school.changed? ? school : nil
-        end.compact
-
-        School.import(
-          schools, validate: false, on_duplicate_key_update: {
-            conflict_target: [ :salesforce_id ], columns: SF_TO_DB_CACHE_COLUMNS_MAP.values
-          }
-        ) unless schools.empty?
-      rescue StandardError => se
-        Sentry.capture_exception se
-      end
-
-      break if sf_schools.length < BATCH_SIZE
+      School.import(
+        schools, validate: false, on_duplicate_key_update: {
+          conflict_target: [ :salesforce_id ], columns: SF_TO_DB_CACHE_COLUMNS_MAP.values
+        }
+      ) unless schools.empty?
+    rescue StandardError => se
+      Sentry.capture_exception se
     end
-
-    log("Finished updating #{schools_updated} schools")
   end
 end
