8e36081b0768d61f8bbb95742905c03237f049be
Michael Harrison <mwharrison@rice.edu>
2022-04-07 17:08:15 -0500

    refactor rejected lead code

--- app/routines/update_rejected_leads_from_salesforce.rb
+++ app/routines/update_rejected_leads_from_salesforce.rb
@@ -1,4 +1,5 @@
 class UpdateRejectedLeadsFromSalesforce
+
   def self.call
     new.call
   end
@@ -6,65 +7,56 @@ class UpdateRejectedLeadsFromSalesforce
   def call
     log("Starting rejected lead sync with Salesforce")
 
-    begin
-    rejected_leads ||= OpenStax::Salesforce::Remote::Lead.select(
+    rejected_leads = OpenStax::Salesforce::Remote::Lead.select(
       :id,
       :faculty_verified,
       :accounts_uuid
-    ).where("Accounts_UUID__c != null").where("FV_Status__c = 'rejected_faculty'").to_a
+    ).where("Accounts_UUID__c != null").where("FV_Status__c = 'rejected_faculty'").where("Status = 'Unqualified'")
 
-    leads_by_uuid = leads_by_uuid_hash(rejected_leads)
-    users ||= User.where(uuid: rejected_leads.map(&:accounts_uuid))
+    begin
+    rejected_leads.each do |reject_lead|
+      rejected_user = User.find_by(uuid: reject_lead.accounts_uuid)
 
-
-    users.each do |user|
-      sf_lead = leads_by_uuid[user.uuid]
-
-      if user.salesforce_lead_id.blank?
-        user.salesforce_lead_id = sf_lead.id
+      if rejected_user.salesforce_lead_id.blank?
+        rejected_user.salesforce_lead_id = reject_lead.id
         SecurityLog.create!(
-          user:       user,
+          user: rejected_user,
           event_type: :user_lead_id_updated_from_salesforce,
-          event_data: { lead_id: sf_lead.id }
+          event_data: { lead_id: reject_lead.id }
         )
-      elsif user.salesforce_lead_id != sf_lead.id
-        user.salesforce_lead_id = sf_lead.id
+      elsif rejected_user.salesforce_lead_id != sf_lead.id
+        rejected_user.salesforce_lead_id = reject_lead.id
         SecurityLog.create!(
-          user:       user,
+          user: rejected_user,
           event_type: :user_lead_id_updated_from_salesforce,
-          event_data: { lead_id: sf_lead.id }
+          event_data: { lead_id: reject_lead.id }
         )
       end
 
-      old_fv_status       = user.faculty_status
-      user.faculty_status = case sf_lead.faculty_verified
-                              when "confirmed_faculty"
-                                :confirmed_faculty
-                              when "pending_faculty"
-                                :pending_faculty
+      old_fv_status       = rejected_user.faculty_status
+      rejected_user.faculty_status = case reject_lead.faculty_verified
                               when "rejected_faculty"
                                 :rejected_faculty
-                              when NilClass
-                                :no_faculty_info
                               else
-                                Sentry.capture_message("Unknown faculty_verified field: '#{
-                                  sf_lead.faculty_verified}'' on lead #{sf_lead.id}")
+                                Sentry.capture_message("Attempting to reject a lead that shouldn't be: '#{
+                                  reject_lead.faculty_verified}'' on lead #{reject_lead.id}")
                             end
-      if user.faculty_status_changed?
+      if rejected_user.faculty_status_changed?
         SecurityLog.create!(
-          user:       user,
+          user: rejected_user,
           event_type: :salesforce_updated_faculty_status,
-          event_data: { user_id: user.id, salesforce_contact_id: sf_lead.id, old_status: old_fv_status, new_status: user.faculty_status }
+          event_data: { user_id: rejected_user.id, salesforce_contact_id: reject_lead.id, old_status: old_fv_status, new_status: rejected_user.faculty_status }
         )
       end
 
-      if user.changed?
-        user.save!
+      if rejected_user.changed?
+        rejected_user.save!
       end
     end
   rescue StandardError => se
     Sentry.capture_exception se
   end
+    log("Finished processing rejected leads")
   end
 
   def leads_by_uuid_hash(leads)
