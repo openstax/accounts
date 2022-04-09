687c5c4637d7aeb401e40cf6b2455fbf6968e427
Michael Harrison <mwharrison@rice.edu>
2022-04-09 12:49:10 -0500

    fix spec class names

--- spec/jobs/add_new_account_to_salesforce_job_spec.rb
+++ spec/jobs/add_new_account_to_salesforce_job_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe SyncExistingAccountsToSalesforceJob, type: :job do
+RSpec.describe AddNewAccountToSalesforceJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/jobs/sync_existing_account_to_salesforce_job_spec.rb
+++ spec/jobs/sync_existing_account_to_salesforce_job_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe SyncNewAccountsToSalesforceJob, type: :job do
+RSpec.describe SyncExistingAccountToSalesforceJob, type: :job do
   pending "add some examples to (or delete) #{__FILE__}"
 end
