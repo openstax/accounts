bc700d4e0faf20ac74bd08ecd28f3c338dc16e18
Michael Harrison <mwharrison@rice.edu>
2022-04-11 11:48:42 -0500

    new_way_to_login -> way_to_login

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,41 +1,5 @@
 module ProfileHelper
 
-  def newflow_way_to_login(provider:, user_authentications: nil, has_authentication: nil, current_providers:)
-    if has_authentication.nil?
-      if user_authentications.nil?
-        raise "At least one of user_authentications or has_authentication must be set"
-      end
-
-      has_authentication = user_authentications.any?{|auth| auth.provider == provider}
-    end
-
-    icon_class, display_name =
-      case provider
-      when 'identity' then ['key', (I18n.t :"users.edit.password")]
-      when 'google_oauth2' then ['google', 'Google']
-      when 'facebooknewflow' then ['facebook', 'Facebook']
-      when 'googlenewflow' then ['google', 'Google']
-      else [ provider, provider.capitalize ]
-      end
-
-    icons = [
-      'glyphicon-pencil edit--newflow', 'glyphicon-trash delete--newflow', 'glyphicon-plus add--newflow',
-    ]
-
-    snippet = <<-SNIPPET
-      <span class="icon fa-stack fa-lg">
-        <i class="fa fa-square fa-stack-2x #{provider}-bkg"></i>
-        <i class="fa fa-#{icon_class} fa-stack-1x fa-inverse"></i>
-      </span>
-      <span class="name">#{display_name}</span>
-      <span class="mod-holder">
-      #{icons.map{|icon| "<span class='glyphicon #{icon} mod'></span>"}.join}
-      </span>
-    SNIPPET
-
-    "<div class='authentication' data-provider='#{provider}'>#{snippet}</div>".html_safe
-  end
-
   def way_to_login(provider:, user_authentications: nil, has_authentication: nil, current_providers:)
     if has_authentication.nil?
       if user_authentications.nil?
@@ -55,7 +19,7 @@ module ProfileHelper
       end
 
     icons = [
-      'glyphicon-pencil edit', 'glyphicon-trash delete', 'glyphicon-plus add',
+      'glyphicon-pencil edit--newflow', 'glyphicon-trash delete--newflow', 'glyphicon-plus add--newflow',
     ]
 
     snippet = <<-SNIPPET
