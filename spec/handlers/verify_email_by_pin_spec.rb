e10a504fb435e876ca8e1d1f0063afb8b1adb5c7
Michael Harrison <mwharrison@rice.edu>
2022-04-19 15:17:02 -0500

    cleanup signup and verify email specs

--- spec/handlers/verify_email_by_pin_spec.rb
+++ spec/handlers/verify_email_by_pin_spec.rb
@@ -1,88 +1,83 @@
 require 'rails_helper'
-require 'vcr_helper'
 
-RSpec.describe VerifyEmailByPin, type: :handler do
-  context 'student' do
-    context 'when success' do
-      let(:user) do
-        FactoryBot.create(:user, state: 'unverified', source_application: source_app, receive_newsletter: false)
-      end
+describe VerifyEmailByPin, type: :handler do
+  context 'when success' do
+    before { disable_sfdc_client }
 
-      let(:source_app) do
-        FactoryBot.create(:doorkeeper_application)
-      end
-
-      let(:email) do
-        FactoryBot.create(:email_address, user: user, value: 'test@openstax.org')
-      end
-
-      let(:params) do
-        {
-          confirm: {
-            pin: email.confirmation_pin
-          }
-        }
-      end
-
-      it 'runs ActivateStudent' do
-        expect_any_instance_of(ActivateUser).to receive(:exec)
-        described_class.call(params: params, email_address: email)
-      end
-
-      it 'sets the user (email owner) state to "activated"' do
-        expect(email.user.state).not_to eq('activated')
-        described_class.call(params: params, email_address: email)
-        expect(email.user.state).to eq('activated')
-      end
-
-      it 'marks the user-s EmailAddress as verified' do
-        expect(email.verified).to be(false)
-        described_class.call(params: params, email_address: email)
-        expect(email.verified).to be(true)
-      end
+    let(:educator_user) do
+      FactoryBot.create(:user, state: 'unverified', source_application: source_app, receive_newsletter: false, role: :instructor)
     end
-  end
 
-  context 'educator' do
-    context 'when success' do
-      before { disable_sfdc_client }
+    let(:student_user) do
+      FactoryBot.create(:user, state: 'unverified', source_application: source_app, receive_newsletter: false, role: :student)
+    end
 
-      let(:user) do
-        FactoryBot.create(:user, state: 'unverified', source_application: source_app, receive_newsletter: true)
-      end
+    let(:source_app) do
+      FactoryBot.create(:doorkeeper_application)
+    end
 
-      let(:source_app) do
-        FactoryBot.create(:doorkeeper_application)
-      end
+    let(:educator_email) do
+      FactoryBot.create(:email_address, user: educator_user, value: Faker::Internet.email)
+    end
 
-      let(:email) do
-        FactoryBot.create(:email_address, user: user)
-      end
+    let(:student_email) do
+      FactoryBot.create(:email_address, user: student_user, value: Faker::Internet.email)
+    end
 
-      let(:params) do
-        {
-          confirm: {
-            pin: email.confirmation_pin
-          }
+    let(:educator_params) do
+      {
+        confirm: {
+          pin: educator_email.confirmation_pin
         }
-      end
+      }
+    end
 
-      it 'activates their account' do
-        expect_any_instance_of(ActivateUser).to receive(:exec).with(user)
-        described_class.call(params: params, email_address: email)
-      end
+    let(:student_params) do
+      {
+        confirm: {
+          pin: student_email.confirmation_pin
+        }
+      }
+    end
 
-      it 'sets the user (email owner) state to "activated"' do
-        expect(email.user.state).not_to eq('activated')
-        described_class.call(params: params, email_address: email)
-        expect(email.user.state).to eq('activated')
-      end
+    it 'runs ActivateUser for educators' do
+      expect_any_instance_of(ActivateUser).to receive(:exec)
+      described_class.call(params: educator_params, email_address: educator_email)
+    end
 
-      it 'marks the user\'s EmailAddress as verified' do
-        expect(email.verified).to be(false)
-        described_class.call(params: params, email_address: email)
-        expect(email.verified).to be(true)
-      end
+    it 'runs ActivateUser for students' do
+      expect_any_instance_of(ActivateUser).to receive(:exec)
+      described_class.call(params: student_params, email_address: student_email)
+    end
+
+    it 'sets the educator user (email owner) state to "activated"' do
+      expect(educator_email.user.state).not_to eq('activated')
+      described_class.call(params: educator_params, email_address: educator_email)
+      expect(educator_email.user.state).to eq('activated')
+    end
+
+    it 'sets the student user (email owner) state to "activated"' do
+      expect(student_email.user.state).not_to eq('activated')
+      described_class.call(params: student_params, email_address: student_email)
+      expect(student_email.user.state).to eq('activated')
+    end
+
+    it 'marks the educator user EmailAddress as verified' do
+      expect(educator_email.verified).to be(false)
+      described_class.call(params: educator_params, email_address: educator_email)
+      expect(educator_email.verified).to be(true)
+    end
+
+    it 'marks the student user EmailAddress as verified' do
+      expect(student_email.verified).to be(false)
+      described_class.call(params: student_params, email_address: student_email)
+      expect(student_email.verified).to be(true)
+    end
+
+    it 'updates a student status to :no_faculty_info' do
+      described_class.call(params: student_params, email_address: student_email)
+      expect(student_user.faculty_status).to eq('no_faculty_info')
     end
   end
 end
+
