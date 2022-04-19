e13acf904bf08b5112911bf680c460336abbd4ac
Michael Harrison <mwharrison@rice.edu>
2022-04-19 11:27:17 -0500

    properly show number of steps for student vs educator

--- app/helpers/educator_signup_helper.rb
+++ app/helpers/educator_signup_helper.rb
@@ -3,14 +3,6 @@ module EducatorSignupHelper
     params[:verificationid]
   end
 
-  def is_school_not_supported_by_sheerid?
-    params[:school].present?
-  end
-
-  def is_country_not_supported_by_sheerid?
-    params[:country].present?
-  end
-
   def should_show_school_issued_email_field?
     is_cs_form?
   end
