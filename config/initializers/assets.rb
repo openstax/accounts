a7529592ae5efd1fd9b4ad98914f007a204a9687
Michael Harrison <volo@rice.edu>
2022-05-03 22:19:33 -0500

    add admin.css to assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.version = '1.1'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
   admin.js
+  admin.css
   profile.js
   profile.css
   libphonenumber/utils.js
