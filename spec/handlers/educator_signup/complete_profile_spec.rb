e0070764ec5913d0a1a30cbf63536cc3b998c586
Michael Harrison <volo@rice.edu>
2022-05-09 15:08:06 -0500

    fix complete profile spec

--- spec/handlers/educator_signup/complete_profile_spec.rb
+++ spec/handlers/educator_signup/complete_profile_spec.rb
@@ -3,31 +3,25 @@ require 'rails_helper'
 module EducatorSignup
 
   describe CompleteProfile, type: :handler do
-    let(:email) { Faker::Internet.email }
+    let(:email) { 'openstax@example.com' }
     let(:user) { create_user(email) }
     let(:handle) { described_class.handle( params: params, user: user) }
-    let(:books_used) { ['Algebra and Trigonometry', 'Physics'] }
-    let(:num_students_per_semester_taught) { 10 }
-    let(:educator_specific_role) { 'instructor' }
-    let(:using_openstax_how) { :as_primary }
-    let(:params) do
-      {
-        signup: {
-          books_used: books_used,
-          num_students_per_semester_taught: num_students_per_semester_taught,
-          using_openstax_how: using_openstax_how,
-          educator_specific_role: :instructor,
-        }
-      }
-    end
-
     before(:each) do
       disable_sfdc_client
     end
 
     context 'with invalid params' do
       context 'other must be filled out' do
-        let(:educator_specific_role) { :other }
+        let(:params) do
+          {
+            signup: {
+              books_used: ['Algebra and Trigonometry', 'Physics'],
+              num_students_per_semester_taught: [10, 15],
+              using_openstax_how: :as_primary,
+              educator_specific_role: :other,
+            }
+          }
+        end
 
         it "should return correct error" do
           result = handle
@@ -37,20 +31,35 @@ module EducatorSignup
       end
 
       context 'books used must be filled out' do
-        let(:educator_specific_role) { :instructor }
-        let(:using_openstax_how) { :as_primary }
-        let(:books_used) { [] }
+        let(:params) do
+          {
+            signup: {
+              books_of_interest: [],
+              num_students_per_semester_taught: [10, 15],
+              using_openstax_how: :as_primary,
+              educator_specific_role: :instructor,
+            }
+          }
+        end
 
         it "should return correct error" do
           result = handle
           expect(result.errors.count).to eq 1
-          expect(result.errors.first.message).to eq 'Please enter books used'
+          expect(result.errors.first.message).to eq 'Please enter books of interest'
         end
       end
 
       context 'number of students taught must be filled out' do
-        let(:educator_specific_role) { :instructor }
-        let(:num_students_per_semester_taught) { nil }
+        let(:params) do
+          {
+            signup: {
+              books_of_interest: ['Algebra and Trigonometry', 'Physics'],
+              num_students_per_semester_taught: [],
+              using_openstax_how: :as_primary,
+              educator_specific_role: :instructor,
+            }
+          }
+        end
 
         it "should return correct error" do
           result = handle
