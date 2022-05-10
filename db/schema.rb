c16e8107829ce84b4b616dcbce5e6c42f70b4f56
Roy Johnson <roy.e.johnson@rice.edu>
2022-05-10 11:45:38 -0500

    Fix up layout for Profile

--- db/schema.rb
+++ db/schema.rb
@@ -131,9 +131,6 @@ ActiveRecord::Schema.define(version: 2022_05_02_215806) do
     t.index ["verified"], name: "index_contact_infos_on_verified"
   end
 
-  create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
-  end
-
   create_table "delayed_jobs", id: :serial, force: :cascade do |t|
     t.integer "priority", default: 0, null: false
     t.integer "attempts", default: 0, null: false
