ca0c32dd33dcb2accbeac1678f7de3220379b58a
Michael Harrison <volo@rice.edu>
2022-05-17 11:20:11 -0500

    add default scope for doorkeeper

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -102,7 +102,7 @@ Doorkeeper.configure do
   # For more information go to
   # https://github.com/doorkeeper-gem/doorkeeper/wiki/Using-Scopes
   #
-  # default_scopes  :public
+  default_scopes :public
   # optional_scopes :write, :update
 
   # Change the way client credentials are retrieved from the request object.
