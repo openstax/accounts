4032ca9aecd78f0130f8698102892bb7ab6cb8c6
Michael Harrison <volo@rice.edu>
2022-05-26 11:59:11 -0500

    fix redirect loop for confirmed users and students

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,10 +2,9 @@ class ProfileController < ApplicationController
 
   before_action :authenticate_user!, only: :profile
   before_action :prevent_caching, only: :profile
+  before_action :user_signup_complete_check, only: :profile
 
-  def profile
-    user_signup_complete_check
-  end
+  def profile; end
 
   def exit_accounts
     if (redirect_param = extract_params(request.referer)[:r])
