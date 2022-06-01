f68e221b8e08ebcaf8a70c679b1b0653f987d98c
Michael Harrison <volo@rice.edu>
2022-06-01 11:11:40 -0500

    upload migrations from PR#1070 to avoid breaking deployed things

--- db/migrate/20220502160133_remove_pre_auth_states.rb
+++ db/migrate/20220502160133_remove_pre_auth_states.rb
@@ -1,6 +1,7 @@
-class RemovePreAuthStates < ActiveRecord::Migration[5.2]
+class RemoveSalesforceStreamingTables < ActiveRecord::Migration[5.2]
   def up
-    drop_table(:pre_auth_states) if table_exists?(:pre_auth_states)
+    drop_table(:salesforce_streaming_replays) if table_exists?(:salesforce_streaming_replays)
+    drop_table(:push_topics) if table_exists?(:push_topics)
   end
 
   def down
