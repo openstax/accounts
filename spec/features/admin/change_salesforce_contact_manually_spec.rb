1e17bbaf91054b618f62a9a409a8c7240c973a90
Michael Harrison <volo@rice.edu>
2022-06-08 14:32:16 -0500

    just for fun, let's revert this back to what wasn't working locally

--- spec/features/admin/change_salesforce_contact_manually_spec.rb
+++ spec/features/admin/change_salesforce_contact_manually_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'vcr_helper'
 
-RSpec.feature "Change Salesforce contact manually", vcr: VCR_OPTS, :type => :feature, js: true do
+RSpec.describe "Change Salesforce contact manually", vcr: VCR_OPTS do
   before(:all) do
     VCR.use_cassette('Admin/sf_setup', VCR_OPTS) do
       @proxy = SalesforceProxy.new
