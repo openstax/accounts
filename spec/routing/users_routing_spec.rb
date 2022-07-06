85ef6016566aba5c2f38276553cfc85bcc3ea4fe
Michael Harrison <volo@rice.edu>
2022-07-06 10:56:03 -0500

    update route spec to other controller

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe OtherController, :type => :routing do
   describe "routing" do
 
     it "routes to #update" do
-      expect(put('/profile')).to route_to('users#update')
+      expect(put('/profile')).to route_to('other#update')
     end
 
   end
