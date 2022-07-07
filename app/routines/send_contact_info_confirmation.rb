6262ceb4f97163f666b8a384b990be4e05fdcc50
Michael Harrison <volo@rice.edu>
2022-07-07 16:52:31 -0500

    update ContactInfosCreate to use the correct mailer

--- app/routines/send_contact_info_confirmation.rb
+++ app/routines/send_contact_info_confirmation.rb
@@ -4,22 +4,16 @@ class SendContactInfoConfirmation
 
   protected
 
-  def exec(contact_info:, send_pin: false)
+  def exec(contact_info:)
     return if contact_info.verified
 
-    contact_info.init_confirmation_pin! if send_pin
     contact_info.init_confirmation_code!
     contact_info.save
     transfer_errors_from(contact_info, {type: :verbatim}, true)
 
     case contact_info.type
     when 'EmailAddress'
-      if contact_info.user.is_unclaimed?
-        UnclaimedUserMailer.welcome(contact_info).deliver_later
-      else
-        ConfirmationMailer.instructions(email_address: contact_info,
-                                        send_pin: send_pin).deliver_later
-      end
+        NewflowMailer.signup_email_confirmation(email_address: contact_info).deliver_later
     else
       fatal_error(code: :not_yet_implemented, data: contact_info)
     end
