51244a16f75571384e1983dc10226a68540143b9
Michael Harrison <volo@rice.edu>
2022-07-12 15:08:54 -0500

    move newflow controller specs out of newflow dir

---
+++ spec/controllers/profile_controller_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe ProfileController, type: :controller do
+  describe 'GET #profile_newflow' do
+    context 'when logged in' do
+      before do
+        user.update!(role: :instructor)
+        mock_current_user(user)
+      end
+
+      let(:user) { create_newflow_user('user@openstax.org') }
+
+      context 'when profile is complete' do
+        before do
+          user.update!(is_profile_complete: true)
+        end
+
+          it 'renders 200 OK status' do
+          get(:profile_newflow)
+          expect(response.status).to eq(200)
+        end
+
+        it 'renders profile_newflow' do
+          get(:profile_newflow)
+          expect(response).to render_template(:profile_newflow)
+        end
+      end
+
+      context 'when profile is not complete' do
+        before { user.update!(is_profile_complete: false) }
+
+        it 'redirects to step 4 — complete profile form' do
+          get(:profile_newflow)
+          expect(response).to redirect_to(educator_profile_form_path)
+        end
+      end
+    end
+
+    context 'while not logged in' do
+      it 'redirects to login form' do
+        get(:profile_newflow)
+        expect(response).to redirect_to login_path
+      end
+    end
+  end
+end
---
+++ spec/controllers/signup_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe SignupController, type: :controller do
+  describe 'GET #welcome' do
+    it 'renders welcome form/page' do
+      get(:welcome)
+      expect(response).to render_template(:welcome)
+    end
+  end
+
+  describe 'GET #signup_done' do
+    before do
+      user = FactoryBot.create(:user)
+      mock_current_user(user)
+    end
+
+    it 'renders' do
+      get(:signup_done)
+      expect(response).to render_template(:signup_done)
+    end
+  end
+end
