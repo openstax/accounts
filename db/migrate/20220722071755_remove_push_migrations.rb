a7f2d15a3c941b2bfd468157131ff4beed183480
Michael Harrison <volo@rice.edu>
2022-07-25 12:06:35 -0500

    remove unneeded migration

--- db/migrate/20220722071755_remove_push_migrations.rb
+++
@@ -1,9 +0,0 @@
-class RemovePushMigrations < ActiveRecord::Migration[5.2]
-  def up
-    drop_table :push_topics if table_exists?(:push_topics)
-  end
-
-  def down
-    fail ActiveRecord::IrreversibleMigration
-  end
-end
