10a1dc3af29092e95fbd4f4490d200331b9478cf
Michael Harrison <volo@rice.edu>
2022-07-28 16:31:16 -0500

    remove group representers

--- app/representers/api/v1/group_owners_representer.rb
+++
@@ -1,9 +0,0 @@
-require 'representable/json/collection'
-
-module Api::V1
-  module GroupOwnersRepresenter
-    include Representable::JSON::Collection
-
-    items class: GroupOwner, decorator: GroupOwnerRepresenter
-  end
-end
--- app/representers/api/v1/groups_representer.rb
+++
@@ -1,9 +0,0 @@
-require 'representable/json/collection'
-
-module Api::V1
-  module GroupsRepresenter
-    include Representable::JSON::Collection
-
-    items class: Group, decorator: GroupRepresenter
-  end
-end
