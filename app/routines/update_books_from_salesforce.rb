3f05e292e6896de3f5f8573dcd79d2ae256a8544
Michael Harrison <volo@rice.edu>
2022-08-04 14:21:55 -0500

    fix book counting on update

--- app/routines/update_books_from_salesforce.rb
+++ app/routines/update_books_from_salesforce.rb
@@ -12,14 +12,14 @@ class UpdateBooksFromSalesforce
     log('Starting UpdateBookSalesforceInfo')
 
     books_updated = 0
-    sf_books      = OpenStax::Salesforce::Remote::Book.order(:Id)
+    sf_books = OpenStax::Salesforce::Remote::Book.order(:Id)
 
     sf_books.each do |sf_book|
-      book                 = Book.find_or_initialize_by(salesforce_id: sf_book.id)
+      book = Book.find_or_initialize_by(salesforce_id: sf_book.id)
       book.salesforce_name = sf_book.name
-      book.official_name   = sf_book.official_name
+      book.official_name = sf_book.official_name
 
-      books_updated        += 1 if book.save!
+      books_updated += 1 if book.save
     end
 
     log("Finished updating #{books_updated} books")
