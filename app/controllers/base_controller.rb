92c88f9e4f03106abf834240c663db712017972b
Michael Harrison <volo@rice.edu>
2022-08-19 11:57:06 -0500

    remove educator_signup_flow_decorator

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -6,11 +6,7 @@ class BaseController < ApplicationController
 
   before_action :set_active_banners
 
-  protected #################
-
-  def decorated_user
-    EducatorSignupFlowDecorator.new(current_user, action_name)
-  end
+  protected
 
   def restart_signup_if_missing_unverified_user
     redirect_to signup_path unless unverified_user.present?
