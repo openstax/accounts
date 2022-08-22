f8aa4505a304ce8eb12a38196aa8324e54a625ca
Michael Harrison <volo@rice.edu>
2022-08-22 12:32:18 -0500

    remove temp_user from access policy

--- spec/access_policies/user_access_policy_spec.rb
+++ spec/access_policies/user_access_policy_spec.rb
@@ -3,16 +3,13 @@ require 'rails_helper'
 RSpec.describe UserAccessPolicy do
 
   let!(:anon)       { AnonymousUser.instance }
-  let!(:temp)       { FactoryBot.create :temp_user }
   let!(:user)       { FactoryBot.create :user }
-  let!(:new_social) { FactoryBot.create :new_social_user }
   let!(:admin)      { FactoryBot.create :user, :admin }
   let!(:app)        { FactoryBot.create :doorkeeper_application }
 
   context 'search' do
-    it 'cannot be accessed by anonymous or temp users' do
+    it 'cannot be accessed by anonymous users' do
       expect(OSU::AccessPolicy.action_allowed?(:search, anon, User)).to eq false
-      expect(OSU::AccessPolicy.action_allowed?(:search, temp, User)).to eq false
     end
 
     it 'can be accessed by non-temp users' do
@@ -31,14 +28,13 @@ RSpec.describe UserAccessPolicy do
       expect(OSU::AccessPolicy.action_allowed?(:update, app, user)).to eq false
     end
 
-    it 'can be accessed by human users, temp or not' do
+    it 'can be accessed by human users' do
       expect(OSU::AccessPolicy.action_allowed?(:read, user, user)).to eq true
       expect(OSU::AccessPolicy.action_allowed?(:read, admin, admin)).to eq true
       expect(OSU::AccessPolicy.action_allowed?(:read, temp, temp)).to eq true
 
       expect(OSU::AccessPolicy.action_allowed?(:update, user, user)).to eq true
       expect(OSU::AccessPolicy.action_allowed?(:update, admin, admin)).to eq true
-      expect(OSU::AccessPolicy.action_allowed?(:update, temp, temp)).to eq true
     end
 
     it 'cannot access other users unless admin' do
@@ -58,17 +54,6 @@ RSpec.describe UserAccessPolicy do
 
     it 'cannot be accessed by apps' do
       expect(OSU::AccessPolicy.action_allowed?(:signup, app, user)).to eq false
-    end
-
-    it 'can be accessed by temp users and new_social_users' do
-      expect(OSU::AccessPolicy.action_allowed?(:signup, temp, temp)).to eq true  # deprecated check
-      expect(OSU::AccessPolicy.action_allowed?(:signup, new_social, new_social)).to eq true
-    end
-
-    it 'cannot access other users' do
-      expect(OSU::AccessPolicy.action_allowed?(:signup, temp, user)).to eq false
-      expect(OSU::AccessPolicy.action_allowed?(:signup, new_social, user)).to eq false
-      expect(OSU::AccessPolicy.action_allowed?(:signup, temp, admin)).to eq false
     end
   end
 
