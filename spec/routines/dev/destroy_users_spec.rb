0fae8893fa5911422b456ceb73746aef4c709867
Michael Harrison <volo@rice.edu>
2022-08-24 12:48:47 -0500

    dev routine spec, destory_org -> destroy

--- spec/routines/dev/destroy_users_spec.rb
+++ spec/routines/dev/destroy_users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Dev::DestroyUsers do
 
   it "works" do
-    FactoryBot.create(:temp_user)
+    FactoryBot.create(:user)
     FactoryBot.create(:user)
 
     expect{Dev::DestroyUsers.call(User.all)}.to change(User, :count).by(-2)
