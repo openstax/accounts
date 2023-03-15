a24985aea4c20f73eac6f1c6e103e9062eceef87
Dante Soares <dante.m.soares@rice.edu>
2023-03-15 13:13:53 -0500

    Allow contracts to be posed and signed with an access token

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,10 +1,11 @@
 class TermsController < ApplicationController
-  skip_before_action :authenticate_user!, :complete_signup_profile, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :pose_by_name, :agree]
+  skip_before_action :complete_signup_profile, only: [:index, :show]
+
+  before_action :authenticate_user_with_token!, :allow_iframe_access, only: [:pose_by_name, :agree]
+  before_action :get_contract, only: [:show]
 
   fine_print_skip :general_terms_of_use, :privacy_policy
-
-  before_action :allow_iframe_access, only: [:pose_by_name, :agree]
-  before_action :get_contract, only: [:show]
 
   def index
     @contracts = [FinePrint.get_contract(:general_terms_of_use),
@@ -45,6 +46,16 @@ class TermsController < ApplicationController
   end
 
   protected
+
+  def authenticate_user_with_token!
+    if params[:token].present?
+      token = Doorkeeper::AccessToken.find_by token: params[:token]
+      return head(:forbidden) if token.nil?
+      @current_user = token.user
+    else
+      authenticate_user!
+    end
+  end
 
   def get_contract
     @contract = FinePrint.get_contract(params[:id])
