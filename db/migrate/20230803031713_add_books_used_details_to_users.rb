30e8fa60662220918b26829a18faa5b3e6a7bd26
Josiah Ivey <josiah.ivey@gmail.com>
2023-08-03 07:49:31 -0700

    rails does not default to jsonb it seems

--- db/migrate/20230803031713_add_books_used_details_to_users.rb
+++ db/migrate/20230803031713_add_books_used_details_to_users.rb
@@ -1,5 +1,5 @@
 class AddBooksUsedDetailsToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :books_used_details, :json
+    add_column :users, :books_used_details, :jsonb
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -481,7 +481,7 @@ ActiveRecord::Schema.define(version: 2023_08_03_031713) do
     t.boolean "faculty_verification_email_sent"
     t.boolean "needs_sync"
     t.boolean "sheer_id_webhook_received"
-    t.json "books_used_details"
+    t.jsonb "books_used_details"
     t.index "lower((first_name)::text)", name: "index_users_on_first_name"
     t.index "lower((last_name)::text)", name: "index_users_on_last_name"
     t.index "lower((username)::text)", name: "index_users_on_username_case_insensitive"
