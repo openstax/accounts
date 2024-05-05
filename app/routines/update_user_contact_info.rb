59ec5e19fa39465d4526daf8ced1e9b5332b1fe1
Michael Harrison <volo@rice.edu>
2024-05-05 16:51:33 -0500

    do not create security logs for failed school cache

--- app/routines/update_user_contact_info.rb
+++ app/routines/update_user_contact_info.rb
@@ -121,11 +121,6 @@ class UpdateUserContactInfo
       user.grant_tutor_access = sf_contact.grant_tutor_access
 
       if school.nil? && !sf_school.nil?
-        SecurityLog.create!(
-          user: user,
-          event_type: :attempted_to_add_school_not_cached_yet,
-          event_data: { school_id: sf_school.id }
-        )
         users_without_cached_school += 1
         Sentry.capture_message("User #{user.id} has a school that is in SF but not cached yet #{sf_school.id}")
       else
