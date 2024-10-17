d1566fb183ad7ada09197246e2e0aaa1fc93cd09
Dante Soares <dante.m.soares@rice.edu>
2024-10-17 16:19:41 -0500

    Updated web-console gem, simplified ErrorPageBuilder

--- lib/error_page_builder.rb
+++ lib/error_page_builder.rb
@@ -1,17 +1,6 @@
 class ErrorPageBuilder
   attr_reader :view, :message, :code
 
-  class DummyRequest < OpenStruct
-    def initialize
-      super
-      self.path_parameters ||= {}
-    end
-
-    def engine_script_name(_)
-      ''
-    end
-  end
-
   def initialize(view:, message:, code:)
     @view = view
     @message = message
@@ -19,9 +8,7 @@ class ErrorPageBuilder
   end
 
   def build
-    renderer = ApplicationController.new
-    renderer.set_request! DummyRequest.new
-    renderer.render_to_string(
+    ApplicationController.renderer.render(
       template: 'errors/static',
       layout: 'static_error',
       locals: { view: view, message: message, code: code }
