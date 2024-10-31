11e9ffdf7330a511cbb6b2c7c4974c8be9ac2d06
Michael Volo <volo@rice.edu>
2024-10-31 16:46:38 -0500

    fix rejected states

--- lib/fetch_book_data.rb
+++ lib/fetch_book_data.rb
@@ -19,7 +19,8 @@ class FetchBookData
 
     items = results.fetch('items', [])
     # See https://github.com/openstax/openstax-cms/blob/main/books/constants.py#L7 for book states
-    books = items.select{ |i| i['book_state'] != 'retired' || i['book_state'] != 'unlisted' }
+    # !['retired', 'unlisted'].include?(i['book_state'])
+    books = items.reject{ %w[retired unlisted].include?(i['book_state']) }
 
     books_with_subject = []
 
