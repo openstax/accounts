35f23b75833bfcd017a0f5e4c3f77e4317ed43f2
Michael Volo <volo@rice.edu>
2024-11-19 12:15:13 -0600

    return 403 if token is expired and add spec

--- spec/mailers/newflow_mailer_spec.rb
+++ spec/mailers/newflow_mailer_spec.rb
@@ -2,45 +2,28 @@ require 'rails_helper'
 
 module Newflow
   describe NewflowMailer, type: :mailer do
-    # let(:user) { FactoryBot.create :user, first_name: 'John', last_name: 'Doe', suffix: 'Jr.' }
-    # let(:email) { FactoryBot.create :email_address, value: 'to@example.org',
-    #                                 user_id: user.id, confirmation_code: '1234', confirmation_pin: '123456' }
+    let(:user) { FactoryBot.create :user, first_name: 'John', last_name: 'Doe', suffix: 'Jr.' }
+    let(:email) { FactoryBot.create :email_address, value: 'to@example.org',
+                                    user_id: user.id, confirmation_code: '1234', confirmation_pin: '123456' }
 
-    describe '' do
-      # it 'has basic header and from info and greeting' do
-      #   mail = ConfirmationMailer.instructions email_address: email
+    describe 'sends email confirmation' do
+      it 'has basic header and from info and greeting' do
+        mail = NewflowMailer.signup_email_confirmation email_address: email
 
-      #   expect(mail.header['to'].to_s).to eq('"John Doe Jr." <to@example.org>')
-      #   expect(mail.from).to eq(["noreply@openstax.org"])
-      #   expect(mail.body.encoded).to include("Hi #{user.casual_name}")
-      # end
+        expect(mail.header['to'].to_s).to eq('to@example.org')
+        expect(mail.from).to eq(["noreply@openstax.org"])
+        expect(mail.body.encoded).to include("Welcome to OpenStax!")
+      end
 
-      # it 'does not include PIN when directed not to' do
-      #   mail = ConfirmationMailer.instructions email_address: email, send_pin: false
+      it "has PIN info" do
+        allow(ConfirmByPin).to receive(:sequential_failure_for) { Hashie::Mash.new('attempts_remaining?' => true)}
 
-      #   expect(mail.subject).to eq("[OpenStax] Confirm your email address")
-      #   expect(mail.body.encoded).not_to include('Your PIN')
-      # end
+        mail = NewflowMailer.signup_email_confirmation email_address: email
 
-      # it "has PIN info when PIN attempts remain" do
-      #   allow(ConfirmByPin).to receive(:sequential_failure_for) { Hashie::Mash.new('attempts_remaining?' => true)}
-
-      #   mail = ConfirmationMailer.instructions email_address: email, send_pin: true
-
-      #   expect(mail.subject).to eq("[OpenStax] Use PIN 123456 to confirm your email address")
-      #   expect(mail.body.encoded).to include('Enter your 6-digit')
-      #   expect(mail.body.encoded).to include('Your PIN: <b>123456</b>')
-      # end
-
-      # it "has just link when no PIN attempts remain" do
-      #   allow(ConfirmByPin).to receive(:sequential_failure_for) { Hashie::Mash.new('attempts_remaining?' => false)}
-
-      #   mail = ConfirmationMailer.instructions email_address: email, send_pin: true
-
-      #   expect(mail.subject).to eq("[OpenStax] Confirm your email address")
-      #   expect(mail.body.encoded).to include('Click on the link below')
-      #   expect(mail.body.encoded).not_to include('Your PIN')
-      # end
+        expect(mail.subject).to eq("[OpenStax] Your OpenStax account PIN has arrived: 123456")
+        expect(mail.body.encoded).to include('<a href="http://localhost:2999/i/verify_email_by_code/1234"')
+        expect(mail.body.encoded).to include('use your pin: <b id=\'pin\'>123456</b>')
+      end
     end
   end
 end
