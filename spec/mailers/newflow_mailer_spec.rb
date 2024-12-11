4b11790f5fac8809b40c973753019d79ffe0b4d7
Josiah Ivey <josiah.ivey@gmail.com>
2024-12-11 08:20:29 -0800

    actually include the dry-ed changes

--- spec/mailers/newflow_mailer_spec.rb
+++ spec/mailers/newflow_mailer_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 module Newflow
   describe NewflowMailer, type: :mailer do
+    let(:pin) { '123456' }
+    let(:code) { '1234' }
+    let(:confirm_url) { "http://localhost:2999/i/verify_email_by_code/#{code}" }
     let(:user) { FactoryBot.create :user, first_name: 'John', last_name: 'Doe', suffix: 'Jr.' }
-    let(:email) { FactoryBot.create :email_address, value: 'to@example.org',
-                                    user_id: user.id, confirmation_code: '1234', confirmation_pin: '123456' }
+    let(:email) {
+      FactoryBot.create :email_address,
+                        value: 'to@example.org',
+                        user_id: user.id,
+                        confirmation_code: code,
+                        confirmation_pin: pin
+    }
 
     describe 'sends email confirmation' do
       it 'has basic header and from info and greeting' do
@@ -20,7 +28,7 @@ module Newflow
           mail = NewflowMailer.signup_email_confirmation(email_address: email)
 
           expect(mail.subject).to eq("[OpenStax] Your OpenStax account PIN has arrived: #{pin}")
-          expect(mail.body.encoded).to include(confirmation_url)
+          expect(mail.body.encoded).to include("<a href=\"#{confirm_url}\"")
           expect(mail.body.encoded).to include("use your pin: <b id='pin'>#{pin}</b>")
         end
       end
@@ -30,7 +38,7 @@ module Newflow
           mail = NewflowMailer.signup_email_confirmation(email_address: email, show_pin: nil)
 
           expect(mail.subject).to eq("[OpenStax] Your OpenStax account PIN has arrived: #{pin}")
-          expect(mail.body.encoded).to include(confirmation_url)
+          expect(mail.body.encoded).to include("<a href=\"#{confirm_url}\"")
           expect(mail.body.encoded).to include("use your pin: <b id='pin'>#{pin}</b>")
         end
       end
@@ -40,7 +48,7 @@ module Newflow
           mail = NewflowMailer.signup_email_confirmation(email_address: email, show_pin: false)
 
           expect(mail.subject).to eq("[OpenStax] Confirm your email address")
-          expect(mail.body.encoded).to include(confirmation_url)
+          expect(mail.body.encoded).to include("<a href=\"#{confirm_url}\"")
           expect(mail.body.encoded).not_to include("use your pin: <b id='pin'>#{pin}</b>")
         end
       end
