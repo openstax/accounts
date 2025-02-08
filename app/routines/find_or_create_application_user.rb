ca77124377e6d63b233913e25362e48ee454e58a
Dante Soares <dante.m.soares@rice.edu>
2025-02-07 18:32:44 -0600

    Attempt to fix some FindOrCreate routines

--- app/routines/find_or_create_application_user.rb
+++ app/routines/find_or_create_application_user.rb
@@ -1,22 +1,15 @@
 # Finds or creates an ApplicationUser for the given application and user.
 class FindOrCreateApplicationUser
-
   lev_routine
 
   protected
 
   def exec(application_id, user_id)
-    application_user = ApplicationUser.where(:application_id => application_id,
-                                             :user_id => user_id).first
-    unless application_user
-      application_user = ApplicationUser.create do |app_user|
-        app_user.application_id = application_id
-        app_user.user_id = user_id
-        app_user.save!
-      end
-    end
-
-    outputs[:application_user] = application_user
+    # Attempt to insert the record, ignoring conflicts
+    # If no record is inserted, query the existing record
+    outputs.application_user = ApplicationUser.import(
+      [ ApplicationUser.new(application_id: application_id, user_id: user_id) ],
+      on_duplicate_key_ignore: true
+    ).results.first || ApplicationUser.find_by(application_id: application_id, user_id: user_id)
   end
-
 end
