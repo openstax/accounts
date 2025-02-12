81a7ff279ad1a41554ab674927c2ae36992c6531
Dante Soares <dante.m.soares@rice.edu>
2025-02-12 14:47:35 -0600

    Attempt to prevent the id sequence from autoincrementing too much

--- app/routines/find_or_create_application_groups.rb
+++ app/routines/find_or_create_application_groups.rb
@@ -1,18 +1,30 @@
 # Finds or creates ApplicationGroups for the given application and group_ids.
 class FindOrCreateApplicationGroups
-  lev_routine
+  lev_routine transaction: :read_committed
 
   protected
 
   def exec(application_id, group_ids)
-    outputs.application_groups = ApplicationGroup.import(
-      group_ids.map do |group_id|
+    outputs.application_groups = ApplicationGroup.where(
+      application_id: application_id, group_id: group_ids
+    ).to_a
+
+    # Insert missing records
+    # Returns newly-inserted records only
+    # We could run this as the first query, but it would cause the id sequence to autoincrement
+    # by the number of group_ids every time this routine is called
+    # We can still do that if we switch the primary key to a uuid column
+    missing_group_ids = group_ids - outputs.application_groups.map(&:group_id)
+    new_records = ApplicationGroup.import(
+      missing_group_ids.map do |group_id|
         ApplicationGroup.new(application_id: application_id, group_id: group_id)
       end, on_duplicate_key_ignore: true
-    ).results # This returns newly-inserted records only
+    ).results
+    outputs.application_groups += new_records
 
-    # This returns records that were already present
-    existing_group_ids = group_ids - outputs.application_groups.map(&:group_id)
+    # Run the first query again in case another process or thread
+    # inserted the missing records before us
+    existing_group_ids = missing_group_ids - new_records.map(&:group_id)
     outputs.application_groups += ApplicationGroup.where(
       application_id: application_id, group_id: existing_group_ids
     ).to_a
--- app/routines/find_or_create_application_user.rb
+++ app/routines/find_or_create_application_user.rb
@@ -1,15 +1,23 @@
 # Finds or creates an ApplicationUser for the given application and user.
 class FindOrCreateApplicationUser
-  lev_routine
+  lev_routine transaction: :read_committed
 
   protected
 
   def exec(application_id, user_id)
-    # Attempt to insert the record, ignoring conflicts
-    # If no record is inserted, query the existing record
-    outputs.application_user = ApplicationUser.import(
+    # First attempt to query the record
+    # If no record found, attempt to insert it record, ignoring conflicts
+    # If no record is inserted, query the existing record again
+    # The first query is necessary to prevent the id sequence from autoincrementing
+    # every time this routine is called
+    # We can remove it if we switch the primary key to a uuid column
+    outputs.application_user = ApplicationUser.find_by(
+      application_id: application_id, user_id: user_id
+    ) || ApplicationUser.import(
       [ ApplicationUser.new(application_id: application_id, user_id: user_id) ],
       on_duplicate_key_ignore: true
-    ).results.first || ApplicationUser.find_by(application_id: application_id, user_id: user_id)
+    ).results.first || ApplicationUser.find_by(
+      application_id: application_id, user_id: user_id
+    )
   end
 end
