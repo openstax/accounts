fabbe35c4c555592488056c3e7cde03a31363d96
Dante Soares <dante.m.soares@rice.edu>
2025-02-13 11:07:37 -0600

    Added some early returns for FindOrCreateApplicationGroups

--- app/routines/find_or_create_application_groups.rb
+++ app/routines/find_or_create_application_groups.rb
@@ -9,12 +9,14 @@ class FindOrCreateApplicationGroups
       application_id: application_id, group_id: group_ids
     ).to_a
 
+    missing_group_ids = group_ids - outputs.application_groups.map(&:group_id)
+    return if missing_group_ids.empty?
+
     # Insert missing records
     # Returns newly-inserted records only
     # We could run this as the first query, but it would cause the id sequence to autoincrement
     # by the number of group_ids every time this routine is called
     # We can still do that if we switch the primary key to a uuid column
-    missing_group_ids = group_ids - outputs.application_groups.map(&:group_id)
     new_records = ApplicationGroup.import(
       missing_group_ids.map do |group_id|
         ApplicationGroup.new(application_id: application_id, group_id: group_id)
@@ -22,9 +24,11 @@ class FindOrCreateApplicationGroups
     ).results
     outputs.application_groups += new_records
 
+    existing_group_ids = missing_group_ids - new_records.map(&:group_id)
+    return if existing_group_ids.empty?
+
     # Run the first query again in case another process or thread
     # inserted the missing records before us
-    existing_group_ids = missing_group_ids - new_records.map(&:group_id)
     outputs.application_groups += ApplicationGroup.where(
       application_id: application_id, group_id: existing_group_ids
     ).to_a
