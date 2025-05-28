ead50b3240d0d058a6d8761d854b1aa612f6f83c
Dante Soares <dante.m.soares@rice.edu>
2025-05-28 08:53:12 -0500

    Fix how recaptcha secrets are handled

--- lib/recaptcha.rb
+++ lib/recaptcha.rb
@@ -1,20 +1,22 @@
 # recaptcha_tags, recaptcha_v3, and verify_recaptcha come from the recaptcha gem
 # Our own helpers are named slightly differently to avoid conflicts
 module Recaptcha
+  def self.secrets
+    Rails.application.secrets.recaptcha || {}
+  end
+
   module View
     def recaptcha_with_fallback(action:, **options)
       if @recaptcha_failed
         recaptcha_tags
       else
-        recaptcha_v3 action: action, site_key: recaptcha_secrets[:site_key], **options
+        recaptcha_v3 action: action, site_key: Recaptcha.secrets[:site_key], **options
       end
     end
   end
 
   module Controller
     MINIMUM_SCORE = 0.2
-
-    recaptcha_secrets = Rails.application.secrets.recaptcha || {}
 
     def self.included(base)
       base.helper View
@@ -24,7 +26,7 @@ module Recaptcha
       params = {
         action: action_name,
         minimum_score: MINIMUM_SCORE,
-        secret_key: recaptcha_secrets[:secret_key],
+        secret_key: Recaptcha.secrets[:secret_key],
         **options
       }
 
