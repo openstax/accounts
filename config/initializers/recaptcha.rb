d528a8c45193de81863474b0a8424f5a4c76379e
Dante Soares <dante.m.soares@rice.edu>
2025-05-29 10:55:59 -0500

    Set recaptcha config in an initializer instead

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+recaptcha_secrets = Rails.application.secrets.recaptcha
+Recaptcha.configure do |config|
+  config.site_key  = recaptcha_secrets[:site_key]
+  config.secret_key = recaptcha_secrets[:secret_key]
+end
--- lib/recaptcha.rb
+++ lib/recaptcha.rb
@@ -1,16 +1,12 @@
 # recaptcha_tags, recaptcha_v3, and verify_recaptcha come from the recaptcha gem
 # Our own helpers are named slightly differently to avoid conflicts
 module Recaptcha
-  def self.secrets
-    Rails.application.secrets.recaptcha || {}
-  end
-
   module View
     def recaptcha_with_fallback(action:, **options)
       if @recaptcha_failed
         recaptcha_tags
       else
-        recaptcha_v3 action: action, site_key: Recaptcha.secrets[:site_key], **options
+        recaptcha_v3 action: action, **options
       end
     end
   end
@@ -26,11 +22,10 @@ module Recaptcha
       params = {
         action: action_name,
         minimum_score: MINIMUM_SCORE,
-        secret_key: Recaptcha.secrets[:secret_key],
         **options
       }
 
-      params[:response] = 'bogus' if params[:secret_key].blank? && Rails.env.development?
+      params[:response] = 'bogus' if Recaptcha.configuration.site_key.blank? && Rails.env.development?
 
       verify_recaptcha(**params).tap { |result| @recaptcha_failed = !result }
     end
