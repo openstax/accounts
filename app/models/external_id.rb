d0b0068e64429ed9667dd434a398d2ef5986119c
Dante Soares <dante.m.soares@rice.edu>
2026-01-28 18:22:32 -0600

    Add roles to external-ids, find and create external-ids for specific roles

--- app/models/external_id.rb
+++ app/models/external_id.rb
@@ -1,6 +1,8 @@
 class ExternalId < ApplicationRecord
   belongs_to :user, inverse_of: :external_ids
 
+  enum(role: User::VALID_ROLES)
+
   validates :user, presence: true
-  validates :external_id, presence: true, uniqueness: true
+  validates :external_id, presence: true, uniqueness: { scope: :role }
 end
