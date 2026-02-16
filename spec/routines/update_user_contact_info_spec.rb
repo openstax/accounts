85c03ac5463e72bb723626fe458e39fd3848d265
Michael Volo <volo@rice.edu>
2026-02-16 13:48:44 -0600

    move salesforce spec helpers to spec/support

--- spec/routines/update_user_contact_info_spec.rb
+++ spec/routines/update_user_contact_info_spec.rb
@@ -2,41 +2,6 @@ require 'rails_helper'
 
 describe UpdateUserContactInfo, type: :routine do
   let!(:school) { FactoryBot.create :school, salesforce_id: 'SF_SCHOOL_001' }
-  
-  # Helper method to create a Salesforce contact mock
-  def create_sf_contact(uuid:, faculty_verified:, contact_id: 'SF_CONTACT_001', school_id: 'SF_SCHOOL_001')
-    contact = OpenStax::Salesforce::Remote::Contact.new(
-      id: contact_id,
-      accounts_uuid: uuid,
-      faculty_verified: faculty_verified,
-      school_type: 'College/University (4)',
-      adoption_status: 'Not Adopter',
-      grant_tutor_access: false
-    )
-    
-    # Mock the school association
-    sf_school = OpenStax::Salesforce::Remote::School.new(
-      id: school_id,
-      school_location: 'Domestic',
-      is_kip: false,
-      is_child_of_kip: false
-    )
-    allow(contact).to receive(:school).and_return(sf_school)
-    allow(contact).to receive(:school_id).and_return(school_id)
-    
-    contact
-  end
-
-  # Helper method to stub the salesforce_contacts method
-  def stub_salesforce_contacts(contacts)
-    allow_any_instance_of(UpdateUserContactInfo).to receive(:salesforce_contacts).and_return(contacts)
-  end
-
-  # Helper method to stub Sentry methods
-  def stub_sentry
-    allow(Sentry).to receive(:capture_check_in).and_return('check_in_id')
-    allow(Sentry).to receive(:capture_message)
-  end
 
   describe 'faculty status preservation logic' do
     before { stub_sentry }
@@ -186,13 +151,31 @@ describe UpdateUserContactInfo, type: :routine do
         expect(user.reload.faculty_status).to eq('pending_faculty')
       end
 
-      it 'allows update to no_faculty_info' do
+      it 'does not downgrade to incomplete_signup' do
+        sf_contact = create_sf_contact(uuid: user.uuid, faculty_verified: 'incomplete_signup')
+        stub_salesforce_contacts([sf_contact])
+
+        described_class.call
+
+        expect(user.reload.faculty_status).to eq('rejected_faculty')
+      end
+
+      it 'does not downgrade to no_faculty_info' do
         sf_contact = create_sf_contact(uuid: user.uuid, faculty_verified: 'no_faculty_info')
         stub_salesforce_contacts([sf_contact])
 
         described_class.call
 
-        expect(user.reload.faculty_status).to eq('no_faculty_info')
+        expect(user.reload.faculty_status).to eq('rejected_faculty')
+      end
+
+      it 'does not downgrade when faculty_verified is nil' do
+        sf_contact = create_sf_contact(uuid: user.uuid, faculty_verified: nil)
+        stub_salesforce_contacts([sf_contact])
+
+        described_class.call
+
+        expect(user.reload.faculty_status).to eq('rejected_faculty')
       end
     end
 
