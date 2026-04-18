73126486d98cf1e3d8cb1f71735c534feba26664
Michael Volo <volo@rice.edu>
2026-04-18 10:49:57 -0500

    DATA-301: fix migration version bracket to [6.1] to match Rails

--- db/migrate/20260418065011_add_expected_start_semester_to_users.rb
+++ db/migrate/20260418065011_add_expected_start_semester_to_users.rb
@@ -1,4 +1,4 @@
-class AddExpectedStartSemesterToUsers < ActiveRecord::Migration[5.2]
+class AddExpectedStartSemesterToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :expected_start_semester, :string
   end
