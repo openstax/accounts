f87162c9f86d80c09ac9f32371931784ee65a976
Michael Volo <volo@rice.edu>
2026-06-11 16:10:35 -0500

    handle merged schools from salesforce

--- spec/routines/update_school_salesforce_info_spec.rb
+++ spec/routines/update_school_salesforce_info_spec.rb
@@ -20,6 +20,12 @@ describe UpdateSchoolSalesforceInfo, type: :routine do
     end
   end
 
+  before do
+    allow_any_instance_of(described_class).to(
+      receive(:merge_winner_salesforce_id).and_return(nil)
+    )
+  end
+
   context 'existing School' do
     before { school.save! }
 
@@ -29,7 +35,6 @@ describe UpdateSchoolSalesforceInfo, type: :routine do
       described_class.call
 
       expect { school.reload }.not_to raise_error
-      expect { deleted_school_with_users.reload }.not_to raise_error
       expect { deleted_school.reload }.to raise_error(ActiveRecord::RecordNotFound)
     end
 
@@ -58,6 +63,98 @@ describe UpdateSchoolSalesforceInfo, type: :routine do
     end
   end
 
+  context 'school with users that is missing from Salesforce' do
+    let!(:stale_user) { User.find_by(school_id: deleted_school_with_users.id) }
+
+    before { school.save! }
+
+    it 'repoints users to the merge winner and deletes the stale school' do
+      stub_schools school
+      allow_any_instance_of(described_class).to(
+        receive(:merge_winner_salesforce_id).with(
+          deleted_school_with_users.salesforce_id
+        ).and_return(school.salesforce_id)
+      )
+
+      described_class.call
+
+      expect(stale_user.reload.school_id).to eq school.id
+      expect { deleted_school_with_users.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'detaches users, deletes the school, and warns when no merge winner is found' do
+      stub_schools school
+
+      expect(Sentry).to receive(:capture_message).with(
+        /no merge winner/, hash_including(level: :warning)
+      )
+
+      described_class.call
+
+      expect(stale_user.reload.school_id).to be_nil
+      expect { deleted_school_with_users.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'leaves the school alone when the winner is not cached locally yet' do
+      stub_schools school
+      allow_any_instance_of(described_class).to(
+        receive(:merge_winner_salesforce_id).with(
+          deleted_school_with_users.salesforce_id
+        ).and_return('0010v0NotCached')
+      )
+
+      described_class.call
+
+      expect(stale_user.reload.school_id).to eq deleted_school_with_users.id
+      expect { deleted_school_with_users.reload }.not_to raise_error
+    end
+  end
+
+  describe '#merge_winner_salesforce_id' do
+    let(:routine)     { described_class.new }
+    let(:sfdc_client) { double('sfdc_client') }
+    let(:loser_id)    { '0010v0LoserAcct' }
+    let(:winner_id)   { '0010v0WinnerAcc' }
+
+    before do
+      allow_any_instance_of(described_class).to(
+        receive(:merge_winner_salesforce_id).and_call_original
+      )
+      allow(ActiveForce).to receive(:sfdc_client).and_return(sfdc_client)
+    end
+
+    it 'follows the MasterRecordId chain to the surviving account' do
+      allow(sfdc_client).to receive(:query_all).with(/#{loser_id}/).and_return(
+        [ { 'IsDeleted' => true, 'MasterRecordId' => winner_id } ]
+      )
+      allow(sfdc_client).to receive(:query_all).with(/#{winner_id}/).and_return(
+        [ { 'IsDeleted' => false, 'MasterRecordId' => nil } ]
+      )
+
+      expect(routine.send(:merge_winner_salesforce_id, loser_id)).to eq winner_id
+    end
+
+    it 'returns nil when the account is gone from Salesforce entirely' do
+      allow(sfdc_client).to receive(:query_all).and_return([])
+
+      expect(routine.send(:merge_winner_salesforce_id, loser_id)).to be_nil
+    end
+
+    it 'returns nil when the account was deleted without a merge' do
+      allow(sfdc_client).to receive(:query_all).and_return(
+        [ { 'IsDeleted' => true, 'MasterRecordId' => nil } ]
+      )
+
+      expect(routine.send(:merge_winner_salesforce_id, loser_id)).to be_nil
+    end
+
+    it 'returns nil without querying for a malformed id' do
+      expect(sfdc_client).not_to receive(:query_all)
+
+      expect(routine.send(:merge_winner_salesforce_id, "bad' OR Id != '")).to be_nil
+    end
+  end
+
   def stub_schools(schools)
     sf_schools = [schools].flatten.map do |school|
       attrs = school.attributes
@@ -67,11 +164,13 @@ describe UpdateSchoolSalesforceInfo, type: :routine do
       OpenStax::Salesforce::Remote::School.new attrs
     end
 
+    # select(:id).where(id: ...) runs once for the user-less school sweep and
+    # once for the schools-with-users reconciliation
     select_query = instance_double(ActiveForce::ActiveQuery)
-    expect(OpenStax::Salesforce::Remote::School).to(
+    allow(OpenStax::Salesforce::Remote::School).to(
       receive(:select).with(:id).and_return(select_query)
     )
-    expect(select_query).to receive(:where).with(id: kind_of(Array)).and_return(sf_schools)
+    allow(select_query).to receive(:where).with(id: kind_of(Array)).and_return(sf_schools)
 
     order_query = instance_double(ActiveForce::ActiveQuery)
     expect(OpenStax::Salesforce::Remote::School).to(
