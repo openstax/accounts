78f96ae0dfb1f5da49e7b658063a39c4ee6fd208
Michael Volo <volo@rice.edu>
2026-07-19 13:22:14 -0500

    Renumber instructor_connections migration to 20260719000004
    20260719000003 is taken by add_lms_used_to_users on the target branch;
    duplicate versions raise DuplicateMigrationVersionError.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2026_07_19_000003) do
+ActiveRecord::Schema.define(version: 2026_07_19_000004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
